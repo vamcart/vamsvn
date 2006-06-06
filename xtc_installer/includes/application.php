a8715a9e8020442c1fdf52c4a560541afbacfaa4
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-06-06 07:38:31 +0000

    Исправлние в файле /xtc_installer/includes/application.php, исправлена ошибка на 6 шаге установки

--- xtc_installer/includes/application.php
+++ xtc_installer/includes/application.php
@@ -29,7 +29,9 @@
       $local_install_path=str_replace('install_finished.php','',$local_install_path);
       define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT . $local_install_path);
   }
-  define('DIR_FS_INC', DIR_FS_CATALOG.'inc/');
+  if (!defined('DIR_FS_INC')) {
+    define('DIR_FS_INC', DIR_FS_CATALOG.'inc/');
+  }
 
 // include
   //require('../includes/functions/validations.php');
