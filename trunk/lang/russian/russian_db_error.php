c27cd76159f60e3a1fb3c959f64e2089290e24f7
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-06-12 12:31:05 +0000

    Исправление кода в уведомители об ошибках MySQL

--- trunk/lang/russian/russian_db_error.php
+++ trunk/lang/russian/russian_db_error.php
@@ -1,5 +1,16 @@
 <?php
+// Обработка MySQL ошибок
+
 define('DB_ERR_MAIL', 'Администратор <vam@test.com>'); // Укажите e-mail адрес и имя получателя, куда будут тприходить пиьсма с технической информацией, в случае возникновения проблем с MySQL сервером.
 define('DB_ERR_MSG', "<br /><br /><center><font face=\"verdana,tahoma,arial\" size=\"2\" color=\"ff0000\"><b>"
                      ."Интернет-магазин закрыт на техническое обслуживание, заходите позже!</b><font></center>"); // Сообщение, которое будет выводиться при возникновении проблем с MySQL сервером.
+
+define('MYSQL QUERY ERROR_TEXT', 'Проблемы с MySQL');
+define('MYSQL QUERY ERROR_SUBJECT', 'Проблемы с MySQL сервером!');
+define('MYSQL QUERY ERROR_SERVER_NAME', 'Сервер: ');
+define('MYSQL QUERY ERROR_REMOTE_ADDR', 'Адрес: ');
+define('MYSQL QUERY ERROR_REFERER', 'Реферер: ');
+define('MYSQL QUERY ERROR_REQUESTED', 'Страница: ');
+define('MYSQL QUERY ERROR_FROM', 'От: db_error@');
+
 ?>
