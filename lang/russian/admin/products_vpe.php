1d591ecba10995bfb6437ff2dcf1b88e3ffd0793
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-06-13 16:02:16 +0000

    Исправлен перевод

--- lang/russian/admin/products_vpe.php
+++ lang/russian/admin/products_vpe.php
@@ -22,10 +22,10 @@ define('TABLE_HEADING_ACTION', 'Действие');
 
 define('TEXT_INFO_EDIT_INTRO', 'Сделайте необходиме изменения');
 define('TEXT_INFO_PRODUCTS_VPE_NAME', 'Статус заказа:');
-define('TEXT_INFO_INSERT_INTRO', 'Введите новый упаковочный </span title="единица измерения короткомерного баланса, равная 5,66 складочных кубических метра">юнит</span>');
-define('TEXT_INFO_DELETE_INTRO', 'Вы уверены, что хотите удалить этот юнит?');
-define('TEXT_INFO_HEADING_NEW_PRODUCTS_VPE', 'Новый упак. единица');
-define('TEXT_INFO_HEADING_EDIT_PRODUCTS_VPE', 'Редактировать упак. единицу');
-define('TEXT_INFO_HEADING_DELETE_PRODUCTS_VPE', 'Удалить упак. единицу');
-define('ERROR_REMOVE_DEFAULT_PRODUCTS_VPE', 'Ошибка: юнит по умолчанию не может быть удален. Пожалуйста, установите другой юнит по умолчанию и попробуйте еще раз.');
+define('TEXT_INFO_INSERT_INTRO', 'Введите новую упаковочную </span title="единица измерения короткомерного баланса, равная 5,66 складочных кубических метра">единицу</span>');
+define('TEXT_INFO_DELETE_INTRO', 'Вы уверены, что хотите удалить эту единицу?');
+define('TEXT_INFO_HEADING_NEW_PRODUCTS_VPE', 'Новая упаковочная единица');
+define('TEXT_INFO_HEADING_EDIT_PRODUCTS_VPE', 'Редактировать упаковочную единицу');
+define('TEXT_INFO_HEADING_DELETE_PRODUCTS_VPE', 'Удалить упаковочную единицу');
+define('ERROR_REMOVE_DEFAULT_PRODUCTS_VPE', 'Ошибка: единица по умолчанию не может быть удалена. Пожалуйста, установите другую единицу по умолчанию и попробуйте еще раз.');
 ?>
