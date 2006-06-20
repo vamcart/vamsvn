1f2713a21b77a5c25775390048d3d57d9542febf
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-06-20 16:23:52 +0000

    E-mail изменён на /dev/null

--- lang/english/english_db_error.php
+++ lang/english/english_db_error.php
@@ -1,7 +1,7 @@
 <?php
 // Обработка MySQL ошибок
 
-define('DB_ERR_MAIL', 'Administrator <vam@test.com>'); // Укажите e-mail адрес и имя получателя, куда будут тприходить пиьсма с технической информацией, в случае возникновения проблем с MySQL сервером.
+define('DB_ERR_MAIL', 'Administrator </dev/null>'); // Укажите e-mail адрес и имя получателя, куда будут тприходить пиьсма с технической информацией, в случае возникновения проблем с MySQL сервером.
 define('DB_ERR_MSG', "<br /><br /><center><font face=\"verdana,tahoma,arial\" size=\"2\" color=\"ff0000\"><b>"
                      ."MySQL problems!</b><font></center>"); // Сообщение, которое будет выводиться при возникновении проблем с MySQL сервером.
 define('MYSQL QUERY ERROR_TEXT', 'MYSQL QUERY ERROR REPORT');
