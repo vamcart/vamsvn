48840bb2070fa1e6113fea12dce30ca407d63586
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-07-06 14:43:06 +0000

    Исправление ошибки с сессиями, по умолчанию сессии записываются в папку tmp

--- includes/application_top.php
+++ includes/application_top.php
@@ -248,7 +248,7 @@ require (DIR_WS_FUNCTIONS.'sessions.php');
 
 // set the session name and save path
 session_name('XTCsid');
-session_save_path(SESSION_WRITE_DIRECTORY);
+session_save_path(DIR_FS_DOCUMENT_ROOT.SESSION_WRITE_DIRECTORY);
 
 // set the session cookie parameters
 if (function_exists('session_set_cookie_params')) {
