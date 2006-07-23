e7b345268c67ba50ec032f6174cba4bd5ef28b99
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-07-23 17:19:55 +0000

    Добавлен модуль RSS2 каналов

---
+++ trunk/xtc_rss2_info.php
@@ -0,0 +1,25 @@
+<?php
+
+include ('includes/application_top.php');
+
+// create smarty elements
+$smarty = new Smarty;
+
+// include boxes
+require (DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/source/boxes.php');
+
+$breadcrumb->add(NAVBAR_TITLE_RSS2_INFO);
+
+require (DIR_WS_INCLUDES.'header.php');
+
+$smarty->assign('RSS2_INFO', TEXT_RSS2_INFO);
+
+$smarty->assign('language', $_SESSION['language']);
+$smarty->caching = 0;
+$main_content = $smarty->fetch(CURRENT_TEMPLATE.'/module/xtc_rss2_info.html');
+$smarty->assign('main_content', $main_content);
+if (!defined(RM))
+	$smarty->load_filter('output', 'note');
+$smarty->display(CURRENT_TEMPLATE.'/index.html');
+
+?>
