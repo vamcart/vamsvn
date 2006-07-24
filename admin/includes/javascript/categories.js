81bf3119bfb64e6039421f7b7ba62388af3fd616
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-07-24 07:37:10 +0000

    Перевёл на русский текст в js окне

--- admin/includes/javascript/categories.js
+++ admin/includes/javascript/categories.js
@@ -14,7 +14,7 @@ function CheckMultiForm ()
           }
       }
     }
-    alert('Bitte markieren Sie mindestens ein Element!\nPlease check at least one element!');
+    alert('Выделите хотя бы один элемент!\nPlease check at least one element!');
     return false;
   }
 
