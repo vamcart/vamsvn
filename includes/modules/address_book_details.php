ece9bf6882213452c45c6bcc57aa1b7b238d48b5
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-08-02 16:56:30 +0000

    Добавлена поддержка загрузки регионов для выбранной страны через ajax в адресной книге

--- includes/modules/address_book_details.php
+++ includes/modules/address_book_details.php
@@ -90,11 +90,54 @@
 
 
 
-  if (ACCOUNT_STATE == 'true') {
-  $module_smarty->assign('state','1');
-  $module_smarty->assign('INPUT_STATE',$state_input);
-  }
-  }
+if (ACCOUNT_STATE == 'true') {
+	$smarty->assign('state', '1');
+
+    if ($process != true) {
+
+//	    $country = (isset($_POST['country']) ? xtc_db_prepare_input($_POST['country']) : STORE_COUNTRY);
+	    $zone_id = 0;
+		 $check_query = xtc_db_query("select count(*) as total from ".TABLE_ZONES." where zone_country_id = '".(int)$entry['entry_country_id']."'");
+		 $check = xtc_db_fetch_array($check_query);
+		 $entry_state_has_zones = ($check['total'] > 0);
+		 if ($entry_state_has_zones == true) {
+			$zones_array = array ();
+			$zones_query = xtc_db_query("select zone_name from ".TABLE_ZONES." where zone_country_id = '".(int)$entry['entry_country_id']."' order by zone_name");
+			while ($zones_values = xtc_db_fetch_array($zones_query)) {
+				$zones_array[] = array ('id' => $zones_values['zone_name'], 'text' => $zones_values['zone_name']);
+			}
+			
+			$zone = xtc_db_query("select distinct zone_id, zone_name from ".TABLE_ZONES." where zone_country_id = '".(int)$entry['entry_country_id']."' and zone_code = '".xtc_db_input($state)."'");
+
+	      if (xtc_db_num_rows($zone) > 0) {
+	        $zone_id = $zone['zone_id'];
+	        $zone_name = $zone['zone_name'];
+
+	      } else {
+
+		   $zone = xtc_db_query("select distinct zone_id, zone_name from ".TABLE_ZONES." where zone_country_id = '".(int)$entry['entry_country_id']."' and (zone_name like '".xtc_db_input($state)."%' or zone_code like '%".xtc_db_input($state)."%')");
+
+	      if (xtc_db_num_rows($zone) > 0) {
+	          $zone_id = $zone['zone_id'];
+	          $zone_name = $zone['zone_name'];
+	        }
+	      }
+		}
+	}
+
+      if ($entry_state_has_zones == true) {
+        $state_input = xtc_draw_pull_down_menuNote(array ('name' => 'state', 'text' => '&nbsp;'. (xtc_not_null(ENTRY_STATE_TEXT) ? '<span class="inputRequirement">'.ENTRY_STATE_TEXT.'</span>' : '')), $zones_array, $zone_name, ' id="state"');
+
+      } else {
+		 $state_input = xtc_draw_input_fieldNote(array ('name' => 'state', 'text' => '&nbsp;'. (xtc_not_null(ENTRY_STATE_TEXT) ? '<span class="inputRequirement">'.ENTRY_STATE_TEXT.'</span>' : '')), xtc_get_zone_name($entry['entry_country_id'], $entry['entry_zone_id'], $entry['entry_state']), ' id="state"');
+
+      }
+		
+	$module_smarty->assign('INPUT_STATE', $state_input);
+} else {
+	$module_smarty->assign('state', '0');
+}
+}
 
   if ($_POST['country']){
   $selected = $_POST['country'];
@@ -102,10 +145,20 @@
   $selected = $entry['entry_country_id'];
   }
 
-  if (ACCOUNT_COUNTRY == 'true') {
+if (ACCOUNT_COUNTRY == 'true') {
+
   $module_smarty->assign('country','1');
-  $module_smarty->assign('SELECT_COUNTRY',xtc_get_country_list(array('name'=>'country','text'=>'&nbsp;' . (xtc_not_null(ENTRY_COUNTRY_TEXT) ? '<span class="inputRequirement">' . ENTRY_COUNTRY_TEXT . '</span>': '')), $selected));
-  }
+  
+  
+  if ($process == true) $entry['entry_country_id'] = (int)$_POST['country'];
+
+   $module_smarty->assign('SELECT_COUNTRY', xtc_get_country_list('country', $entry['entry_country_id'], 'id="country", onChange="document.getElementById(\'stateXML\').innerHTML = \'' . ENTRY_STATEXML_LOADING . '\';loadXMLDoc(\'loadStateXML\',{country_id: this.value});"') . (xtc_not_null(ENTRY_COUNTRY_TEXT) ? '<span class="alert">' . ENTRY_COUNTRY_TEXT . '</span>': ''));
+
+   $module_smarty->assign('SELECT_COUNTRY_NOSCRIPT', '<noscript><br />' . xtc_image_submit('button_update.gif', IMAGE_BUTTON_UPDATE, 'name=loadStateXML') . '<br />' . ENTRY_STATE_RELOAD . '</noscript>');
+
+} else {
+	$smarty->assign('country', '0');
+}
 
   if ((isset($_GET['edit']) && ($_SESSION['customer_default_address_id'] != $_GET['edit'])) || (isset($_GET['edit']) == false) ) {
   $module_smarty->assign('new','1');
