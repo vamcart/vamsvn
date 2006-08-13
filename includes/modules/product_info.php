8cc24550f3491a551318d7b5064409c502facf3a
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-08-13 17:00:30 +0000

    Исправление в выводе дополнительных картинок

--- includes/modules/product_info.php
+++ includes/modules/product_info.php
@@ -104,13 +104,16 @@ if (!is_object($product) || !$product->isProduct()) { // product not found in da
 		}
 		$info_smarty->assign('PRODUCTS_POPUP_LINK', 'javascript:popupWindow(\''.xtc_href_link(FILENAME_POPUP_IMAGE, 'pID='.$product->data['products_id'].$connector.'imgID=0').'\')');
 		$mo_images = xtc_get_products_mo_images($product->data['products_id']);
-		if ($mo_images != false) {
-			foreach ($mo_images as $img) {
-				$mo_img = DIR_WS_INFO_IMAGES.$img['image_name'];
-				$info_smarty->assign('PRODUCTS_IMAGE_'.$img['image_nr'], $mo_img);
-				$info_smarty->assign('PRODUCTS_POPUP_LINK_'.$img['image_nr'], 'javascript:popupWindow(\''.xtc_href_link(FILENAME_POPUP_IMAGE, 'pID='.$product->data['products_id'].$connector.'imgID='.$img['image_nr']).'\')');
-			}
-		}
+        if ($mo_images != false) {
+    $info_smarty->assign('PRODUCTS_MO_IMAGES', $mo_images);
+            foreach ($mo_images as $img) {
+                $mo_img[] = array(
+                'PRODUCTS_MO_IMAGE' => xtc_image(DIR_WS_INFO_IMAGES . $img['image_name'], '', '', '', 'class="mo_img"'),
+                'PRODUCTS_MO_POPUP_LINK' => xtc_href_link(FILENAME_POPUP_IMAGE, 'pID=' . $product->data['products_id'] . $connector . 'imgID='.$img['image_nr'])
+                );
+        $info_smarty->assign('mo_img', $mo_img);
+            }
+        }
 		//mo_images EOF
 		$discount = 0.00;
 		if ($_SESSION['customers_status']['customers_status_public'] == 1 && $_SESSION['customers_status']['customers_status_discount'] != '0.00') {
