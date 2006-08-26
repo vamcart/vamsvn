c6cb37e23076c26fcfb22a1d3fe353642d0a45b1
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-08-26 17:42:57 +0000

    Добавлено текст сообщения в случае отсутствия новостей

--- trunk/lang/russian/russian.php
+++ trunk/lang/russian/russian.php
@@ -488,5 +488,6 @@ define('TABLE_HEADING_LATEST_NEWS', 'Последние новости');
 define('NAVBAR_TITLE_NEWS', 'Новости');
 
 define('TEXT_DISPLAY_NUMBER_OF_LATEST_NEWS', 'Показано <b>%d</b> - <b>%d</b> (всего <b>%d</b> новостей)');
+define('TEXT_NO_NEWS', 'Нет новостей.');
 
 ?>
