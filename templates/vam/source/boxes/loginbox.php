da29c593e0500829eb648667b49dca7bfa01433e
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-10-07 16:31:06 +0000

    Новый дизайн бокса вход

--- templates/vam/source/boxes/loginbox.php
+++ templates/vam/source/boxes/loginbox.php
@@ -29,10 +29,11 @@ require_once (DIR_FS_INC.'xtc_draw_password_field.inc.php');
 if (!xtc_session_is_registered('customer_id')) {
 
 	$box_smarty->assign('FORM_ACTION', '<form id="loginbox" method="post" action="'.xtc_href_link(FILENAME_LOGIN, 'action=process', 'SSL').'">');
-	$box_smarty->assign('FIELD_EMAIL', xtc_draw_input_field('email_address', '', 'size="25" maxlength="50"'));
-	$box_smarty->assign('FIELD_PWD', xtc_draw_password_field('password', '', 'size="10" maxlength="30"'));
+	$box_smarty->assign('FIELD_EMAIL', xtc_draw_input_field('email_address', '', 'size="15" maxlength="30"'));
+	$box_smarty->assign('FIELD_PWD', xtc_draw_password_field('password', '', 'size="15" maxlength="30"'));
 	$box_smarty->assign('BUTTON', xtc_image_submit('button_login_small.gif', IMAGE_BUTTON_LOGIN));
 	$box_smarty->assign('LINK_LOST_PASSWORD', xtc_href_link(FILENAME_PASSWORD_DOUBLE_OPT, '', 'SSL'));
+	$box_smarty->assign('LINK_NEW_ACCOUNT', xtc_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL'));
 	$box_smarty->assign('FORM_END', '</form>');
 
 	$box_smarty->assign('BOX_CONTENT', $loginboxcontent);
