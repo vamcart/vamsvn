5a9c23880690a45c7d803be33785136f40f7fb87
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-10-13 16:18:23 +0000

    Новая шапка в админке

--- trunk/lang/english/admin/english.php
+++ trunk/lang/english/admin/english.php
@@ -425,7 +425,7 @@ define('BOX_TOOLS_RECOVER_CART', 'Recover Carts');
 
 define('BOX_FEATURED', 'Featured Products');
 
-define('TEXT_HEADER_NEWS','News');
+define('TEXT_HEADER_DEFAULT','Main');
 define('TEXT_HEADER_SUPPORT','Support');
 define('TEXT_HEADER_SHOP','Shop');
 define('TEXT_HEADER_LOGOFF','Logout');
