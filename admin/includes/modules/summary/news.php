c69324e9055d70e3df352e68ba0391985ed10768
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-10-17 09:00:00 +0000

    Новый дизайн заголовков на странице start.php в админке

--- admin/includes/modules/summary/news.php
+++ admin/includes/modules/summary/news.php
@@ -16,7 +16,20 @@ defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
           <tr>
             <td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
 				  <tr> 
-				    <td class="pageHeading"><?php echo TABLE_HEADING_NEWS; ?></td>
+				    <td class="pageHeading">
+				    
+<table width="100%" border="0" cellpadding="0" cellspacing="0">  <tr>
+    <td class="contentBoxHeading1"><img src="images/heading1.gif" border="0" alt="" /></td>
+    <td height="14" class="contentBoxHeading1" width="100%"><?php echo TABLE_HEADING_NEWS; ?></td>
+  </tr>
+  <tr>
+    <td class="line" width="100%" colspan="2"><img src="images/pixel_trans.gif" border="0" alt="" width="1" height="2" /></td>
+  </tr>
+  <tr>
+    <td width="100%" colspan="2"><img src="images/pixel_trans.gif" border="0" alt="" width="1" height="1" /></td>
+  </tr>
+</table>				    
+				    </td>
 				  </tr>
 
               <tr>
