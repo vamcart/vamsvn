57089fc012f149270c6093c52e0c63df9071004e
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-10-18 16:11:42 +0000

    Ссылки на новости прописаны через функцию xtc_href_link

--- trunk/news.php
+++ trunk/news.php
@@ -70,7 +70,8 @@
               'NEWS_HEADING' => $one['headline'],
               'NEWS_CONTENT' => $one['content'],
               'NEWS_ID'      => $one['news_id'],
-        'NEWS_DATA'    => xtc_date_short($one['date_added']),
+              'NEWS_DATA'    => xtc_date_short($one['date_added']),
+              'NEWS_LINK_MORE'    => xtc_href_link(FILENAME_NEWS, 'news_id='.$one['news_id'], 'NONSSL'),
               );
       }
   } else {
