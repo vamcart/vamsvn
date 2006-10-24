d8063af82c4ca219c861566dbfc3a50160374d11
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-10-24 11:30:21 +0000

    Новый шаблон бокса валюты

--- templates/vamshop/source/boxes/currencies.php
+++ templates/vamshop/source/boxes/currencies.php
@@ -46,7 +46,7 @@
   $box_smarty = new smarty;
   $box_smarty->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
   $box_content='';
-  $box_content=xtc_draw_form('currencies', xtc_href_link(basename($PHP_SELF), '', $request_type, false), 'get').xtc_draw_pull_down_menu('currency', $currencies_array, $_SESSION['currency'], 'onChange="this.form.submit();" style="width: 100%"') . $hidden_get_variables . xtc_hide_session_id().'</form>';
+  $box_content=xtc_draw_form('currencies', xtc_href_link(basename($PHP_SELF), '', $request_type, false), 'get').xtc_draw_pull_down_menu('currency', $currencies_array, $_SESSION['currency'], 'onchange="this.form.submit();"') . $hidden_get_variables . xtc_hide_session_id().'</form>';
 
 
   $box_smarty->assign('BOX_CONTENT', $box_content);
