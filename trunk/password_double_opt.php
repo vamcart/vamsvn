e9943ec41238ef1c098994e8e7ac58ae7bdf7370
alexvam <alexvam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-11-03 09:16:15 +0000

    Исправление в выводе случайной картинки

--- trunk/password_double_opt.php
+++ trunk/password_double_opt.php
@@ -131,7 +131,7 @@ switch ($case) {
 		break;
 	case code_error :
 
-		$smarty->assign('VVIMG', '<img src="'.FILENAME_DISPLAY_VVCODES.'" alt="captcha" />');
+		$smarty->assign('VVIMG', '<img src="'.xtc_href_link(FILENAME_DISPLAY_VVCODES).'" alt="captcha" />');
 		$smarty->assign('text_heading', HEADING_PASSWORD_FORGOTTEN);
 		$smarty->assign('info_message', $info_message);
 		$smarty->assign('message', TEXT_PASSWORD_FORGOTTEN);
