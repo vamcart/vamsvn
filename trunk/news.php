41e2956cbf4aebb94140c31cef2778224603b89e
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2006-11-15 07:14:39 +0000

    Не выводилось в новостях время парсинга страниц и количество запросов к базе

--- trunk/news.php
+++ trunk/news.php
@@ -82,5 +82,6 @@
   $smarty->caching = 0;
   if (!defined(RM))
       $smarty->load_filter('output', 'note');
-  $smarty->display(CURRENT_TEMPLATE . '/index.html');
+  $smarty->display(CURRENT_TEMPLATE.'/index.html');
+  include ('includes/application_bottom.php');
 ?>
