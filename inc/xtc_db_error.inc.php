677bf637a8a9fcc204cb29659e76d3ac48ece868
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-01-04 13:49:07 +0000

    Корректировка в коде

--- inc/xtc_db_error.inc.php
+++ inc/xtc_db_error.inc.php
@@ -22,7 +22,7 @@
 function xtc_db_error($query, $errno, $error) {
 // BOF db-error processing
    include(DIR_WS_LANGUAGES . 'russian/russian_db_error.php');
-   $msg = "\n" . 'MYSQL QUERY ERROR REPORT' . "\n" . " - " . date("d/m/Y H:m:s",time()) . "\n" . '---------------------------------------' . "\n";
+   $msg = "\n" . 'MYSQL ERROR REPORT' . "\n" . " - " . date("d/m/Y H:m:s",time()) . "\n" . '---------------------------------------' . "\n";
    $msg .= $errno . ' - ' . $error . "\n\n" . $query . "\n";
    $msg .= '---------------------------------------' . "\n";
    $msg .= 'Server Name   : ' . $_SERVER['SERVER_NAME'] . "\n";
