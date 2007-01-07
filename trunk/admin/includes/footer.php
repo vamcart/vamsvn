28a076a145418833704db9c99f3f7e8cb7eba68b
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-01-07 18:15:53 +0000

    Корректировка даты

--- trunk/admin/includes/footer.php
+++ trunk/admin/includes/footer.php
@@ -34,7 +34,7 @@
   following copyright announcement.
 */
 ?>
-E-Commerce Engine Copyright &copy; 2003 <a href="http://www.oscommerce.com" target="_blank">osCommerce</a> Portions Copyright &copy; 2003 - 2005 <a href="http://xt-commerce.com" target="_blank">xt:Commerce</a>, &copy; 2006-2007 <a href="http://vamshop.ru" target="_blank">VaM Shop</a><br />
+E-Commerce Engine Copyright &copy; 2003 <a href="http://www.oscommerce.com" target="_blank">osCommerce</a> Portions Copyright &copy; 2003 - 2005 <a href="http://xt-commerce.com" target="_blank">xt:Commerce</a>, &copy; 2005-2007 <a href="http://vamshop.ru" target="_blank">VaM Shop</a><br />
 osCommerce provides no warranty and is redistributable under the <a href="http://www.fsf.org/licenses/gpl.txt" target="_blank">GNU General Public License</a><br>
 <a href="http://vamshop.ru" target="_blank">VaM Shop</a> provides no warranty except as to associated support contracts<br />which are limited by and to the Service Level Agreement.<br />
   </td>
