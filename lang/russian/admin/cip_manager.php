a348ff88aa6fbe6fb9eb072abcedb0a506213238
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-01-09 19:04:19 +0000

    Корректировка в переводе

--- lang/russian/admin/cip_manager.php
+++ lang/russian/admin/cip_manager.php
@@ -22,12 +22,16 @@ define('TEXT_FILE_NAME', 'Имя файла:');
 define('TEXT_FILE_SIZE', 'Размер:');
 define('TEXT_FILE_CONTENTS', 'Содержимое:');
 define('TEXT_LAST_MODIFIED', 'Последние изменения:');
-
+define('TEXT_NEW_FOLDER', 'Новая папка');
+define('TEXT_NEW_FOLDER_INTRO', 'Введите название новой папки:');
 define('TEXT_DELETE_INTRO', 'Вы действительно хотите удалить данный файл?');
 define('TEXT_UPLOAD_INTRO', 'Выберите файл для загрузки.');
 
 define('ERROR_DIRECTORY_NOT_WRITEABLE', 'Ошибка: Нет доступа на запись в данную директорию. Установите правильные права доступа на: %s');
 define('ERROR_FILE_NOT_WRITEABLE', 'Ошибка: Нет доступа на запись в данный файл. Установите правильные права доступа на: %s');
+define('ERROR_DIRECTORY_NOT_REMOVEABLE', 'Ошибка: Не могу удалить данную директорию. Установите правильные права доступа на: %s');
+define('ERROR_FILE_NOT_REMOVEABLE', 'Ошибка: Не могу удалить данный файл. Установите правильные права доступа на: %s');
+define('ERROR_DIRECTORY_DOES_NOT_EXIST', 'Ошибка: Директория не найдена: %s');
 //======================
 define('ICON_UNZIP', 'Рахархивировать');
 define('ICON_ZIP', 'Архивировать');
@@ -45,13 +49,13 @@ define('ERROR_FILE_ALREADY_EXISTS','Файл %s  <b>уже существует</b>.');
 
 define('CIP_MANAGER_SUPPORT','Поддержка: ');
 define('CIP_MANAGER_UPLOADER','Модуль добавил: ');
-define('CIP_MANAGER_SUPPORT_FORUM','Форум поддержки данного модуля на официальном сайте VaM Shop');
+define('CIP_MANAGER_SUPPORT_FORUM','Форум поддержки данного модуля на официальном сайте магазина');
 define('CIP_MANAGER_CONTRIBUTION_PAGE','Официальная страница модуля');
 define('CIP_MANAGER_SUPPORT_FORUM_DEVELOPER','Форум поддержки данного модуля на сайте разработчика');
 define('CIP_MANAGER_INFO','Информация о модуле: ');
 define('CIP_MANAGER_INSTALLED','Модуль установлен');
 define('CIP_MANAGER_NOT_INSTALLED','Модуль не был установлен');
-define('CIP_MANAGER_UPLOAD_NOTE','Вы можете загружать <b>только ZIP архивы</b>, <br><b>не более 500Kb</b><br>и <b>только архивы с модулями для VaM Shop</b>.');
+define('CIP_MANAGER_UPLOAD_NOTE','Вы можете загружать <b>только ZIP архивы</b>, <br><b>не более 500Kb</b><br>и <b>только архивы с модулями</b>.');
 define('CIP_MANAGER_XML_NOT_FOUND',' не найден!');
 define('CIP_MANAGER_GENERAL_INFO','Информация о файле: ');
 define('CIP_MANAGER_IMAGE_PREVIEW','Картинка: ');
