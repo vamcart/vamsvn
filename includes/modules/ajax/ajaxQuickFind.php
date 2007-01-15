d82550c0da9bc76c48691c35f32eb1162e2f379e
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-01-15 15:57:07 +0000

    Добавлен в сборку ajax модуль быстрого поиска

---
+++ includes/modules/ajax/ajaxQuickFind.php
@@ -0,0 +1,55 @@
+<?php
+
+//var_dump($_POST);echo '<br>';
+	define("AJAX_QUICKSEARCH_RESULT", 'text'); // dropdown or text
+	define("AJAX_QUICKSEARCH_DROPDOWN_SIZE", 5);
+	define("AJAX_QUICKSEARCH_LIMIT", 15);
+
+	$q = addslashes(preg_replace("%[^0-9a-zA-Zà-ÿÀ-ß]%", "", $_REQUEST['keywords']) );
+
+	$out = "";
+	if(isset($q) && xtc_not_null($q)) {
+
+		$searchwords = explode(" ",$q);
+		$nosearchwords = sizeof($searchwords);
+		foreach($searchwords as $key => $value) {
+			if ($value == '')
+				unset($searchwords[$key]);
+		}
+		$searchwords = array_values($searchwords);
+		$nosearchwords = sizeof($searchwords);
+		foreach($searchwords as $key => $value) {
+			$booltje = '+' . $searchwords[$key] . '*';
+			$searchwords[$key] = $booltje;
+		}
+		$q = implode(" ",$searchwords);
+
+		$products_query = xtc_db_query("select pd.products_id, pd.products_name, p.products_model
+							from " . TABLE_PRODUCTS_DESCRIPTION . " pd
+							inner join " . TABLE_PRODUCTS . " p
+							on (p.products_id = pd.products_id)
+							where (match (pd.products_name) against ('" . $q . "' in boolean mode)
+							or match (p.products_model) against ('" . $q . "' in boolean mode)" .
+							($_REQUEST['search_in_description'] == '1' ? "or match (pd.products_description) against ('" . $q . "' in boolean mode)" : "") . ")
+							and p.products_status = '1'
+							and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'
+							order by pd.products_name asc
+							limit " . AJAX_QUICKSEARCH_LIMIT);
+
+		if($products = xtc_db_fetch_array($products_query)) {
+			$out .= sprintf(TEXT_AJAX_QUICKSEARCH_TOP, AJAX_QUICKSEARCH_LIMIT) . '<br />';
+			$dropdown = array();
+			$out .= '<ul class="ajaxQuickFind">';
+			while($products = xtc_db_fetch_array($products_query)) {
+				$out .= '<li class="ajaxQuickFind"><a href="' . xtc_href_link(FILENAME_PRODUCT_INFO, xtc_product_link($products['products_id'], $products['products_name']), 'NONSSL', false) . '">' . $products['products_name'] . '</a></li>' . "\n";
+				$dropdown[] = array('id' => $products['products_id'],
+														'text' => $products['products_name']);
+			}
+			$out .= '</ul>' . "\n";
+			if(AJAX_QUICKSEARCH_RESULT == 'dropdown') {
+				$out .= xtc_draw_pull_down_menu('AJAX_QUICKSEARCH_pid', $dropdown, '', 'onChange="this.form.submit();" size="' . AJAX_QUICKSEARCH_DROPDOWN_SIZE . '" class="ajaxQuickFind"') . xtc_hide_session_id();
+			}
+		}
+	}
+	$_RESULT['ajaxQuickFind'] = $out;
+?>
