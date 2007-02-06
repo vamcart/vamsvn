49cd5c546f91524a0823b53b4f20afde41c4e2fd
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-02-06 17:38:58 +0000

    Изменён copyright

--- trunk/includes/functions/sessions.php
+++ trunk/includes/functions/sessions.php
@@ -1,16 +1,18 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: sessions.php 1195 2005-08-28 21:10:52Z mz $   
+   $Id: sessions.php 1195 2007-02-06 02:40:57 VaM $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2007 VaM Shop
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(sessions.php,v 1.16 2003/04/02); www.oscommerce.com 
    (c) 2003	 nextcommerce (sessions.php,v 1.5 2003/08/13); www.nextcommerce.org 
+   (c) 2004	 xt:Commerce (sessions.php,v 1.5 2003/08/13); xt-commerce.com 
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
