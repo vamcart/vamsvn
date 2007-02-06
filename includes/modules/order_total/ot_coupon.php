1def107493791d722efc47be27ea154a12cd7bb6
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-02-06 18:05:49 +0000

    Изменён copyright

--- includes/modules/order_total/ot_coupon.php
+++ includes/modules/order_total/ot_coupon.php
@@ -1,16 +1,17 @@
 <?php
-
 /* -----------------------------------------------------------------------------------------
-   $Id: ot_coupon.php 1322 2005-10-27 13:58:22Z mz $
+   $Id: ot_coupon.php 1322 2007-02-06 21:01:37 VaM $
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2007 VaM Shop
    -----------------------------------------------------------------------------------------
    based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(ot_coupon.php,v 1.1.2.37.3); www.oscommerce.com
+   (c) 2004 xt:Commerce (ot_coupon.php,v 1.1.2.37.3); xt-commerce.com
 
    Released under the GNU General Public License
    -----------------------------------------------------------------------------------------
--- includes/modules/order_total/ot_shipping.php
+++ includes/modules/order_total/ot_shipping.php
@@ -1,16 +1,18 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: ot_shipping.php 1002 2005-07-10 16:11:37Z mz $   
+   $Id: ot_shipping.php 1002 2007-02-06 21:01:37 VaM $
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2007 VaM Shop
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(ot_shipping.php,v 1.15 2003/02/07); www.oscommerce.com 
    (c) 2003	 nextcommerce (ot_shipping.php,v 1.13 2003/08/24); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (ot_shipping.php,v 1.13 2003/08/24); xt-commerce.com
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
