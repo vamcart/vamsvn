7b280974b702f2d30d1830dce8c280e5f0a886ea
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-02-06 18:28:50 +0000

    Изменён copyright

--- trunk/includes/modules/shipping/dhl.php
+++ trunk/includes/modules/shipping/dhl.php
@@ -1,16 +1,18 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: dhl.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id: dhl.php 899 2007-02-06 21:19:57 VaM $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2007 VaM Shop
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(dhl.php,v 1.02 2003/02/18); www.oscommerce.com 
    (c) 2003	 nextcommerce (dhl.php,v 1.10 2003/08/24); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (dhl.php,v 1.10 2003/08/24); xt-commerce.com
 
    Released under the GNU General Public License 
    -----------------------------------------------------------------------------------------
--- trunk/includes/modules/shipping/item.php
+++ trunk/includes/modules/shipping/item.php
@@ -1,16 +1,18 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: item.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id: item.php 899 2007-02-06 21:19:57 VaM $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2007 VaM Shop
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(item.php,v 1.39 2003/02/05); www.oscommerce.com 
    (c) 2003	 nextcommerce (item.php,v 1.7 2003/08/24); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (item.php,v 1.7 2003/08/24); xt-commerce.com
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
