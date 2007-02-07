554530172477faef261b03a1e08dd1b048e5d8e7
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-02-07 09:23:16 +0000

    Изменён copyright

--- trunk/jscript/jscript_ajax.js
+++ trunk/jscript/jscript_ajax.js
@@ -1,23 +1,18 @@
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-// $Id: jscript_ajax.js,v 1.0.0 17.03.2006 11:43 Andrew Berezin $
-//
+/* -----------------------------------------------------------------------------------------
+   $Id: jscript_ajax.js 899 2007-02-06 20:14:56 VaM $   
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2006	 Andrew Berezin (jscript_ajax.js,v 1.1 2007/03/17); zen-cart.com 
+
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
 function loadXMLDoc(ajax_page, put_vars, caching) {
   var url="./index_ajax.php?ajax_page=" + ajax_page;
   var req = new Subsys_JsHttpRequest_Js();
