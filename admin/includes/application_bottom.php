8b1eea6b8a88f88cdcc576f622e29117963ed312
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-02-08 09:13:53 +0000

    Изменён copyright

--- admin/includes/application_bottom.php
+++ admin/includes/application_bottom.php
@@ -1,16 +1,18 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: application_bottom.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id: application_bottom.php 899 2007-02-08 12:90:57 VaM $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2007 VaM Shop
    --------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(application_bottom.php,v 1.8 2002/03/15); www.oscommerce.com 
    (c) 2003	 nextcommerce (application_bottom.php,v 1.6 2003/08/1); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (application_bottom.php,v 1.6 2003/08/1); xt-commerce.com
 
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
