64fce81e0fd70003f4c9a07bc381a02025dd347c
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-03-09 11:28:55 +0000

    Добавлены файлы модуля Article Manager для админки

---
+++ admin/articles_config.php
@@ -0,0 +1,172 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: articles_config.php 1125 2007-03-09 11:13:01Z VaM $   
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(configuration.php,v 1.40 2002/12/29); www.oscommerce.com 
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+  require('includes/application_top.php');
+
+$gID = 26;
+
+  if ($_GET['action']) {
+    switch ($_GET['action']) {
+      case 'save':
+
+          $configuration_query = xtc_db_query("select configuration_key,configuration_id, configuration_value, use_function,set_function from " . TABLE_CONFIGURATION . " where configuration_group_id = '" . (int)$gID . "' order by sort_order");
+
+          while ($configuration = xtc_db_fetch_array($configuration_query))
+              xtc_db_query("UPDATE ".TABLE_CONFIGURATION." SET configuration_value='".$_POST[$configuration['configuration_key']]."' where configuration_key='".$configuration['configuration_key']."'");
+
+               xtc_redirect(FILENAME_ARTICLES_CONFIG. '?gID=' . (int)$gID);
+        break;
+
+    }
+  }
+
+  $cfg_group_query = xtc_db_query("select configuration_group_title from " . TABLE_CONFIGURATION_GROUP . " where configuration_group_id = '" . (int)$gID . "'");
+  $cfg_group = xtc_db_fetch_array($cfg_group_query);
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>"> 
+<title><?php echo TITLE; ?></title>
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+<script type="text/javascript" src="includes/general.js"></script>
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF" onload="SetFocus();">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="2" cellpadding="2">
+  <tr>
+        <td width="100%">
+
+    <h1 class="contentBoxHeading"><?php echo BOX_CONFIGURATION; ?></h1>
+  
+  </td>
+  </tr>
+  <tr>
+<!-- body_text //-->
+    <td class="boxCenter" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+      <tr>
+        <td class="main"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td valign="top" align="right">
+            
+<?php echo xtc_draw_form('configuration', FILENAME_ARTICLES_CONFIG, 'gID=' . (int)$gID . '&action=save'); ?>
+            <table width="100%"  border="0" cellspacing="0" cellpadding="4">
+<?php
+  $configuration_query = xtc_db_query("select configuration_key,configuration_id, configuration_value, use_function,set_function from " . TABLE_CONFIGURATION . " where configuration_group_id = '" . (int)$gID . "' order by sort_order");
+
+  while ($configuration = xtc_db_fetch_array($configuration_query)) {
+    if ($gID == 6) {
+      switch ($configuration['configuration_key']) {
+        case 'MODULE_PAYMENT_INSTALLED':
+          if ($configuration['configuration_value'] != '') {
+            $payment_installed = explode(';', $configuration['configuration_value']);
+            for ($i = 0, $n = sizeof($payment_installed); $i < $n; $i++) {
+              include(DIR_FS_CATALOG_LANGUAGES . $language . '/modules/payment/' . $payment_installed[$i]);
+            }
+          }
+          break;
+
+        case 'MODULE_SHIPPING_INSTALLED':
+          if ($configuration['configuration_value'] != '') {
+            $shipping_installed = explode(';', $configuration['configuration_value']);
+            for ($i = 0, $n = sizeof($shipping_installed); $i < $n; $i++) {
+              include(DIR_FS_CATALOG_LANGUAGES . $language . '/modules/shipping/' . $shipping_installed[$i]);			
+            }
+          }
+          break;
+
+        case 'MODULE_ORDER_TOTAL_INSTALLED':
+          if ($configuration['configuration_value'] != '') {
+            $ot_installed = explode(';', $configuration['configuration_value']);
+            for ($i = 0, $n = sizeof($ot_installed); $i < $n; $i++) {
+              include(DIR_FS_CATALOG_LANGUAGES . $language . '/modules/order_total/' . $ot_installed[$i]);			
+            }
+          }
+          break;
+      }
+    }
+    if (xtc_not_null($configuration['use_function'])) {
+      $use_function = $configuration['use_function'];
+      if (ereg('->', $use_function)) {
+        $class_method = explode('->', $use_function);
+        if (!is_object(${$class_method[0]})) {
+          include(DIR_WS_CLASSES . $class_method[0] . '.php');
+          ${$class_method[0]} = new $class_method[0]();
+        }
+        $cfgValue = xtc_call_function($class_method[1], $configuration['configuration_value'], ${$class_method[0]});
+      } else {
+        $cfgValue = xtc_call_function($use_function, $configuration['configuration_value']);
+      }
+    } else {
+      $cfgValue = $configuration['configuration_value'];
+    }
+
+    if (((!$_GET['cID']) || (@$_GET['cID'] == $configuration['configuration_id'])) && (!$cInfo) && (substr($_GET['action'], 0, 3) != 'new')) {
+      $cfg_extra_query = xtc_db_query("select configuration_key,configuration_value, date_added, last_modified, use_function, set_function from " . TABLE_CONFIGURATION . " where configuration_id = '" . $configuration['configuration_id'] . "'");
+      $cfg_extra = xtc_db_fetch_array($cfg_extra_query);
+
+      $cInfo_array = xtc_array_merge($configuration, $cfg_extra);
+      $cInfo = new objectInfo($cInfo_array);
+    }
+    if ($configuration['set_function']) {
+        eval('$value_field = ' . $configuration['set_function'] . '"' . htmlspecialchars($configuration['configuration_value']) . '");');
+      } else {
+        $value_field = xtc_draw_input_field($configuration['configuration_key'], $configuration['configuration_value'],'size=40');
+      }
+   // add
+
+   if (strstr($value_field,'configuration_value')) $value_field=str_replace('configuration_value',$configuration['configuration_key'],$value_field);
+
+   echo '
+  <tr>
+    <td width="300" valign="top" class="dataTableContent"><b>'.constant(strtoupper($configuration['configuration_key'].'_TITLE')).'</b> <a class="info" href="#">? <span class="help">'.constant(strtoupper( $configuration['configuration_key'].'_DESC')).'<!--[if lte IE 6.5]><iframe frameborder="0"></iframe><![endif]--></span></a></td>
+    <td valign="top" class="dataTableContent">
+    <table width="100%"  border="0" cellspacing="0" cellpadding="2">
+      <tr>
+        <td class="main">'.$value_field.'</td>
+      </tr>
+    </table>
+</td>
+  </tr>
+  ';
+
+  }
+?>
+            </table>
+<?php echo '<input type="submit" class="button" onClick="this.blur();" value="' . BUTTON_SAVE . '"/>'; ?></form>
+            </td>
+
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br />
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
