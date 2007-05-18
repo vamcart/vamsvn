68eecf22fba393dec62b2e258ac2e40ecf7c67bd
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-05-18 06:59:50 +0000

    Не записывалось в таблицу значение entry_zone_id пре регистрации покупателя

--- checkout_shipping_address.php
+++ checkout_shipping_address.php
@@ -125,32 +125,32 @@ if (isset ($_POST['action']) && ($_POST['action'] == 'submit')) {
 		}
      }
 
-//		if (ACCOUNT_STATE == 'true') {
-//			$zone_id = 0;
-//			$check_query = xtc_db_query("select count(*) as total from ".TABLE_ZONES." where zone_country_id = '".(int) $country."'");
-//			$check = xtc_db_fetch_array($check_query);
-//			$entry_state_has_zones = ($check['total'] > 0);
-//			if ($entry_state_has_zones == true) {
-//				$zone_query = xtc_db_query("select distinct zone_id from ".TABLE_ZONES." where zone_country_id = '".(int) $country."' and (zone_name like '".xtc_db_input($state)."%' or zone_code like '%".xtc_db_input($state)."%')");
-//				if (xtc_db_num_rows($zone_query) > 1) {
-//					$zone_query = xtc_db_query("select distinct zone_id from ".TABLE_ZONES." where zone_country_id = '".(int) $country."' and zone_name = '".xtc_db_input($state)."'");
-//				}
-//				if (xtc_db_num_rows($zone_query) >= 1) {
-//					$zone = xtc_db_fetch_array($zone_query);
-//					$zone_id = $zone['zone_id'];
-//				} else {
-//					$error = true;
-//
-//					$messageStack->add('create_account', ENTRY_STATE_ERROR_SELECT);
-//				}
-//			} else {
-//				if (strlen($state) < ENTRY_STATE_MIN_LENGTH) {
-//					$error = true;
-//
-//					$messageStack->add('checkout_address', ENTRY_STATE_ERROR);
-//				}
-//			}
-//		}
+		if (ACCOUNT_STATE == 'true') {
+			$zone_id = 0;
+			$check_query = xtc_db_query("select count(*) as total from ".TABLE_ZONES." where zone_country_id = '".(int) $country."'");
+			$check = xtc_db_fetch_array($check_query);
+			$entry_state_has_zones = ($check['total'] > 0);
+			if ($entry_state_has_zones == true) {
+				$zone_query = xtc_db_query("select distinct zone_id from ".TABLE_ZONES." where zone_country_id = '".(int) $country."' and (zone_name like '".xtc_db_input($state)."%' or zone_code like '%".xtc_db_input($state)."%')");
+				if (xtc_db_num_rows($zone_query) > 1) {
+					$zone_query = xtc_db_query("select distinct zone_id from ".TABLE_ZONES." where zone_country_id = '".(int) $country."' and zone_name = '".xtc_db_input($state)."'");
+				}
+				if (xtc_db_num_rows($zone_query) >= 1) {
+					$zone = xtc_db_fetch_array($zone_query);
+					$zone_id = $zone['zone_id'];
+				} else {
+					$error = true;
+
+					$messageStack->add('create_account', ENTRY_STATE_ERROR_SELECT);
+				}
+			} else {
+				if (strlen($state) < ENTRY_STATE_MIN_LENGTH) {
+					$error = true;
+
+					$messageStack->add('checkout_address', ENTRY_STATE_ERROR);
+				}
+			}
+		}
 
 		if ((is_numeric($country) == false) || ($country < 1)) {
 			$error = true;
