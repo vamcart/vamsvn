b0165123578061afcedbad0b9b22275f8cae4446
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-05-22 15:42:34 +0000

    Исправлена ошибка в выводе заголовка на странице настройки атрибутов

--- trunk/admin/new_attributes.php
+++ trunk/admin/new_attributes.php
@@ -64,7 +64,7 @@
 <!-- body_text //-->
     <td class="boxCenter" width="100%" valign="top">
     
-    <h1 class="contentBoxHeading"><?php echo HEADING_TITLE; ?></h1>
+    <h1 class="contentBoxHeading"><?php echo TITLE_EDIT; ?></h1>
     
     <table border="0" width="100%" cellspacing="0" cellpadding="2">
 <?php
