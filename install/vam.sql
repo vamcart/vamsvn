adcbe59f65e3a0f59a0266f9be2781b50839e09b
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-07-03 09:45:28 +0000

    Добавлен новый модуль - safe login, добавляется каптча на страницу login.php в случае если пользователь X раз неправильно указал e-mail и/или пароль

--- install/vam.sql
+++ install/vam.sql
@@ -454,6 +454,8 @@ CREATE TABLE customers (
   customers_last_modified datetime DEFAULT '0000-00-00 00:00:00',
   orig_reference text,
   login_reference text,
+  login_tries char(2) NOT NULL default '0',
+  login_time datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY (customers_id)
 );
 
@@ -1635,6 +1637,8 @@ INSERT INTO configuration (configuration_id,  configuration_key, configuration_v
 INSERT INTO configuration (configuration_id,  configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES   ('', 'DISPLAY_REVOCATION_ON_CHECKOUT', 'true', 17, 13, NULL, '', NULL, 'vam_cfg_select_option(array(\'true\', \'false\'),');
 INSERT INTO configuration (configuration_id,  configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES   ('', 'REVOCATION_ID', '', 17, 14, NULL, '2003-12-05 05:01:41', NULL, NULL);
 INSERT INTO configuration (configuration_id,  configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES   ('', 'USE_EP_IMAGE_MANIPULATOR', 'false', 17, 15, NULL, '', NULL, 'vam_cfg_select_option(array(\'true\', \'false\'),');
+INSERT INTO configuration (configuration_id,  configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('', 'LOGIN_NUM', '3', '17', '16', NULL, '0000-00-00 00:00:00', NULL, NULL);
+INSERT INTO configuration (configuration_id,  configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('', 'LOGIN_TIME', '300',  '17', '17', NULL, '0000-00-00 00:00:00', NULL, NULL);
 
 #configuration_group_id 18
 INSERT INTO configuration (configuration_id,  configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES   ('', 'ACCOUNT_COMPANY_VAT_CHECK', 'false', 18, 4, '', '', NULL, 'vam_cfg_select_option(array(\'true\', \'false\'),');
--- login.php
+++ login.php
@@ -45,21 +45,39 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 	$password = vam_db_prepare_input($_POST['password']);
 
 	// Check if email exists
-	$check_customer_query = vam_db_query("select customers_id, customers_vat_id, customers_firstname,customers_lastname, customers_gender, customers_password, customers_email_address, customers_default_address_id from ".TABLE_CUSTOMERS." where customers_email_address = '".vam_db_input($email_address)."' and account_type = '0'");
+	$check_customer_query = vam_db_query("select customers_id, customers_vat_id, customers_firstname,customers_lastname, customers_gender, customers_password, customers_email_address, login_tries, login_time, customers_default_address_id from ".TABLE_CUSTOMERS." where customers_email_address = '".vam_db_input($email_address)."' and account_type = '0'");
 	if (!vam_db_num_rows($check_customer_query)) {
 		$_GET['login'] = 'fail';
 		$info_message = TEXT_NO_EMAIL_ADDRESS_FOUND;
 	} else {
 		$check_customer = vam_db_fetch_array($check_customer_query);
+
+// Check the login is blocked while login_tries is more than 5 and blocktime is not over
+	$blocktime = LOGIN_TIME; 	 																 			// time to block the login in seconds
+	$time = time();  																				// time now as a timestamp
+	$logintime = strtotime($check_customer['login_time']);  // conversion from the ISO date format to a timestamp
+	$difference = $time - $logintime; 											// The difference time in seconds between the last login and now
+  if ($check_customer['login_tries'] >= LOGIN_NUM and $difference < $blocktime) {
+		// Action for böse ?
+    require_once (DIR_FS_INC.'vam_random_charcode.inc.php');
+    $vlcode = vam_random_charcode(32);
+    $smarty->assign('VVIMG', '<img src="'.FILENAME_DISPLAY_VVCODES.'" alt="" />');    
+    $smarty->assign('INPUT_CODE', vam_draw_input_field('vvcode', '', 'size="6" maxlength="6"', 'text', false));
+    if ($_POST['vvcode'] == $_SESSION['vvcode']){
+    // code ok
 		// Check that password is good
 		if (!vam_validate_password($password, $check_customer['customers_password'])) {
 			$_GET['login'] = 'fail';
+      // Login tries + 1
+		  vam_db_query("update ".TABLE_CUSTOMERS." SET login_tries = login_tries+1, login_time = now() WHERE customers_email_address = '".vam_db_input($email_address)."'");		
 			$info_message = TEXT_LOGIN_ERROR;
 		} else {
 			if (SESSION_RECREATE == 'True') {
 				vam_session_recreate();
 			}
-
+      // Login tries = 0			$date_now = date('Ymd');
+		  vam_db_query("update ".TABLE_CUSTOMERS." SET login_tries = 0, login_time = now() WHERE customers_email_address = '".vam_db_input($email_address)."'");		
+		  
 			$check_country_query = vam_db_query("select entry_country_id, entry_zone_id from ".TABLE_ADDRESS_BOOK." where customers_id = '".(int) $check_customer['customers_id']."' and address_book_id = '".$check_customer['customers_default_address_id']."'");
 			$check_country = vam_db_fetch_array($check_country_query);
 
@@ -72,7 +90,7 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 			$_SESSION['customer_country_id'] = $check_country['entry_country_id'];
 			$_SESSION['customer_zone_id'] = $check_country['entry_zone_id'];
 
-			$date_now = date('Ymd');
+
 
 			vam_db_query("update ".TABLE_CUSTOMERS_INFO." SET customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1 WHERE customers_info_id = '".(int) $_SESSION['customer_id']."'");
 			vam_write_user_info((int) $_SESSION['customer_id']);
@@ -86,6 +104,53 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 			}
 
 		}
+    }else{
+    // code falsch
+    $info_message = TEXT_WRONG_CODE;
+    // Login tries + 1
+		vam_db_query("update ".TABLE_CUSTOMERS." SET login_tries = login_tries+1, login_time = now() WHERE customers_email_address = '".vam_db_input($email_address)."'");		
+    }		
+	} else {
+		// Check that password is good
+		if (!vam_validate_password($password, $check_customer['customers_password'])) {
+			$_GET['login'] = 'fail';
+      // Login tries + 1
+		  vam_db_query("update ".TABLE_CUSTOMERS." SET login_tries = login_tries+1, login_time = now() WHERE customers_email_address = '".vam_db_input($email_address)."'");		
+			$info_message = TEXT_LOGIN_ERROR;
+		} else {
+			if (SESSION_RECREATE == 'True') {
+				vam_session_recreate();
+			}
+      // Login tries = 0			$date_now = date('Ymd');
+		  vam_db_query("update ".TABLE_CUSTOMERS." SET login_tries = 0, login_time = now() WHERE customers_email_address = '".vam_db_input($email_address)."'");		
+		  
+			$check_country_query = vam_db_query("select entry_country_id, entry_zone_id from ".TABLE_ADDRESS_BOOK." where customers_id = '".(int) $check_customer['customers_id']."' and address_book_id = '".$check_customer['customers_default_address_id']."'");
+			$check_country = vam_db_fetch_array($check_country_query);
+
+			$_SESSION['customer_gender'] = $check_customer['customers_gender'];
+			$_SESSION['customer_first_name'] = $check_customer['customers_firstname'];
+			$_SESSION['customer_last_name'] = $check_customer['customers_lastname'];
+			$_SESSION['customer_id'] = $check_customer['customers_id'];
+			$_SESSION['customer_vat_id'] = $check_customer['customers_vat_id'];
+			$_SESSION['customer_default_address_id'] = $check_customer['customers_default_address_id'];
+			$_SESSION['customer_country_id'] = $check_country['entry_country_id'];
+			$_SESSION['customer_zone_id'] = $check_country['entry_zone_id'];
+
+
+
+			vam_db_query("update ".TABLE_CUSTOMERS_INFO." SET customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1 WHERE customers_info_id = '".(int) $_SESSION['customer_id']."'");
+			vam_write_user_info((int) $_SESSION['customer_id']);
+			// restore cart contents
+			$_SESSION['cart']->restore_contents();
+
+			if ($_SESSION['cart']->count_contents() > 0) {
+				vam_redirect(vam_href_link(FILENAME_SHOPPING_CART, '', 'SSL'));
+			} else {
+				vam_redirect(vam_href_link(FILENAME_DEFAULT));
+			}
+
+		}
+	 }
 	}
 }
 
