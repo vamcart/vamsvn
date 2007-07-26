5127530138377ad0c77ac9a66e882bf989160875
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-07-26 15:13:29 +0000

    Версия 1.32

--- install/vam.sql
+++ install/vam.sql
@@ -1330,7 +1330,7 @@ create table cip_depend (
 );
 
 #database Version
-INSERT INTO database_version(version) VALUES ('1.3.1');
+INSERT INTO database_version(version) VALUES ('1.32');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
