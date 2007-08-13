3e0c6850ee06317b01ae8eea863e89bfe83c4217
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-08-13 08:48:33 +0000

    Версия 1.33

--- trunk/install/vam.sql
+++ trunk/install/vam.sql
@@ -1330,7 +1330,7 @@ create table cip_depend (
 );
 
 #database Version
-INSERT INTO database_version(version) VALUES ('1.32');
+INSERT INTO database_version(version) VALUES ('1.33');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
