d059f2b69fdf1c616ee769008d68771839428291
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-09-03 16:04:57 +0000

    Добавлены новые классы vam_data, vam_form, vam_localization, vam_mail и vam_template, будут использоваться в будущих версиях

---
+++ includes/classes/vam_form.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once (HOME_DIR . '/includes/external/form/class.php');
+
+class vamForm extends form {
+
+}
+
+$vamForm = new vamForm;
+
+?>
---
+++ includes/classes/vam_template.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once (HOME_DIR . '/includes/external/smarty/Smarty.class.php');
+
+class vamTemplate extends Smarty {
+
+   function vamTemplate()
+   {
+
+        $this->Smarty();
+
+        $this->template_dir = HOME_DIR . '/templates/test/';
+        $this->compile_dir = HOME_DIR . '/templates_c/';
+        $this->config_dir   = HOME_DIR . '/configs/';
+        $this->cache_dir    = HOME_DIR . '/cache/';
+        $this->plugins_dir = array(
+        HOME_DIR . '/includes/external/smarty/plugins',
+        HOME_DIR . '/includes/external/smarty/plugins_vam',
+        );
+
+        $this->assign('app_name', 'vamTemplate');
+   }
+
+}
+
+$vamTemplate = new vamTemplate;
+
+?>
