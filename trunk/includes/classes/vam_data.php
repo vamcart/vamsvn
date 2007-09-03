eba65fa721684e5f62917ed0de01e4084ad0a405
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-09-03 16:04:57 +0000

    Добавлены новые классы vam_data, vam_form, vam_localization, vam_mail и vam_template, будут использоваться в будущих версиях

---
+++ trunk/includes/classes/vam_data.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once (HOME_DIR . '/includes/external/adodb/adodb.inc.php');
+
+$dbdriver = 'mysql';
+$charset = 'utf-8';
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'vam-cms';
+
+$vamData = ADONewConnection($dbdriver);
+$vamData->debug = false;
+$vamData->charset = $charset;
+//$vamData->cacheSecs = 3600*24;
+$vamData->Connect($server, $user, $password, $database);
+
+?>
