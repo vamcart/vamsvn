94d8521da84cf0f6df35f345181f725f99321cc5
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-09-04 10:58:30 +0000

    Корректировка в классе vamTemplate

--- includes/classes/vam_template.php
+++ includes/classes/vam_template.php
@@ -14,8 +14,8 @@ class vamTemplate extends Smarty {
         $this->config_dir   = DIR_FS_CATALOG . 'lang';
         $this->cache_dir    = DIR_FS_CATALOG . 'cache';
         $this->plugins_dir = array(
-        DIR_WS_INCLUDES . 'includes/external/smarty/plugins',
-        DIR_WS_INCLUDES . 'includes/external/smarty/plugins_vam',
+        DIR_WS_INCLUDES . 'external/smarty/plugins',
+        DIR_WS_INCLUDES . 'external/smarty/plugins_vam',
         );
 
         $this->assign('app_name', 'vamTemplate');
