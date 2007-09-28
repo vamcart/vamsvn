f26bbd43748f843ebf49e08fdad982ef402d9714
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-09-28 15:23:55 +0000

    Корректировка в коде функции vam_set_customer_status_upgrade

--- trunk/inc/vam_set_customer_status_upgrade.inc.php
+++ trunk/inc/vam_set_customer_status_upgrade.inc.php
@@ -20,6 +20,8 @@
 //set customer satus to new customer for upgrade account
 function vam_set_customer_status_upgrade($customer_id){
 
+global $customer_notified;
+
 if ( ($_SESSION['customer_status_value']['customers_status_id'] == "' . DEFAULT_CUSTOMERS_STATUS_ID_NEWSLETTER .'" ) AND ($_SESSION['customer_status_value']['customers_is_newsletter'] == 0 ) ) {
   vam_db_query("update " . TABLE_CUSTOMERS . " set customers_status = '" . DEFAULT_CUSTOMERS_STATUS_ID . "' where customers_id = '" . $_SESSION['customer_id'] . "'");
   vam_db_query("insert into " . TABLE_CUSTOMERS_STATUS_HISTORY . " (customers_id, new_value, old_value, date_added, customer_notified) values ('" . $_SESSION['customer_id'] . "', '" . DEFAULT_CUSTOMERS_STATUS_ID . "', '" . DEFAULT_CUSTOMERS_STATUS_ID_NEWSLETTER . "', now(), '" . $customer_notified . "')");
