967d216852ce165d278bc74a3a476e31581bf7de
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-10-13 13:52:10 +0000

    Корректировка в коде

--- trunk/includes/external/smarty/plugins_vam/block.translate.php
+++ trunk/includes/external/smarty/plugins_vam/block.translate.php
@@ -1,4 +1,19 @@
 <?php
+/* -----------------------------------------------------------------------------------------
+   $Id: block.translate.php 899 2007-10-13 20:14:56 VaM $   
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2006	 John (AZTEK) Downey (block.translate.php,v 1.1 2003/03/17); 
+
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+   
 $xml = array();
 
 function smarty_block_translate($params, $string, &$smarty) {
