f4d3019140efd186fed52742f47f5816e875dd54
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-10-29 16:32:27 +0000

    Добавлена новая каптча - kcaptcha с captcha.ru

---
+++ captcha.php
@@ -0,0 +1,30 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+  $Id: captcha.php 831 2007-10-29 19:20:03 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2007 KCAPTCHA - author Kruglov Sergei; captcha.ru 
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+require ('includes/application_top.php');
+require_once (DIR_WS_CLASSES.'kcaptcha.php');
+
+if(isset($_REQUEST[session_name()])){
+	session_start();
+}
+
+$captcha = new KCAPTCHA();
+
+if($_REQUEST[session_name()]){
+	$_SESSION['captcha_keystring'] = $captcha->getKeyString();
+}
+
+?>
