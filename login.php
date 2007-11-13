aaf60de4141f4c900a9a068efdc96912424d4491
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-11-13 17:07:29 +0000

    Удалён неиспользуемый код со страницы login.php

--- login.php
+++ login.php
@@ -59,8 +59,6 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
 	$difference = $time - $logintime; 											// The difference time in seconds between the last login and now
   if ($check_customer['login_tries'] >= LOGIN_NUM and $difference < $blocktime) {
 		// Action for bцse ?
-    require_once (DIR_FS_INC.'vam_random_charcode.inc.php');
-    $vlcode = vam_random_charcode(32);
     $vamTemplate->assign('CAPTCHA_IMG', '<img src="'.FILENAME_DISPLAY_CAPTCHA.'" alt="captcha" />');    
     $vamTemplate->assign('CAPTCHA_INPUT', vam_draw_input_field('captcha', '', 'size="6" maxlength="6"', 'text', false));
     if ($_POST['captcha'] == $_SESSION['captcha_keystring']){
