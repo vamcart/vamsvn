a6461f82aa1c009f61d8584fbd037776bb4bbfb5
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-12-15 10:01:40 +0000

    Исправлена ошибка в модуле доставки selfpickup, не учитывались зоны

--- includes/modules/shipping/selfpickup.php
+++ includes/modules/shipping/selfpickup.php
@@ -28,6 +28,9 @@ class selfpickup
 
     function selfpickup()
     {
+
+      global $order;
+
         $this->code        = 'selfpickup';
         $this->title       = MODULE_SHIPPING_SELFPICKUP_TEXT_TITLE;
         $this->description = MODULE_SHIPPING_SELFPICKUP_TEXT_DESCRIPTION;
@@ -38,7 +41,7 @@ class selfpickup
 
       if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_SELFPICKUP_ZONE > 0) ) {
         $check_flag = false;
-        $check_query = vam_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_FLAT_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
+        $check_query = vam_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_SELFPICKUP_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
         while ($check = vam_db_fetch_array($check_query)) {
           if ($check['zone_id'] < 1) {
             $check_flag = true;
