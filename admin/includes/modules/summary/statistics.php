266d4fedc80820223bbc5fd11d1d4b5f33945d52
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2007-12-15 19:44:59 +0000

    Корректировка в закладке статистика на главной странице админки

--- admin/includes/modules/summary/statistics.php
+++ admin/includes/modules/summary/statistics.php
@@ -28,13 +28,13 @@ defined('_VALID_VAM') or die('Direct Access to this location is not allowed.');
 				  </tr>
 
               <tr>
-                <td class="dataTableContentRss" valign="top" width="60%">
+                <td class="dataTableContentRss" valign="top" width="50%">
 <?php
 include(DIR_WS_CLASSES . 'ofc-library/open_flash_chart_object.php');
 open_flash_chart_object( '100%', 250, vam_href_link('chart_data.php', vam_get_all_get_params(), 'NONSSL'), false );
 ?>
                 </td>
-                <td class="dataTableContentRss" valign="top" width="35%">
+                <td class="dataTableContentRss" valign="top" width="45%">
 <?php
 open_flash_chart_object( '100%', 250, vam_href_link('chart_data.php', 'report_type=orders', 'NONSSL'), false );
 ?>
