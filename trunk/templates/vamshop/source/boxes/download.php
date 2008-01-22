8baf489f96603a2f8e245ecfc7006395ddce0359
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-01-22 13:02:02 +0000

    Добавлено SQL кэширование в бокс Мои загрузки

--- trunk/templates/vamshop/source/boxes/download.php
+++ trunk/templates/vamshop/source/boxes/download.php
@@ -12,8 +12,8 @@ $box->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
   if (!strstr($_SERVER['SCRIPT_NAME'], FILENAME_ACCOUNT_HISTORY_INFO)) {
 // Get last order id for checkout_success
     $orders_query_raw = "SELECT orders_id FROM " . TABLE_ORDERS . " WHERE customers_id = '" . $_SESSION['customer_id'] . "' ORDER BY orders_id DESC LIMIT 1";
-    $orders_query = vam_db_query($orders_query_raw);
-    $orders_values = vam_db_fetch_array($orders_query);
+    $orders_query = vamDBquery($orders_query_raw);
+    $orders_values = vam_db_fetch_array($orders_query,true);
     $last_order = $orders_values['orders_id'];
   } else {
     $last_order = $_GET['order_id'];
@@ -28,12 +28,12 @@ $box->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
                           AND op.orders_id = '" . $last_order . "'
                           AND opd.orders_products_id=op.orders_products_id
                           AND opd.orders_products_filename<>''";
-  $downloads_query = vam_db_query($downloads_query_raw);
+  $downloads_query = vamDBquery($downloads_query_raw);
 
 // Don't display if there is no downloadable product
-  if (vam_db_num_rows($downloads_query) > 0) {
+  if (vam_db_num_rows($downloads_query,true) > 0) {
 
-    while ($downloads_values = vam_db_fetch_array($downloads_query)) {
+    while ($downloads_values = vam_db_fetch_array($downloads_query,true)) {
 
 // MySQL 3.22 does not have INTERVAL
     	list($dt_year, $dt_month, $dt_day) = explode('-', $downloads_values['date_purchased_day']);
