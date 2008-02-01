1439714b3e465bd773f31d67b96b205ddef67452
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-02-01 16:49:18 +0000

    Изменён бокс Разделы, теперь категории выводятся списком, также в настройках /templates/vamshop/soruce/boxes/categories.php можно разворачивать дерево категорий

--- trunk/templates/vamshop/source/inc/vam_show_category.inc.php
+++ trunk/templates/vamshop/source/inc/vam_show_category.inc.php
@@ -15,76 +15,220 @@
    (c) 2004	 xt:Commerce (vam_show_category.inc.php,v 1.4 2003/08/13); xt-commerce.com 
 
    Released under the GNU General Public License 
-   ---------------------------------------------------------------------------------------*/
+   --------------------------------------------------------------------------------------- 
+	GUNNART "SHOW_CATEGORY ADVANCED"
+	
+	erweiterte Kategorien-Navigation fьr xt:Commerce 3.04 SP1 / SP2.1
 
-     function vam_show_category($counter) {
-    global $foo, $categories_string, $id;
+ 	Proudly togetherfummeled by Gunnar Tillmann
+ 	http://www.gunnart.de
+ 	Version 0.3 Beta / 12. Juli 2007 
+ 	- Bugfix 1, Danke fьr's Testen, Sandi 
+ 	- Bugfix 2, Danke fьr den Hinweis, Krischan 
+   --------------------------------------------------------------------
 
+   
+   --------------------------------------------------------------------
+	1) BESONDERHEITEN
+	
+	Mit dieser neuen Kategorien-Navigation fьr xt:Commerce kann man 
+	a) Leere Kategorien verstecken
+	b) Bestimmen, wie weit der Kategorien-Baum von Anfang an 
+	   "ausgeklappt" sein soll
+	c) Festlegen, ob sich der Kategorien-Baum trotz einer festgelegten
+	   "Beschrдnkung" unterhalb von aktiven Kategorien weiter
+	   ausklappen kann
+	
+	Die HTML-Ausgabe erfolgt als hierarchische Liste
+   --------------------------------------------------------------------
 
-    // image for first level
-    $img_1='';
+   
+   --------------------------------------------------------------------
+	2) ANLEITUNG / KONFIGURATION
+	
+	Sie kцnnen die Navigation nach Ihren Wьnschen anpassen. 
+	In den Zeilen 96 bis 98 finden Sie 3 Variablen, mit denen das 
+	Verhalten der Kategorien-Liste bestimmt werden kann.
+   --------------------------------------------------------------------
+	a) "TIEFE" DER NAVIGATION BESTIMMEN 
 
-    for ($a=0; $a<$foo[$counter]['level']; $a++) {
+		$MaxLevel 	=	Zahl von 1 bis 5 oder false
+	
+		false:		=	Alle Kategorie-Ebenen werden angezeigt
+		3:			=	Es werden alle Kategorien bis einschlieЯlich  
+						der "dritten Ebene" gezeigt
+   --------------------------------------------------------------------
+	b) LEERE KATEGORIEN VERSTECKEN:
+	
+		$HideEmpty	=	true oder false
+	
+		true		= 	Leere Kategorien werden nicht angezeigt
+		false		= 	Leere Kategorien werden angezeigt
+   --------------------------------------------------------------------
+	c) "UNTERKATEGORIEN AKTIVER KATEGORIEN" ANZEIGEN:
+	
+		$ShowAktSub	=	true oder false
+	
+		true		= 	Wenn man seine Kategorien z.B. bis zur zweiten 
+						Ebene "aufklappt" und dann eine Kategorie der 
+						zweiten Ebene anwдhlt, werden die enthaltenen
+						Unterkategorien der dritten Ebene angezeigt
+		false		= 	Es werden NUR die Kategorie-Ebenen angezeigt, 
+						die man mit $MaxLevel bestimmt hat
+   --------------------------------------------------------------------
 
-      if ($foo[$counter]['level']=='1') {
-      $categories_string .= "";
-      }
+   
+   --------------------------------------------------------------------
+	BITTE BEACHTEN SIE: 
 
-      $categories_string .= "";
+	1) Der Einsatz dieser Kategorien-Navi geschieht auf eigene Gefahr! 
+	2) Jegliche Haftung fьr Schдden an Ihrem System oder Verdienst-
+	Ausfдlle wird abgelehnt 
+	3) Ich empfehle dringend, diese Erweiterung VOR einem produktiven
+	Einsatz zunдchst an einem Test-System auszuprobieren!
+   --------------------------------------------------------------------
 
-    }
-    if ($foo[$counter]['level']=='') {
-    if (strlen($categories_string)=='0') {
-    $categories_string .='';
-    } else {
-    $categories_string .='';
-    }
+   
+   --------------------------------------------------------------------
+	IN EIGENER SACHE:
+	
+	Diese Funktionen wurden kostenlos und ohne Anspruch auf Gegen-
+	leistung zur Verfьgung gestellt. Ich bitte allerdings darum, den 
+	Autoren-Hinweis intakt zu lassen bzw. bei Bearbeitungen mit zu 
+	ьbernehmen. 
+	
+	Ein Backlink zu meiner Website http://www.gunnart.de wьrde mich 
+	natьrlich auch freuen.
+	
+	Vielen Dank
+	
+	Gunnar Tillmann 
+   --------------------------------------------------------------------
+	Feedback is Welcome: http://www.gunnart.de?p=311
+   --------------------------------------------------------------------
+*/
 
-    $categories_string .= $img_1;
-    $categories_string .= '<div class="boxMenuCategory"><a href="';
-    } else {
-    $categories_string .= '<div class="boxMenuSubCategory'.($foo[$counter]['level']+1).'"><a href="'; 
-    }
+// KONFIGURATION
+global $MaxLevel, $HideEmpty, $ShowAktSub;
 
-	$cPath_new=vam_category_link($counter,$foo[$counter]['name']);
-  
-    $categories_string .= vam_href_link(FILENAME_DEFAULT, $cPath_new);
-    $categories_string .= '">';
+	$MaxLevel = 1;
+	$HideEmpty = false;
+	$ShowAktSub = true;
 
-    if ( ($id) && (in_array($counter, $id)) ) {
-// Выделенная категория
-      $categories_string .= '<span>';
-    }
+function vam_show_category($cid, $level, $foo, $cpath) {
 
-    // display category name
-    $categories_string .= $foo[$counter]['name'];
+	global $old_level, $categories_string; //, $HTTP_GET_VARS; // Brauchen wir nicht
+	global $MaxLevel, $HideEmpty, $ShowAktSub;
 
-    if ( ($id) && (in_array($counter, $id)) ) {
-// /Выделенная категория
-      $categories_string .= '</span>';
-    }
+	// 1) Ьberprьfen, ob Kategorie Produkte enthдlt
+	$Empty = true;
+	$pInCat = vam_count_products_in_category($cid);
+	if ($pInCat > 0)
+		$Empty = false;
+	
+	// 2) Ьberprьfen, ob Kategorie gezeigt werden soll
+	$Show = false;
+	if ($HideEmpty) {
+		if (!$Empty)
+			$Show = true;
+	} else {
+		$Show = true;
+	}
 
-    if (SHOW_COUNTS == 'true') {
-      $products_in_category = vam_count_products_in_category($counter);
-      if ($products_in_category > 0) {
-        $categories_string .= '&nbsp;(' . $products_in_category . ')';
-      }
-    }
-
-    if ($foo[$counter]['level']=='') {
-    $categories_string .= '</a></div>';
-    } else {
-    $categories_string .= '</a></div>';
-    }
-
-// Окончание строки каждой категории
-    $categories_string .= "\n";
-
-    if ($foo[$counter]['next_id']) {
-        vam_show_category($foo[$counter]['next_id']);
-    } else {
-        $categories_string .= '';
-    }
-  }
-
+	// 3) Ьberprьfen, ob Unterkategorien gezeigt werden sollen
+	$ShowSub = false;
+	if ($MaxLevel) {
+		if ($level < $MaxLevel)
+			$ShowSub = true;
+	} else {
+		$ShowSub = true;
+	}
+				
+	if($Show) { // Wenn Kategorie gezeigt werden soll ....
+	
+		if ($cid != 0) {
+			
+			// 24.06.2007 BugFix
+			// Auf "product_info"-Seiten wurde Kategorie nicht erkannt 
+			// $category_path = explode('_',$HTTP_GET_VARS['cPath']);
+			$category_path = explode('_',$GLOBALS['cPath']); 
+			$in_path = in_array($cid, $category_path);
+			$this_category = array_pop($category_path);
+		
+			for ($a = 0; $a < $level; $a++)                           ;
+			
+			// Produktzдhlung
+			$ProductsCount = false;
+			// Lange gerдtselt, aber das ist tatsдchlich 
+			// ein String und kein Boolean.                                                                                
+			if (SHOW_COUNTS == 'true') 
+				$ProductsCount = ' <em>(' . $pInCat . ')</em>';	
+                                                  
+			// Aktiv - Nicht Aktiv
+			$Aktiv = false;
+			if ($this_category == $cid) 
+				// Wenn Kategorie aktiv ist
+				$Aktiv = ' Current'; 
+			elseif ($in_path) 
+				// Wenn Oberkategorie aktiv ist
+				$Aktiv = ' CurrentParent'; 
+	
+			// Hat ein SubMenue - hat kein SubMenue
+			// CSS-Klasse festlegen
+			$SubMenue = false;
+			if (vam_has_category_subcategories($cid)) 
+				$SubMenue = " SubMenue";
+			
+			// Listenpunkt
+			// CSS-Klasse festlegen
+			$MainStyle = 'CatLevel'.$level;
+			
+			// Quelltext einrьcken
+			$Tabulator = str_repeat("\t",$level-1);
+	
+			// Navigations-Liste ist jetzt hierarchisch!
+			if($old_level) { 
+				if ($old_level < $level) {
+					$Pre = "\n<ul>";
+					$Pre = str_replace("\n","\n".$Tabulator, $Pre)."\n";
+				} else {
+					$Pre = "</li>\n";
+					if ($old_level > $level) {
+						// Listenpunkte schlieЯen
+						// Quelltext einrьcken
+						for ($counter = 0; $counter < $old_level - $level; $counter++) {
+							$Pre .= str_repeat("\t", $old_level - $counter -1)."</ul>\n".str_repeat("\t", $old_level - $counter- 2)."</li>\n";
+						}
+					}
+				} 
+			}
+				
+			// Listenpunkte zusammensetzen
+			$categories_string .=	$Pre.$Tabulator.
+									'<li class="'.$MainStyle.$SubMenue.$Aktiv.'">'.
+									// Bugfix, 12. Juli 2007
+									//'<a href="' . vam_href_link(FILENAME_DEFAULT, 'cPath=' . $cpath . $cid) . '">'.
+									'<a href="' . vam_href_link(FILENAME_DEFAULT, vam_category_link($cid, $foo[$cid]['name']) ) . '">'.
+									$foo[$cid]['name'].$ProductsCount.
+									'</a>';
+		}
+		
+		// fьr den nдchsten Durchgang ...
+		$old_level = $level;
+	
+		// Unterkategorien durchsteppen
+		foreach ($foo as $key => $value) {
+	
+			if ($foo[$key]['parent'] == $cid) {
+					
+				// Sollen Unterkategorien gezeigt werden?
+				if ($ShowAktSub && $Aktiv)
+					$ShowSub = true;
+				
+				if ($ShowSub) 
+					vam_show_category($key, $level+1, $foo, ($level != 0 ? $cpath . $cid . '_' : ''));
+			} 
+		}
+	} // Ende if($Show)
+} 		
 ?>
