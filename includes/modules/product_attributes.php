41f296ba6bffd6c72b19224c78bdf823fdd8f077
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-02-11 11:32:55 +0000

    Добавлен шаблон для разных типов атрибутов

--- includes/modules/product_attributes.php
+++ includes/modules/product_attributes.php
@@ -28,7 +28,7 @@ $module->assign('tpl_path', 'templates/'.CURRENT_TEMPLATE.'/');
 
 
 if ($product->getAttributesCount() > 0) {
-	$products_options_name_query = vamDBquery("select distinct popt.products_options_id, popt.products_options_name from ".TABLE_PRODUCTS_OPTIONS." popt, ".TABLE_PRODUCTS_ATTRIBUTES." patrib where patrib.products_id='".$product->data['products_id']."' and patrib.options_id = popt.products_options_id and popt.language_id = '".(int) $_SESSION['languages_id']."' order by popt.products_options_name");
+	$products_options_name_query = vamDBquery("select distinct popt.products_options_id, popt.products_options_name,popt.products_options_type,popt.products_options_length,popt.products_options_rows,popt.products_options_size from ".TABLE_PRODUCTS_OPTIONS." popt, ".TABLE_PRODUCTS_ATTRIBUTES." patrib where patrib.products_id='".$product->data['products_id']."' and patrib.options_id = popt.products_options_id and popt.language_id = '".(int) $_SESSION['languages_id']."' order by popt.products_options_name");
 
 	$row = 0;
 	$col = 0;
@@ -37,9 +37,20 @@ if ($product->getAttributesCount() > 0) {
 		$selected = 0;
 		$products_options_array = array ();
 
-		$products_options_data[$row] = array ('NAME' => $products_options_name['products_options_name'], 'ID' => $products_options_name['products_options_id'], 'DATA' => '');
+		$products_options_data[$row] = array (
+		
+		'NAME' => $products_options_name['products_options_name'],
+		'TYPE'=>$products_options_name['products_options_type'],
+		'ROWS'=>$products_options_name['products_options_rows'],
+		'LENGTH'=>$products_options_name['products_options_length'],
+		'SIZE'=>$products_options_name['products_options_size'], 
+		'ID' => $products_options_name['products_options_id'], 
+		'DATA' => ''
+		
+		);
+
 		$products_options_query = vamDBquery("select pov.products_options_values_id,
-		                                                 pov.products_options_values_name,
+		                                                 pov.products_options_values_name,pov.products_options_values_description,
 		                                                 pa.attributes_model,
 		                                                 pa.options_values_price,
 		                                                 pa.price_prefix,
@@ -59,44 +70,43 @@ if ($product->getAttributesCount() > 0) {
 				$products_options_data[$row]['DATA'][$col] = array (
 				
 				'ID' => $products_options['products_options_values_id'], 
-				'TEXT' => $products_options['products_options_values_name'], 
+				'TEXT' => $products_options['products_options_values_name'],
+				'DESCRIPTION' => $products_options['products_options_values_description'], 
 				'MODEL' => $products_options['attributes_model'], 
 				'PRICE' => '', 
 				'FULL_PRICE' => '', 
 				'PREFIX' => $products_options['price_prefix']
 				
 				);
+			
+				$price = '';
+				$full_price = '';
 			} else {
 				if ($products_options['options_values_price'] != '0.00') {
-//					$price = $vamPrice->GetPrice($product->data['products_id'], $format = false, 1, $product->data['products_tax_class_id'], $products_options['options_values_price']);
-					$price = $vamPrice->Format($products_options['options_values_price'], false, $product->data['products_tax_class_id'],true);
+					$price = $vamPrice->Format($products_options['options_values_price'], false, $product->data['products_tax_class_id']);
 				}
-
 				$products_price = $vamPrice->GetPrice($product->data['products_id'], $format = false, 1, $product->data['products_tax_class_id'], $product->data['products_price']);
 				if ($_SESSION['customers_status']['customers_status_discount_attributes'] == 1 && $products_options['price_prefix'] == '+')
-					$price -= $price / 100 * $discount;
-					
+					$price -= $price / 100 * $discount;				
 					$attr_price=$price;
 					if ($products_options['price_prefix']=="-") $attr_price=$price*(-1);
-					$full = $products_price + $attr_price;
-				$products_options_data[$row]['DATA'][$col] = array (
-				
-				'ID' => $products_options['products_options_values_id'], 
-				'TEXT' => $products_options['products_options_values_name'], 
-				'MODEL' => $products_options['attributes_model'], 
-				'PRICE' => $vamPrice->Format($price, true), 
-				'FULL_PRICE' => $vamPrice->Format($full, true), 
-				'PREFIX' => $products_options['price_prefix']
-				
-				);
-
-				//if PRICE for option is 0 we don't need to display it
-				if ($price == 0) {
-					unset ($products_options_data[$row]['DATA'][$col]['PRICE']);
-					unset ($products_options_data[$row]['DATA'][$col]['PREFIX']);
-				}
-
+					$full_price = $products_price + $attr_price;
+					$price = $vamPrice->Format($price, true);
+					$full_price = $vamPrice->Format($full_price, true);
 			}
+			
+			$products_options_data[$row]['DATA'][$col] = array (
+			
+			'ID' => $products_options['products_options_values_id'], 
+			'TEXT' => $products_options['products_options_values_name'],
+			'DESCRIPTION' => $products_options['products_options_values_description'], 
+			'MODEL' => $products_options['attributes_model'], 
+			'PRICE' => $price, 
+			'FULL_PRICE' => $full_price, 'PREFIX' => $products_options['price_prefix']
+			
+			);
+			
+			
 			$col ++;
 		}
 		$row ++;
