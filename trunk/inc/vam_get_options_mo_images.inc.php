107f7c735d0547b3957220f1d6d0dc28f6b93e64
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-02-11 09:35:38 +0000

    Добавлены новые файлы необходимые для модуля типы атрибутов

---
+++ trunk/inc/vam_get_options_mo_images.inc.php
@@ -0,0 +1,30 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: vam_get_options_mo_images.inc.php 1101 2008-02-07 17:36:57 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2006	 xt:Commerce (xtc_get_options_mo_images.inc.php,v 1.4 2003/08/1); xt-commerce.com
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+function vam_get_options_mo_images($id = '') {
+	$mo_query = "select * from " . TABLE_PRODUCTS_OPTIONS_IMAGES . " where products_options_values_id = '" . $id . "' ORDER BY image_nr";
+
+	$products_mo_images_query = vam_db_query($mo_query);
+
+	while ($row = vam_db_fetch_array($products_mo_images_query, true))
+		$results[$row['image_nr']-1] = $row;
+	if (is_array($results)) {
+		return $results;
+	} else {
+		return false;
+	}
+}
+?>
