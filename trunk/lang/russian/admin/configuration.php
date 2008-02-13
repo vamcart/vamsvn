e10cc9b5db4aa32a6f4d0ba46284ece53c3f2bf9
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-02-13 17:18:04 +0000

    Корректировка в переводе админки

--- trunk/lang/russian/admin/configuration.php
+++ trunk/lang/russian/admin/configuration.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: configuration.php 1286 2008-02-13 17:36:57 VaM $
+   $Id: configuration.php 1286 2008-02-13 17:36:58 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
