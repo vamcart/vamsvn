58b95b67042f57f1753688f2d1f574faf8c72f15
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-02-17 12:47:27 +0000

    Корректировка в тексте ошибки mysql - /lang/russian/russian_db_error.php

--- trunk/lang/russian/russian_db_error.php
+++ trunk/lang/russian/russian_db_error.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: russian_db_error.php 1260 2007/02/07 13:24:46 VaM $
+   $Id: russian_db_error.php 1260 2008/02/07 13:24:46 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
@@ -15,7 +15,7 @@
 
 define('DB_ERR_MAIL', 'Администратор </dev/null>'); // Укажите E-Mail адрес и имя получателя, куда будут приходить письма с технической информацией, в случае возникновения проблем с MySQL сервером.
 define('DB_ERR_MSG', "<br /><br /><center><font face=\"verdana,tahoma,arial\" size=\"2\" color=\"ff0000\"><b>"
-                     ."Интернет-магазин закрыт на техническое обслуживание, заходите позже!</b><font></center>"); // Сообщение, которое будет выводиться при возникновении проблем с MySQL сервером.
+                     ."Интернет-магазин закрыт на техническое обслуживание, заходите позже!</b></font><br /><br /><a href=\"http://vamshop.ru/support/modules/smartfaq/faq.php?faqid=32\" target=\"_blank\"><u>Проблема с MySQL. Как узнать причину ошибки. </u></a></center>"); // Сообщение, которое будет выводиться при возникновении проблем с MySQL сервером.
 
 define('MYSQL QUERY ERROR_TEXT', 'Проблемы с MySQL');
 define('MYSQL QUERY ERROR_SUBJECT', 'Проблемы с MySQL сервером!');
