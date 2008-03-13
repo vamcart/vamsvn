8d2e76bf15629ad94add0c786b2e9a174972196a
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-03-13 09:04:23 +0000

    Исправлена ошибка в SQL кэшировании модуля статьи

--- trunk/article_info.php
+++ trunk/article_info.php
@@ -34,11 +34,11 @@ require (DIR_WS_INCLUDES.'header.php');
 
   $article_check_query = "select count(*) as total from " . TABLE_ARTICLES . " a, " . TABLE_ARTICLES_DESCRIPTION . " ad where a.articles_status = '1' and a.articles_id = '" . (int)$_GET['articles_id'] . "' and ad.articles_id = a.articles_id and ad.language_id = '" . (int)$_SESSION['languages_id'] . "'";
   $article_check_query = vamDBquery($article_check_query);
-  $article_check = vam_db_fetch_array($article_check_query);
+  $article_check = vam_db_fetch_array($article_check_query, true);
 
     $article_info_query = "select a.articles_id, a.articles_date_added, a.articles_date_available, a.authors_id, ad.articles_name, ad.articles_description, ad.articles_url, au.authors_name from " . TABLE_ARTICLES . " a left join " . TABLE_AUTHORS . " au on a.authors_id = au.authors_id, " . TABLE_ARTICLES_DESCRIPTION . " ad where a.articles_status = '1' and a.articles_id = '" . (int)$_GET['articles_id'] . "' and ad.articles_id = a.articles_id and ad.language_id = '" . (int)$_SESSION['languages_id'] . "'";
     $article_info_query = vamDBquery($article_info_query);
-    $article_info = vam_db_fetch_array($article_info_query);
+    $article_info = vam_db_fetch_array($article_info_query, true);
 
 if ($article_check['total'] > 0) {
 
