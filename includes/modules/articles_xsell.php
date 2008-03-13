9cfdb92833a9fd15383a8b6c864f806ef876d5db
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-03-13 09:04:23 +0000

    Исправлена ошибка в SQL кэшировании модуля статьи

--- includes/modules/articles_xsell.php
+++ includes/modules/articles_xsell.php
@@ -20,7 +20,7 @@
 if ($_GET['articles_id']) {
 
 $xsell_query = vamDBquery("select distinct a.products_id, a.products_fsk18, ad.products_name, ad.products_short_description, a.products_image, a.products_price, a.products_vpe, a.products_quantity, a.products_vpe_status, a.products_vpe_value, a.products_tax_class_id, a.products_date_added from " . TABLE_ARTICLES_XSELL . " ax, " . TABLE_PRODUCTS . " a, " . TABLE_PRODUCTS_DESCRIPTION . " ad where ax.articles_id = '" . $_GET['articles_id'] . "' and ax.xsell_id = a.products_id and a.products_id = ad.products_id and ad.language_id = '" . $_SESSION['languages_id'] . "' and a.products_status = '1' order by ax.sort_order asc limit " . MAX_DISPLAY_ALSO_PURCHASED);
-$num_products_xsell = vam_db_num_rows($xsell_query); 
+$num_products_xsell = vam_db_num_rows($xsell_query, true); 
 if ($num_products_xsell >= MIN_DISPLAY_ALSO_PURCHASED) {
 
 $module_content = array ();
