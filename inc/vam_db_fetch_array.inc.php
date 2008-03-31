f5331aace814f406b81bba4ab750f85b5f4be084
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-03-31 10:05:10 +0000

    Корректировка в SQL кэшировании

--- inc/vam_db_fetch_array.inc.php
+++ inc/vam_db_fetch_array.inc.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: vam_db_fetch_array.inc.php 864 2007-02-07 10:51:57 VaM $
+   $Id: vam_db_fetch_array.inc.php 864 2008-02-07 10:51:57 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
