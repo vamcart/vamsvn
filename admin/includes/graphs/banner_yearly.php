dc966969b9443673223bf6934d04f27928f80e54
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-04-09 14:44:15 +0000

    Исправлена ошибка с неправильной кодировкой в статистике по баннерам

--- admin/includes/graphs/banner_yearly.php
+++ admin/includes/graphs/banner_yearly.php
@@ -38,7 +38,7 @@
 
   $graph->SetPlotBorderType('left');
   $graph->SetTitleFontSize('4');
-  $graph->SetTitle(sprintf(TEXT_BANNERS_YEARLY_STATISTICS, $banner['banners_title']));
+  $graph->SetTitle('');
 
   $graph->SetBackgroundColor('white');
 
