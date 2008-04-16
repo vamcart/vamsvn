73288cadf48664a6c48c65e0b89bc15fd551c225
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-04-16 13:49:49 +0000

    Подключён html-редактор на страницу управления faq в админке

--- admin/faq.php
+++ admin/faq.php
@@ -124,7 +124,7 @@
           </tr>
           <tr>
             <td class="main"><?php echo TEXT_FAQ_ANSWER; ?>:</td>
-            <td class="main"><?php echo vam_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . vam_draw_textarea_field('answer', '', '100%', '25', stripslashes($faq['answer'])); ?></td>
+            <td class="main"><?php echo vam_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . vam_draw_textarea_field('answer', '', '100%', '25', stripslashes($faq['answer'])); ?><br /><a href="javascript:toggleHTMLEditor('content');"><?php echo vam_image(DIR_WS_IMAGES . 'icon_popup.gif', TEXT_TOGGLE_EDITOR); ?></a></td>
           </tr>
           <tr>
             <td colspan="2"><?php echo vam_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
