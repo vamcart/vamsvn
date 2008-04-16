34fa7efe80a1dd5dbc82271c0ea555e597c65c14
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-04-16 15:45:29 +0000

    Добавлен новый модуль - вопросы и ответы

--- trunk/inc/vam_wysiwyg_tiny.inc.php
+++ trunk/inc/vam_wysiwyg_tiny.inc.php
@@ -78,6 +78,64 @@ function toggleHTMLEditor(id) {
                         	   	</script>';
                         break;
 
+                // WYSIWYG editor faq textarea named faq
+                case 'faq':
+                        $val ='<script type="text/javascript" src="'.$js_src.'"></script>
+                        	   <script type="text/javascript">
+tinyMCE.init({
+		mode : "none",
+      editor_deselector : "notinymce",
+		theme : "advanced",
+		language : "'.$lang.'",
+		plugins : "table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,paste,preview,zoom,flash,searchreplace,print,contextmenu",
+	   theme_advanced_layout_manager : "RowLayout",
+	   theme_advanced_containers : "top1,top2,top3,top4,mceEditor,mceElementpath",
+	   theme_advanced_containers_default_class : "mceToolbar",
+	   theme_advanced_containers_default_align : "center",
+	   theme_advanced_container_top1_align : "left",
+	   theme_advanced_container_top2_align : "left",
+	   theme_advanced_container_top3_align : "left",
+	   theme_advanced_container_top4_align : "left",
+	   theme_advanced_container_top1 : "save,separator,bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,undo,redo,cut,copy,paste,pastetext,pasteword,selectall,separator,code,removeformat,cleanup,search,replace,separator,sub,sup",
+	   theme_advanced_container_top2 : "outdent,indent,bullist,numlist,separator,charmap,hr,visualaid,image,separator,link,unlink,anchor,separator,tablecontrols",
+	   theme_advanced_container_top3 : "styleselect,formatselect,separator,fontselect,fontsizeselect",
+	   theme_advanced_container_top4 : "print,emotions,iespell,flash,advhr,fullscreen,ltr,rtl,forecolor,backcolor",
+	   theme_advanced_container_top1_class : "mceToolbarTop",
+	   theme_advanced_container_top2_class : "mceToolbarTop",
+	   theme_advanced_container_top3_class : "mceToolbarTop",
+	   theme_advanced_container_top4_class : "mceToolbarTop",
+	   theme_advanced_container_mceElementpath_class : "mcePathBottom",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_path_location : "bottom",
+	   theme_advanced_resize_horizontal : true,
+	   theme_advanced_resizing : true,	
+		plugin_insertdate_dateFormat : "%Y-%m-%d",
+		plugin_insertdate_timeFormat : "%H:%M:%S",
+		extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+	   paste_create_paragraphs : false,
+	   paste_create_linebreaks : false,
+	   paste_use_dialog : true,
+	   paste_convert_middot_lists : false,
+	   paste_unindented_list_class : "unindentedList",
+	   paste_convert_headers_to_strong : true,
+		external_link_list_url : "example_data/example_link_list.js",
+		external_image_list_url : "example_data/example_image_list.js",
+		flash_external_list_url : "example_data/example_flash_list.js"  
+});
+
+function toggleHTMLEditor(id) {
+  var elm = document.getElementById(id);
+
+  if (tinyMCE.getInstanceById(id) == null) {
+    tinyMCE.execCommand("mceAddControl", false, id);
+  } else {
+    tinyMCE.execCommand("mceRemoveControl", false, id);
+  }
+}
+                        	   	</script>';
+                        break;
+
                 // WYSIWYG editor latest news textarea named articles_description
                 case 'articles_description':
                         $val ='<script type="text/javascript" src="'.$js_src.'"></script>
