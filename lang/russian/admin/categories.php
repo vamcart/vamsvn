60e6c316c4264986e5881f202d97e45d1fbb36d0
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-05-08 10:12:35 +0000

    Корректировка в переводе

--- lang/russian/admin/categories.php
+++ lang/russian/admin/categories.php
@@ -1,6 +1,6 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: categories.php 1249 2007-02-07 17:36:57 VaM $
+   $Id: categories.php 1249 2008-02-07 17:36:57 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
