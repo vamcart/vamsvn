60e8c29245a8cca01dde318b7d761ef49560a43b
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-05-16 14:28:41 +0000

    Убран заголовок с главной страницы админки

--- trunk/admin/start.php
+++ trunk/admin/start.php
@@ -64,8 +64,6 @@ require ('includes/application_top.php');
 <!-- body_text //-->
     <td class="boxCenter" width="100%" valign="top">
     
-    <h1 class="contentBoxHeading"><?php echo HEADING_TITLE; ?></h1>
-    
     <table border="0" width="100%" cellspacing="0" cellpadding="2">
       <tr>
         <td colspan="2">
