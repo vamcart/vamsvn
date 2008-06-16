5bcb58148fefa8945072dc09a04fb5333eaddfb1
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-06-16 08:09:38 +0000

    Корректировка SQL запроса на странице скидок, добавлено поле описание товара в SQL запрос

--- specials.php
+++ specials.php
@@ -39,6 +39,7 @@ if (GROUP_CHECK == 'true') {
 $specials_query_raw = "select p.products_id,
                                 pd.products_name,
                                 pd.products_short_description,
+                                pd.products_description,
                                 p.products_price,
                                 p.products_tax_class_id,p.products_shippingtime,
                                 p.products_image,p.products_vpe_status,p.products_vpe_value,p.products_vpe,p.products_fsk18,
