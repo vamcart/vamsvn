adee96f8a40c941d916accab3d0dc83aaadb8329
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-07-03 07:55:54 +0000

    Корректировка в сортировке по производителю

--- includes/modules/default.php
+++ includes/modules/default.php
@@ -362,7 +362,7 @@ elseif ($category_depth == 'products' || $_GET['manufacturers_id']) {
     $manufacturer_dropdown .= vam_draw_hidden_field(vam_session_name(), vam_session_id());
     while ($filterlist = vam_db_fetch_array($filterlist_query, true)) {
     $options[] = array ('id' => $filterlist['id'], 'text' => $filterlist['name']);
-    $manufacturer_sort .= '<a href="'.vam_href_link(FILENAME_DEFAULT, 'cPath='.$cPath.'&filter_id='.$filterlist['id']).'">' . $filterlist['name'] . '</a> ';
+    $manufacturer_sort .= '<a href="'.vam_href_link(FILENAME_DEFAULT, 'cat='.$cat.'&filter_id='.$filterlist['id']).'">' . $filterlist['name'] . '</a> ';
     }
     $manufacturer_sort .= '<a href="'.vam_href_link(FILENAME_DEFAULT, 'cPath='.$cPath).'">' . TEXT_ALL_MANUFACTURERS . '</a> ';
     $manufacturer_dropdown .= vam_draw_pull_down_menu('filter_id', $options, $_GET['filter_id'], 'onchange="this.form.submit()"');
