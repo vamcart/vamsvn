59ad5e71236a9eb0d6f4f217a9eaa87df83670d9
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-10-07 15:08:37 +0000

    Корректировка в html-коде

--- trunk/includes/header.php
+++ trunk/includes/header.php
@@ -37,12 +37,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>" />
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
-<!--
-	This OnlineStore is brought to you by XT-Commerce, Community made shopping
-	XTC is a free open source e-Commerce System
-	created by Mario Zanier & Guido Winger and licensed under GNU/GPL.
-	Information and contribution at http://www.xt-commerce.com
--->
 <meta name="generator" content="(c) by xt:Commerce vx.x.x , http://www.xt-commerce.com" />
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>" />
