d8b2f0165966729bcc7796c62e8dd67ceac2362b
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2008-11-30 06:52:19 +0000

    Версия 1.48

--- install/vam.sql
+++ install/vam.sql
@@ -1637,7 +1637,7 @@ CREATE TABLE post_index (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 #database Version
-INSERT INTO database_version(version) VALUES ('1.47');
+INSERT INTO database_version(version) VALUES ('1.48');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
