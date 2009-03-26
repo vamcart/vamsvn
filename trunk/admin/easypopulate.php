95bd8ecabb3ea748f13c26cee6d78a51deb3a44e
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-03-26 13:39:00 +0000

    Удалена краткая выгрузка из easypopulate

--- trunk/admin/easypopulate.php
+++ trunk/admin/easypopulate.php
@@ -923,7 +923,6 @@ if (is_uploaded_file($_FILES['usrfl']['tmp_name']) && $_GET['split']==1) {
 </span>
 			<select name="dltype">
 			<option selected value ="full" size="10"><?php echo EASY_LABEL_COMPLETE //full;?>
-			<option value="priceqty" size="10"><?php echo EASY_LABEL_MPQ //model price qty;?>
 			<option value="category" size="10"><?php echo EASY_LABEL_EP_MC //model category;?>
 			<option value="froogle" size="10"><?php echo EASY_LABEL_EP_FROGGLE //froggle;?>
 			<option value="attrib" size="10"><?php echo EASY_LABEL_EP_ATTRIB //attibutes;?>
@@ -2171,7 +2170,7 @@ vam_db_query("delete from " . TABLE_PRODUCTS_ATTRIBUTES . " where products_id ='
 		// the following is common in both the updating an existing product and creating a new product
 		if ( isset($v_products_name)){
 			foreach( $v_products_name as $key => $name){
-							if ($name!='' and $v_products_description[$key] !=''){
+							if ($name!='' or $v_products_description[$key] !=''){
 					$sql = "SELECT * FROM ".TABLE_PRODUCTS_DESCRIPTION." WHERE
 							products_id = $v_products_id AND
 							language_id = " . $key;
