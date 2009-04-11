803ad5823601b2e927cad073d3c465d42424390b
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-04-11 18:52:49 +0000

    Добавлен учёт настроек боксов (вкл./выкл.) в каталог

--- trunk/templates/vamshop/source/boxes.php
+++ trunk/templates/vamshop/source/boxes.php
@@ -16,52 +16,52 @@
 
   define('DIR_WS_BOXES',DIR_FS_CATALOG .'templates/'.CURRENT_TEMPLATE. '/source/boxes/');
 
-  include(DIR_WS_BOXES . 'affiliate.php');
-  include(DIR_WS_BOXES . 'categories.php');
-  include(DIR_WS_BOXES . 'authors.php');
-  include(DIR_WS_BOXES . 'articles.php');
-  include(DIR_WS_BOXES . 'articles_new.php');
-  include(DIR_WS_BOXES . 'manufacturers.php');
+  if (BOX_AFFILIATE == 'true') include(DIR_WS_BOXES . 'affiliate.php');
+  if (BOX_CATEGORIES == 'true') include(DIR_WS_BOXES . 'categories.php');
+  if (BOX_AUTHORS == 'true') include(DIR_WS_BOXES . 'authors.php');
+  if (BOX_ARTICLES == 'true') include(DIR_WS_BOXES . 'articles.php');
+  if (BOX_ARTICLESNEW == 'true') include(DIR_WS_BOXES . 'articles_new.php');
+  if (BOX_MANUFACTURERS == 'true') include(DIR_WS_BOXES . 'manufacturers.php');
   if ($_SESSION['customers_status']['customers_status_show_price']!='0') {
-  require(DIR_WS_BOXES . 'add_a_quickie.php');
+  if (BOX_ADD_QUICKIE == 'true') require(DIR_WS_BOXES . 'add_a_quickie.php');
   }
-  require(DIR_WS_BOXES . 'last_viewed.php');
-   if (substr(basename($PHP_SELF), 0,8) != 'advanced') {require(DIR_WS_BOXES . 'whats_new.php'); }
-  require(DIR_WS_BOXES . 'search.php');
-  require(DIR_WS_BOXES . 'content.php');
-  require(DIR_WS_BOXES . 'information.php');
-  include(DIR_WS_BOXES . 'news.php');
-  include(DIR_WS_BOXES . 'faq.php');
-  include(DIR_WS_BOXES . 'languages.php');
-  if ($_SESSION['customers_status']['customers_status_id'] == 0) include(DIR_WS_BOXES . 'admin.php');
-  require(DIR_WS_BOXES . 'infobox.php');
-  require(DIR_WS_BOXES . 'loginbox.php');
-  include(DIR_WS_BOXES . 'newsletter.php');
-  if ($_SESSION['customers_status']['customers_status_show_price'] == 1) include(DIR_WS_BOXES . 'shopping_cart.php');
-  if ($product->isProduct()) include(DIR_WS_BOXES . 'manufacturer_info.php');
+  if (BOX_LAST_VIEWED == 'true') require(DIR_WS_BOXES . 'last_viewed.php');
+   if (substr(basename($PHP_SELF), 0,8) != 'advanced') { if (BOX_WHATSNEW == 'true') require(DIR_WS_BOXES . 'whats_new.php'); }
+  if (BOX_SEARCH == 'true') require(DIR_WS_BOXES . 'search.php');
+  if (BOX_CONTENT == 'true') require(DIR_WS_BOXES . 'content.php');
+  if (BOX_INFORMATION == 'true') require(DIR_WS_BOXES . 'information.php');
+  if (BOX_LATESTNEWS == 'true') include(DIR_WS_BOXES . 'news.php');
+  if (BOX_FAQ == 'true') include(DIR_WS_BOXES . 'faq.php');
+  if (BOX_LANGUAGES == 'true') include(DIR_WS_BOXES . 'languages.php');
+  if ($_SESSION['customers_status']['customers_status_id'] == 0) { if (BOX_ADMIN == 'true') include(DIR_WS_BOXES . 'admin.php'); }
+  if (BOX_INFOBOX == 'true') require(DIR_WS_BOXES . 'infobox.php');
+  if (BOX_LOGIN == 'true') require(DIR_WS_BOXES . 'loginbox.php');
+  if (BOX_NEWSLETTER == 'true')  include(DIR_WS_BOXES . 'newsletter.php');
+  if ($_SESSION['customers_status']['customers_status_show_price'] == 1) { if (BOX_CART == 'true') include(DIR_WS_BOXES . 'shopping_cart.php'); }
+  if ($product->isProduct()) { if (BOX_MANUFACTURERS_INFO == 'true') include(DIR_WS_BOXES . 'manufacturer_info.php'); }
 
-  if (isset($_SESSION['customer_id'])) include(DIR_WS_BOXES . 'order_history.php');
+  if (isset($_SESSION['customer_id'])) { include(DIR_WS_BOXES . 'order_history.php'); }
 
   if (!$product->isProduct()) {
-    include(DIR_WS_BOXES . 'best_sellers.php');
-  }
-
-  if (!$product->isProduct()) {
-    include(DIR_WS_BOXES . 'specials.php');
+    if (BOX_BESTSELLERS == 'true') include(DIR_WS_BOXES . 'best_sellers.php');
   }
 
   if (!$product->isProduct()) {
-    include(DIR_WS_BOXES . 'featured.php');
+    if (BOX_SPECIALS == 'true') include(DIR_WS_BOXES . 'specials.php');
   }
 
-  if ($_SESSION['customers_status']['customers_status_read_reviews'] == 1) require(DIR_WS_BOXES . 'reviews.php');
+  if (!$product->isProduct()) {
+    if (BOX_FEATURED == 'true') include(DIR_WS_BOXES . 'featured.php');
+  }
+
+  if ($_SESSION['customers_status']['customers_status_read_reviews'] == 1) { if (BOX_REVIEWS == 'true') require(DIR_WS_BOXES . 'reviews.php'); }
 
   if (substr(basename($PHP_SELF), 0, 8) != 'checkout') {
 
-    include(DIR_WS_BOXES . 'currencies.php');
+    if (BOX_CURRENCIES == 'true') include(DIR_WS_BOXES . 'currencies.php');
   }
 
-  include(DIR_WS_BOXES . 'download.php');
+  if (BOX_DOWNLOADS == 'true') include(DIR_WS_BOXES . 'download.php');
 
 $vamTemplate->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
 ?>
