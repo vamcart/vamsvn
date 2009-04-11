b8c4148b178e21f43b6744912cd38b751dfaf1f7
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-04-11 19:00:57 +0000

    Корректировка в условии вывода боксов

--- templates/vamshop_table/source/boxes.php
+++ templates/vamshop_table/source/boxes.php
@@ -16,52 +16,52 @@
 
   define('DIR_WS_BOXES',DIR_FS_CATALOG .'templates/'.CURRENT_TEMPLATE. '/source/boxes/');
 
-  if (BOX_AFFILIATE == 'true') include(DIR_WS_BOXES . 'affiliate.php');
-  if (BOX_CATEGORIES == 'true') include(DIR_WS_BOXES . 'categories.php');
-  if (BOX_AUTHORS == 'true') include(DIR_WS_BOXES . 'authors.php');
-  if (BOX_ARTICLES == 'true') include(DIR_WS_BOXES . 'articles.php');
-  if (BOX_ARTICLESNEW == 'true') include(DIR_WS_BOXES . 'articles_new.php');
-  if (BOX_MANUFACTURERS == 'true') include(DIR_WS_BOXES . 'manufacturers.php');
+  if (SET_BOX_AFFILIATE == 'true') include(DIR_WS_BOXES . 'affiliate.php');
+  if (SET_BOX_CATEGORIES == 'true') include(DIR_WS_BOXES . 'categories.php');
+  if (SET_BOX_AUTHORS == 'true') include(DIR_WS_BOXES . 'authors.php');
+  if (SET_BOX_ARTICLES == 'true') include(DIR_WS_BOXES . 'articles.php');
+  if (SET_BOX_ARTICLESNEW == 'true') include(DIR_WS_BOXES . 'articles_new.php');
+  if (SET_BOX_MANUFACTURERS == 'true') include(DIR_WS_BOXES . 'manufacturers.php');
   if ($_SESSION['customers_status']['customers_status_show_price']!='0') {
-  if (BOX_ADD_QUICKIE == 'true') require(DIR_WS_BOXES . 'add_a_quickie.php');
+  if (SET_BOX_ADD_QUICKIE == 'true') require(DIR_WS_BOXES . 'add_a_quickie.php');
   }
-  if (BOX_LAST_VIEWED == 'true') require(DIR_WS_BOXES . 'last_viewed.php');
-   if (substr(basename($PHP_SELF), 0,8) != 'advanced') { if (BOX_WHATSNEW == 'true') require(DIR_WS_BOXES . 'whats_new.php'); }
-  if (BOX_SEARCH == 'true') require(DIR_WS_BOXES . 'search.php');
-  if (BOX_CONTENT == 'true') require(DIR_WS_BOXES . 'content.php');
-  if (BOX_INFORMATION == 'true') require(DIR_WS_BOXES . 'information.php');
-  if (BOX_LATESTNEWS == 'true') include(DIR_WS_BOXES . 'news.php');
-  if (BOX_FAQ == 'true') include(DIR_WS_BOXES . 'faq.php');
-  if (BOX_LANGUAGES == 'true') include(DIR_WS_BOXES . 'languages.php');
-  if ($_SESSION['customers_status']['customers_status_id'] == 0) { if (BOX_ADMIN == 'true') include(DIR_WS_BOXES . 'admin.php'); }
-  if (BOX_INFOBOX == 'true') require(DIR_WS_BOXES . 'infobox.php');
-  if (BOX_LOGIN == 'true') require(DIR_WS_BOXES . 'loginbox.php');
-  if (BOX_NEWSLETTER == 'true')  include(DIR_WS_BOXES . 'newsletter.php');
-  if ($_SESSION['customers_status']['customers_status_show_price'] == 1) { if (BOX_CART == 'true') include(DIR_WS_BOXES . 'shopping_cart.php'); }
-  if ($product->isProduct()) { if (BOX_MANUFACTURERS_INFO == 'true') include(DIR_WS_BOXES . 'manufacturer_info.php'); }
+  if (SET_BOX_LAST_VIEWED == 'true') require(DIR_WS_BOXES . 'last_viewed.php');
+   if (substr(basename($PHP_SELF), 0,8) != 'advanced') { if (SET_BOX_WHATSNEW == 'true') require(DIR_WS_BOXES . 'whats_new.php'); }
+  if (SET_BOX_SEARCH == 'true') require(DIR_WS_BOXES . 'search.php');
+  if (SET_BOX_CONTENT == 'true') require(DIR_WS_BOXES . 'content.php');
+  if (SET_BOX_INFORMATION == 'true') require(DIR_WS_BOXES . 'information.php');
+  if (SET_BOX_LATESTNEWS == 'true') include(DIR_WS_BOXES . 'news.php');
+  if (SET_BOX_FAQ == 'true') include(DIR_WS_BOXES . 'faq.php');
+  if (SET_BOX_LANGUAGES == 'true') include(DIR_WS_BOXES . 'languages.php');
+  if ($_SESSION['customers_status']['customers_status_id'] == 0) { if (SET_BOX_ADMIN == 'true') include(DIR_WS_BOXES . 'admin.php'); }
+  if (SET_BOX_INFOBOX == 'true') require(DIR_WS_BOXES . 'infobox.php');
+  if (SET_BOX_LOGIN == 'true') require(DIR_WS_BOXES . 'loginbox.php');
+  if (SET_BOX_NEWSLETTER == 'true')  include(DIR_WS_BOXES . 'newsletter.php');
+  if ($_SESSION['customers_status']['customers_status_show_price'] == 1) { if (SET_BOX_CART == 'true') include(DIR_WS_BOXES . 'shopping_cart.php'); }
+  if ($product->isProduct()) { if (SET_BOX_MANUFACTURERS_INFO == 'true') include(DIR_WS_BOXES . 'manufacturer_info.php'); }
 
   if (isset($_SESSION['customer_id'])) { include(DIR_WS_BOXES . 'order_history.php'); }
 
   if (!$product->isProduct()) {
-    if (BOX_BESTSELLERS == 'true') include(DIR_WS_BOXES . 'best_sellers.php');
+    if (SET_BOX_BESTSELLERS == 'true') include(DIR_WS_BOXES . 'best_sellers.php');
   }
 
   if (!$product->isProduct()) {
-    if (BOX_SPECIALS == 'true') include(DIR_WS_BOXES . 'specials.php');
+    if (SET_BOX_SPECIALS == 'true') include(DIR_WS_BOXES . 'specials.php');
   }
 
   if (!$product->isProduct()) {
-    if (BOX_FEATURED == 'true') include(DIR_WS_BOXES . 'featured.php');
+    if (SET_BOX_FEATURED == 'true') include(DIR_WS_BOXES . 'featured.php');
   }
 
-  if ($_SESSION['customers_status']['customers_status_read_reviews'] == 1) { if (BOX_REVIEWS == 'true') require(DIR_WS_BOXES . 'reviews.php'); }
+  if ($_SESSION['customers_status']['customers_status_read_reviews'] == 1) { if (SET_BOX_REVIEWS == 'true') require(DIR_WS_BOXES . 'reviews.php'); }
 
   if (substr(basename($PHP_SELF), 0, 8) != 'checkout') {
 
-    if (BOX_CURRENCIES == 'true') include(DIR_WS_BOXES . 'currencies.php');
+    if (SET_BOX_CURRENCIES == 'true') include(DIR_WS_BOXES . 'currencies.php');
   }
 
-  if (BOX_DOWNLOADS == 'true') include(DIR_WS_BOXES . 'download.php');
+  if (SET_BOX_DOWNLOADS == 'true') include(DIR_WS_BOXES . 'download.php');
 
 $vamTemplate->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
 ?>
