0603f3238342e69dfff21d677f9b6c9653cfb0cc
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-04-17 17:06:54 +0000

    Корректировка в быстром поиске ajax

--- trunk/includes/modules/ajax/ajaxQuickFind.php
+++ trunk/includes/modules/ajax/ajaxQuickFind.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: ajaxQuickFind.php 1243 2007-02-06 20:41:56 VaM $   
+   $Id: ajaxQuickFind.php 1243 2009-02-06 20:41:56 VaM $   
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
