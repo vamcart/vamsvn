e05aade93365f16c0ec1a227e655a2a4e37607ef
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-05-26 14:53:39 +0000

    Корректировка кода в функции vam_cleanName, спасибо PlazaUA

--- inc/vam_cleanName.inc.php
+++ inc/vam_cleanName.inc.php
@@ -22,7 +22,8 @@ function vam_cleanName($name) {
      $cyrillic = array("ж", "ё", "й","ю", "ь","ч", "щ", "ц","у","к","е","н","г","ш", "з","х","ъ","ф","ы","в","а","п","р","о","л","д","э","я","с","м","и","т","б","Ё","Й","Ю","Ч","Ь","Щ","Ц","У","К","Е","Н","Г","Ш","З","Х","Ъ","Ф","Ы","В","А","П","Р","О","Л","Д","Ж","Э","Я","С","М","И","Т","Б");
      $translit = array("zh","yo","i","yu","","ch","sh","c","u","k","e","n","g","sh","z","h","",  "f",  "y",  "v",  "a",  "p",  "r",  "o",  "l",  "d",  "ye", "ya", "s",  "m",  "i",  "t",  "b",  "yo", "I",  "YU", "CH", "",  "SH", "C",  "U",  "K",  "E",  "N",  "G",  "SH", "Z",  "H",  "",  "F",  "Y",  "V",  "A",  "P",  "R",  "O",  "L",  "D",  "Zh", "Ye", "Ya", "S",  "M",  "I",  "T",  "B");
      $name = str_replace($cyrillic, $translit, $name);    
-     $name=preg_replace($replace_param,'-',$name);    
+     $name=preg_replace($replace_param,'-',$name);
+     $name = urlencode($name);    
      return $name;
 }
 
