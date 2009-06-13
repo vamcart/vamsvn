aa5de2a345d6a06990be7ee135ff332280e8c41f
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-06-13 12:29:08 +0000

    Добавлена кнопка Справка на страницы модуля рассылок и незавершённых заказов

--- admin/recover_cart_sales.php
+++ admin/recover_cart_sales.php
@@ -123,7 +123,12 @@ if ($_GET['delete']) {
 <!-- body_text //-->
     <td class="boxCenter" width="100%" valign="top">
     
-    <h1 class="contentBoxHeading"><?php echo HEADING_TITLE_RECOVER; ?></h1>
+        <table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo HEADING_TITLE_RECOVER; ?></td>
+            <td class="pageHeading" align="right"><a class="button" href="<?php echo MANUAL_LINK_RCS; ?>" target="_blank"><span><?php echo TEXT_MANUAL_LINK; ?></span></a></td>
+          </tr>
+        </table>
     
     <table border="0" width="100%" cellspacing="0" cellpadding="2">
       <tr>
