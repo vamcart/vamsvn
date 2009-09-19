d364cbd062245d431c5b0b2856ee288debb093f9
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-09-19 07:33:02 +0000

    Корректировка кода для совместимости с php 5.3

--- admin/csv_backend.php
+++ admin/csv_backend.php
@@ -138,7 +138,7 @@
     }
     if (vam_not_null($configuration['use_function'])) {
       $use_function = $configuration['use_function'];
-      if (ereg('->', $use_function)) {
+      if (preg_match('/->/', $use_function)) {
         $class_method = explode('->', $use_function);
         if (!is_object(${$class_method[0]})) {
           include(DIR_WS_CLASSES . $class_method[0] . '.php');
