b6295d2cb649db4cc28706ffef34c3808d5d1047
vam <vam@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2009-11-24 07:59:45 +0000

    Корректировка в модуле быстрого оформления заказа

--- trunk/checkout_alternative.php
+++ trunk/checkout_alternative.php
@@ -235,7 +235,7 @@ if (isset ($_POST['payment']))
 	$vatID = new vat_validation($vat, '', '', $country,true);
 
 	$customers_vat_id_status = 0;
-	$customers_status = $vatID->vat_info['status'];
+//	$customers_status = $vatID->vat_info['status'];
 	$customers_vat_id_status = $vatID->vat_info['vat_id_status'];
 	$error = $vatID->vat_info['error'];
 
