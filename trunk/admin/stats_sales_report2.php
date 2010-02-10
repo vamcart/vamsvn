40b3bd6737024d521d2f3a167c3f2f9cb0ca88e8
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-02-10 16:14:41 +0000

    Исправлен путь к jQuery

--- trunk/admin/stats_sales_report2.php
+++ trunk/admin/stats_sales_report2.php
@@ -99,7 +99,7 @@
 <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
 <?php if (ENABLE_TABS == 'true') { ?>
 		<link type="text/css" href="../jscript/jquery/plugins/ui/css/smoothness/jquery-ui-1.7.2.custom.css" rel="stylesheet" />	
-		<script type="text/javascript" src="../jscript/jquery/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="../jscript/jquery/jquery.js"></script>
 		<script type="text/javascript" src="../jscript/jquery/plugins/ui/jquery-ui-1.7.2.custom.min.js"></script>
 		<script type="text/javascript">
 			$(function(){
