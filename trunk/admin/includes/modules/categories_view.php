4320375d467b49d4393508394a4aa7714f782453
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-04-24 11:34:13 +0000

    Корректировка html-кода в админке

--- trunk/admin/includes/modules/categories_view.php
+++ trunk/admin/includes/modules/categories_view.php
@@ -812,7 +812,7 @@ if ($numr>$max_count){
 
     if ((vam_not_null($heading)) && (vam_not_null($contents))) {
       //display info box
-      echo '<td valign="top" style="padding-left: 5px;">' . "\n";
+      echo '<td valign="top">' . "\n";
       $box = new box;
       echo $box->infoBox($heading, $contents);
       echo '</td>' . "\n";
