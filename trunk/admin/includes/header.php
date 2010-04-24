4cedf9118b375e5735a5b769c945d6193be5bac9
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-04-24 17:48:39 +0000

    Корректировка меню в админке

--- trunk/admin/includes/header.php
+++ trunk/admin/includes/header.php
@@ -40,7 +40,7 @@
 
 <?php if (ADMIN_DROP_DOWN_NAVIGATION == 'true') { ?>
 
-          <table width="99%" align="center" border="0" cellspacing="0" cellpadding="0">
+          <table width="100%"  border="0" cellspacing="0" cellpadding="0">
             <tr>
               <td>
               
