b4a0b63b43f724f3b87ca81dfb8681e41b1a1b19
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-05-07 17:25:33 +0000

    Добавлена мультиязычность в вывод суммы заказа в счёт и накладную в админке и каталоге

--- trunk/admin/print_packingslip.php
+++ trunk/admin/print_packingslip.php
@@ -22,79 +22,79 @@
   require_once(DIR_FS_INC .'vam_not_null.inc.php');
   require_once(DIR_FS_INC .'vam_format_price_order.inc.php');
 
-class inwords {
+class inwords { 
 
-var $diw=Array(    0 =>    Array(    0  => Array( 0=> "ноль",    1=>1),
-                1  => Array( 0=> "",        1=>2),
-                2  => Array( 0=> "",        1=>3),
-                3  => Array( 0=> "три",        1=>0),
-                4  => Array( 0=> "четыре",    1=>0),
-                5  => Array( 0=> "пять",    1=>1),
-                6  => Array( 0=> "шесть",    1=>1),
-                7  => Array( 0=> "семь",    1=>1),
-                8  => Array( 0=> "восемь",    1=>1),
-                9  => Array( 0=> "девять",    1=>1),
-                10 => Array( 0=> "десять",    1=>1),
-                11 => Array( 0=> "одиннадцать",    1=>1),
-                12 => Array( 0=> "двенадцать",    1=>1),
-                13 => Array( 0=> "тринадцать",    1=>1),
-                14 => Array( 0=> "четырнадцать",1=>1),
-                15 => Array( 0=> "пятнадцать",    1=>1),
-                16 => Array( 0=> "шестнадцать",    1=>1),
-                17 => Array( 0=> "семнадцать",    1=>1),
-                18 => Array( 0=> "восемнадцать",1=>1),
-                19 => Array( 0=> "девятнадцать",1=>1)
-            ),
-        1 =>    Array(    2  => Array( 0=> "двадцать",    1=>1),
-                3  => Array( 0=> "тридцать",    1=>1),
-                4  => Array( 0=> "сорок",    1=>1),
-                5  => Array( 0=> "пятьдесят",    1=>1),
-                6  => Array( 0=> "шестьдесят",    1=>1),
-                7  => Array( 0=> "семьдесят",    1=>1),
-                8  => Array( 0=> "восемьдесят",    1=>1),
-                9  => Array( 0=> "девяносто",    1=>1)
-            ),
-        2 =>    Array(    1  => Array( 0=> "сто",        1=>1),
-                2  => Array( 0=> "двести",    1=>1),
-                3  => Array( 0=> "триста",    1=>1),
-                4  => Array( 0=> "четыреста",    1=>1),
-                5  => Array( 0=> "пятьсот",    1=>1),
-                6  => Array( 0=> "шестьсот",    1=>1),
-                7  => Array( 0=> "семьсот",    1=>1),
-                8  => Array( 0=> "восемьсот",    1=>1),
-                9  => Array( 0=> "девятьсот",    1=>1)
-            )
-);
+var $diw=Array(    0 =>    Array(    0  => Array( 0=> text_zero,    1=>1), 
+                1  => Array( 0=> "",        1=>2), 
+                2  => Array( 0=> "",        1=>3), 
+                3  => Array( 0=> text_three,        1=>0), 
+                4  => Array( 0=> text_four,    1=>0), 
+                5  => Array( 0=> text_five,    1=>1), 
+                6  => Array( 0=> text_six,    1=>1), 
+                7  => Array( 0=> text_seven,    1=>1), 
+                8  => Array( 0=> text_eight,    1=>1), 
+                9  => Array( 0=> text_nine,    1=>1), 
+                10 => Array( 0=> text_ten,    1=>1), 
+                11 => Array( 0=> text_eleven,    1=>1), 
+                12 => Array( 0=> text_twelve,    1=>1), 
+                13 => Array( 0=> text_thirteen,    1=>1), 
+                14 => Array( 0=> text_fourteen,1=>1), 
+                15 => Array( 0=> text_fifteen,    1=>1), 
+                16 => Array( 0=> text_sixteen,    1=>1), 
+                17 => Array( 0=> text_seventeen,    1=>1), 
+                18 => Array( 0=> text_eighteen,1=>1), 
+                19 => Array( 0=> text_nineteen,1=>1) 
+            ), 
+        1 =>    Array(    2  => Array( 0=> text_twenty,    1=>1), 
+                3  => Array( 0=> text_thirty,    1=>1), 
+                4  => Array( 0=> text_forty,    1=>1), 
+                5  => Array( 0=> text_fifty,    1=>1), 
+                6  => Array( 0=> text_sixty,    1=>1), 
+                7  => Array( 0=> text_seventy,    1=>1), 
+                8  => Array( 0=> text_eighty,    1=>1), 
+                9  => Array( 0=> text_ninety,    1=>1)  
+            ), 
+        2 =>    Array(    1  => Array( 0=> text_hundred,        1=>1), 
+                2  => Array( 0=> text_two_hundred,    1=>1), 
+                3  => Array( 0=> text_three_hundred,    1=>1), 
+                4  => Array( 0=> text_four_hundred,    1=>1), 
+                5  => Array( 0=> text_five_hundred,    1=>1), 
+                6  => Array( 0=> text_six_hundred,    1=>1), 
+                7  => Array( 0=> text_seven_hundred,    1=>1), 
+                8  => Array( 0=> text_eight_hundred,    1=>1), 
+                9  => Array( 0=> text_nine_hundred,    1=>1) 
+            ) 
+); 
 
-var $nom=Array(    0 => Array(0=>"копейки",  1=>"копеек",    2=>"одна копейка", 3=>"две копейки"),
-        1 => Array(0=>"рубля",    1=>"рублей",    2=>"один рубль",   3=>"два рубля"),
-        2 => Array(0=>"тысячи",   1=>"тысяч",     2=>"одна тысяча",  3=>"две тысячи"),
-        3 => Array(0=>"миллиона", 1=>"миллионов", 2=>"один миллион", 3=>"два миллиона"),
-        4 => Array(0=>"миллиарда",1=>"миллиардов",2=>"один миллиард",3=>"два миллиарда"),
-/* :))) */
-        5 => Array(0=>"триллиона",1=>"триллионов",2=>"один триллион",3=>"два триллиона")
-);
+var $nom=Array(    0 => Array(0=> text_penny,  1=> text_kopecks,    2=> text_single_kopek, 3=> text_two_penny), 
+        1 => Array(0=> text_ruble,    1=> text_rubles,    2=> text_one_ruble,   3=> text_two_rubles), 
+        2 => Array(0=> text_thousands,   1=> text_thousand,     2=> text_one_thousand,  3=> text_two_thousand), 
+        3 => Array(0=> text_million, 1=> text_millions, 2=> text_one_million, 3=> text_two_million), 
+        4 => Array(0=> text_billion, 1=> text_billions, 2=> text_one_billion, 3=> text_two_billion), 
+/* :))) */ 
+        5 => Array(0=> text_trillion, 1=> text_trillions, 2=> text_one_trillion, 3=>text_two_trillion) 
+); 
 
-var $out_rub;
+var $out_rub; 
 
-function get($summ){
- if($summ>=1) $this->out_rub=0;
- else $this->out_rub=1;
- $summ_rub= doubleval(sprintf("%0.0f",$summ));
- if(($summ_rub-$summ)>0) $summ_rub--;
- $summ_kop= doubleval(sprintf("%0.2f",$summ-$summ_rub))*100;
- $kop=$this->get_string($summ_kop,0);
- $retval="";
- for($i=1;$i<6&&$summ_rub>=1;$i++):
-  $summ_tmp=$summ_rub/1000;
-  $summ_part=doubleval(sprintf("%0.3f",$summ_tmp-intval($summ_tmp)))*1000;
-  $summ_rub= doubleval(sprintf("%0.0f",$summ_tmp));
-  if(($summ_rub-$summ_tmp)>0) $summ_rub--;
-  $retval=$this->get_string($summ_part,$i)." ".$retval;
- endfor;
- if(($this->out_rub)==0) $retval.=" рублей";
- return $retval." ".$kop;
-}
+function get($summ){ 
+ if($summ>=1) $this->out_rub=0; 
+ else $this->out_rub=1; 
+ $summ_rub= doubleval(sprintf("%0.0f",$summ)); 
+ if(($summ_rub-$summ)>0) $summ_rub--; 
+ $summ_kop= doubleval(sprintf("%0.2f",$summ-$summ_rub))*100; 
+ $kop=$this->get_string($summ_kop,0); 
+ $retval=""; 
+ for($i=1;$i<6&&$summ_rub>=1;$i++): 
+  $summ_tmp=$summ_rub/1000; 
+  $summ_part=doubleval(sprintf("%0.3f",$summ_tmp-intval($summ_tmp)))*1000; 
+  $summ_rub= doubleval(sprintf("%0.0f",$summ_tmp)); 
+  if(($summ_rub-$summ_tmp)>0) $summ_rub--; 
+  $retval=$this->get_string($summ_part,$i)." ".$retval; 
+ endfor; 
+ if(($this->out_rub)==0) $retval.=' ' . text_rubles; 
+ return $retval." ".$kop; 
+} 
 
 function get_string($summ,$nominal){
  $retval="";
