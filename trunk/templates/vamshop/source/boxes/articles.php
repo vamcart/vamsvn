c8a060ec5e0178576c83091deb96191e06266ea5
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-06-24 07:59:45 +0000

    Корректировка бокса статьи

--- trunk/templates/vamshop/source/boxes/articles.php
+++ trunk/templates/vamshop/source/boxes/articles.php
@@ -221,8 +221,8 @@ if (!$cache) {
 	$box_articles = $box->fetch(CURRENT_TEMPLATE.'/boxes/box_articles.html', $cache_id);
 }
 
-if (vam_db_num_rows($topics_query, true) > 0) {
+//if (vam_db_num_rows($topics_query, true) > 0) {
 	$vamTemplate->assign('box_ARTICLES',$box_articles);
-}	
+//}	
 
 ?>
