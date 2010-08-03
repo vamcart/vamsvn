5c355a32841a101c655f668e1712f0c481634cb9
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-08-03 06:57:58 +0000

    Корректировка метода getReverseCrossSells, добавлена колонка products_quantity в SQL запрос

--- includes/classes/product.php
+++ includes/classes/product.php
@@ -283,6 +283,7 @@ class product {
 																						 p.products_tax_class_id,
 																                                                 p.products_id,
 																                                                 p.products_image,
+																                                                 p.products_quantity,
 																                                                 pd.products_name,
 																						 						pd.products_short_description,
 																                                                 p.products_fsk18,p.products_price,p.products_vpe,
