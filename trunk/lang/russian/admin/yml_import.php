e7a5cfcfdfdd5a037fcb0781587e3adfc1bb68c3
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-08-06 11:26:52 +0000

    Корректировка перевода модуля YML импорт

--- trunk/lang/russian/admin/yml_import.php
+++ trunk/lang/russian/admin/yml_import.php
@@ -1,6 +1,6 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: yml_import.php,v 1.1 2010-08-06 17:36:57 VaM $
+   $Id: yml_import.php,v 1.1 2010-08-06 17:37:57 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
