404cb8589653044c968325428ff0cdb9bada0705
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-08-06 11:25:08 +0000

    Добавлен новый модуль - YML (Яндекс-маркет) импорт

---
+++ lang/russian/admin/yml_import.php
@@ -0,0 +1,24 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: yml_import.php,v 1.1 2010-08-06 17:36:57 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2010 VaMSoft Ltd.
+   --------------------------------------------------------------
+   Released under the GNU General Public License
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Яндекс-Маркет импорт/экспорт');
+
+define('TEXT_YML_UPDATED', 'Обновлено ');
+define('TEXT_YML_CHANGED', 'Без изменений ');
+define('TEXT_YML_ADDED','Добавлено ');
+define('TEXT_YML_ERROR', 'Файл не загружен');
+define('TEXT_YML_MAX_SIZE', 'Максиальный размер файла:');
+define('TEXT_YML_IMPORT', 'Импорт');
+define('TEXT_YML_EXPORT', 'Экспорт');
+
+?>
