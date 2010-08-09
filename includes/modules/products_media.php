22463b42db9f5918dd9af04ac2f5cdde256e632c
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2010-08-09 14:58:38 +0000

    Корректировка кода

--- includes/modules/products_media.php
+++ includes/modules/products_media.php
@@ -87,6 +87,52 @@ if (vam_in_array($product->data['products_id'], $check_data)) {
 
 	$module->assign('language', $_SESSION['language']);
 	$module->assign('module_content', $module_content);
+	
+	$module->assign('download_permission', false);
+
+// Get last order id for checkout_success
+    $orders_query_raw = "SELECT orders_id FROM " . TABLE_ORDERS . " WHERE customers_id = '" . (int)$_SESSION['customer_id'] . "' ORDER BY orders_id DESC LIMIT 1";
+    $orders_query = vam_db_query($orders_query_raw);
+    $orders_values = vam_db_fetch_array($orders_query);
+    $last_order = $orders_values['orders_id'];
+
+// Now get all downloadable products in that order
+  $downloads_query_raw = "SELECT DATE_FORMAT(date_purchased, '%Y-%m-%d') as date_purchased_day, opd.download_maxdays, op.products_name, opd.orders_products_download_id, opd.orders_products_filename, opd.download_count, opd.download_maxdays
+                          FROM " . TABLE_ORDERS . " o, " . TABLE_ORDERS_PRODUCTS . " op, " . TABLE_ORDERS_PRODUCTS_DOWNLOAD . " opd
+                          WHERE customers_id = '" . (int)$_SESSION['customer_id'] . "'
+                          AND o.orders_id = '" . (int)$last_order . "'
+                          AND o.orders_status >= " . DOWNLOAD_MIN_ORDERS_STATUS . "
+                          AND op.orders_id = '" . (int)$last_order . "'
+                          AND opd.orders_products_id=op.orders_products_id
+                          AND opd.orders_products_filename<>'' limit 1";
+  $downloads_query = vam_db_query($downloads_query_raw);
+
+// Don't display if there is no downloadable product
+  if (vam_db_num_rows($downloads_query) > 0) {
+
+    $downloads_values = vam_db_fetch_array($downloads_query);
+
+// MySQL 3.22 does not have INTERVAL
+    	list($dt_year, $dt_month, $dt_day) = explode('-', $downloads_values['date_purchased_day']);
+    	$download_timestamp = mktime(23, 59, 59, $dt_month, $dt_day + $downloads_values['download_maxdays'], $dt_year);
+  	    $download_expiry = date('Y-m-d H:i:s', $download_timestamp);
+
+// The link will appear only if:
+// - Download remaining count is > 0, AND
+// - The file is present in the DOWNLOAD directory, AND EITHER
+// - No expiry date is enforced (maxdays == 0), OR
+// - The expiry date is not reached
+      if (($downloads_values['download_count'] > 0) &&
+          (file_exists(DIR_FS_DOWNLOAD . $downloads_values['orders_products_filename'])) &&
+          (($downloads_values['download_maxdays'] == 0) ||
+           ($download_timestamp > time()))) {
+           	
+	$module->assign('download_permission', true);
+  	
+  	}
+	
+	}
+	
 	// set cache ID
 
 		$module->caching = 0;
