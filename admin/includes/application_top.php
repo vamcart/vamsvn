6cc35075eb0a9c23b2c44210e8a7d236e441a578
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-01-09 15:19:18 +0000

    Версия 1.61

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -56,7 +56,7 @@
   define('SQL_CACHEDIR',DIR_FS_CATALOG.'cache/');
 
   // Define the project version
-  define('PROJECT_VERSION', 'VaM Shop 1.60');
+  define('PROJECT_VERSION', 'VaM Shop 1.61');
 
   // Set the length of the redeem code, the longer the more secure
   define('SECURITY_CODE_LENGTH', '6');
