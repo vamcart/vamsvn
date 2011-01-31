e0785d77e8b0557147dbc223e4bf5bf0325f5aae
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-01-31 16:04:58 +0000

    Корректировка вывода каптчи

--- inc/vam_random_charcode.inc.php
+++ inc/vam_random_charcode.inc.php
@@ -17,8 +17,9 @@
    
   // build to generate a random charcode
   function vam_random_charcode($length) {
- $arraysize = 34; 
- $chars = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9');
+ $arraysize = 10; 
+// $chars = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9');
+ $chars = array('0','1','2','3','4','5','6','7','8','9');
 
   $code = '';
     for ($i = 1; $i <= $length; $i++) {
