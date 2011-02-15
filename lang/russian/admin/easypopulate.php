08d458be3ad5ce16679f7fe8f2bb413b7f65e2b9
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-02-15 17:33:32 +0000

    Добавлен новый перевод для модуля Excel импорт/экспорт

--- lang/russian/admin/easypopulate.php
+++ lang/russian/admin/easypopulate.php
@@ -1,12 +1,12 @@
 <?php
 /* --------------------------------------------------------------
-   $Id: easypopulate.php 899 2007-02-07 17:36:57 VaM $
+   $Id: easypopulate.php 899 2011-02-07 17:36:57 oleg_vamsoft $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
    http://vamshop.com
 
-   Copyright (c) 2007 VaM Shop
+   Copyright (c) 2011 VaM Shop
    --------------------------------------------------------------
    based on: 
    (c) 2003	 osCommerce (easypopulate.php,v 1.4 2003/08/14); oscommerce.com
@@ -15,94 +15,158 @@
    --------------------------------------------------------------*/
 
 define('HEADING_TITLE', 'Excel импорт/экспорт');
-define('EASY_VERSION_A', 'Excel импорт/экспорт ');
-define('EASY_DEFAULT_LANGUAGE', '  -  Язык по умолчанию - ');
+
+define('EASYPOPULATE_TAB_IMPORT','Импорт');
+define('EASYPOPULATE_TAB_IMPORT_TEMP','Импорт из директории');
+define('EASYPOPULATE_TAB_SPLIT','Разделить');
+define('EASYPOPULATE_TAB_EXPORT','Экспорт');
+define('EASYPOPULATE_TAB_QUICK_LINKS','Быстрые ссылки');
+define('EASYPOPULATE_TAB_INFO','Информация');
+
+define('TEXT_EASYPOPULATE_PLEASE','Пожалуйста,');
+define('TEXT_EASYPOPULATE_BACKUP_TEXT','делайте резервные копии базы данных');
+define('TEXT_EASYPOPULATE_BACKUP_TEXT1',', прежде чем работать с модулем!');
+
+define('TEXT_EASYPOPULATE_BACKUP_BUTTON','Резервное копирование');
+
+define('EASY_UPLOAD_EP_FILE', 'Импорт');
+define('EASY_INSERT', 'Импортировать');
+
+define('TEXT_EASYPOPULATE_NORMAL','Нормальный');
+define('TEXT_EASYPOPULATE_ADD','Добавить новые');
+define('TEXT_EASYPOPULATE_UPDATE','Обновление');
+define('TEXT_EASYPOPULATE_DELETE','Удаление');
+
+define('EASY_IMPORT_TEMP_DIR', 'Импортировать из директории');
+define('EASY_SPLIT_EP_FILE', 'Загрузить и разделить файл на части');
+
+define('EASYPOPULATE_BUTTON_SPLIT','Разделить');
+
+define('TEXT_EASYPOPULATE_EXPORT','Экспорт');
+
+define('TEXT_EASYPOPULATE_ON_THE_FLY','Скачать');
+define('TEXT_EASYPOPULATE_CREATE_THEN_DOWNLOAD','Создать и затем скачать');
+define('TEXT_EASYPOPULATE_CREATE_IN_TEMP','Создать в директории');
+define('TEXT_EASYPOPULATE_TYPE','файл');
+define('TEXT_EASYPOPULATE_COMPLETE','Полный');
+define('TEXT_EASYPOPULATE_CUSTOM','Выборочный');
+define('TEXT_EASYPOPULATE_PRICE_QTY','Цена/Количество');
+define('TEXT_EASYPOPULATE_CATEGORIES','Категории');
+define('TEXT_EASYPOPULATE_ATTRIBUTES','Атрибуты');
+define('TEXT_EASYPOPULATE_FROOGLE','Фругл');
+define('TEXT_EASYPOPULATE_FILE_FORMAT',' файл (артикул всегда добавляется в файл).');
+
+define('TEXT_EASYPOPULATE_LABEL_NAME','название');
+define('TEXT_EASYPOPULATE_LABEL_DESC','описание');
+define('TEXT_EASYPOPULATE_LABEL_URL','url ссылка');
+define('TEXT_EASYPOPULATE_LABEL_IMAGE','картинка');
+define('TEXT_EASYPOPULATE_LABEL_ATTRIBUTES','атрибуты');
+define('TEXT_EASYPOPULATE_LABEL_CATEGORIES','категория');
+define('TEXT_EASYPOPULATE_LABEL_MANUFACTURERS','производитель');
+define('TEXT_EASYPOPULATE_LABEL_PRICE','цена');
+define('TEXT_EASYPOPULATE_LABEL_QUANTITY','количество');
+define('TEXT_EASYPOPULATE_LABEL_WEIGHT','вес');
+define('TEXT_EASYPOPULATE_LABEL_TAX_CLASS','налог');
+define('TEXT_EASYPOPULATE_LABEL_AVAILABLE','дата доступности');
+define('TEXT_EASYPOPULATE_LABEL_DATE_ADDED','дата создания');
+define('TEXT_EASYPOPULATE_LABEL_STATUS','статус');
+define('TEXT_EASYPOPULATE_LABEL_SPECIALS','скидки');
+define('TEXT_EASYPOPULATE_LABEL_ADD_IMAGES','добавить картинки');
+define('TEXT_EASYPOPULATE_LABEL_VENDOR','вендор');
+define('TEXT_EASYPOPULATE_LABEL_XSELL','сопутствующие товары');
+
+define('TEXT_EASYPOPULATE_FILTER_BY','Фильтр: ');
+define('TEXT_EASYPOPULATE_FILTER_CATEGORY','- Категория -');
+define('TEXT_EASYPOPULATE_FILTER_MANUFACTURER','- Производитель -');
+define('TEXT_EASYPOPULATE_FILTER_STATUS','- Статус -');
+define('TEXT_EASYPOPULATE_FILTER_STATUS_ACTIVE','Активные');
+define('TEXT_EASYPOPULATE_FILTER_STATUS_DISABLED','Неактивные');
+define('TEXT_EASYPOPULATE_BUILD_BUTTON','Создать файл');
+
+define('TEXT_EASYPOPULATE_QUICK_LINKS','Быстрые ссылки');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_1','Создать и затем скачать');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_2','Генерировать файл целиком и затем скачать.');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_3','Скачать <b>Полный');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_4',' с модулем SPPC');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_5','файл для редактирования');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_6','файл в директории');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_7','Скачать <b>Дополнительные поля</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_8','Скачать <b>Артикул/Цена/Количество');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_9','Скачать <b>Артикул/Категория');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_10','Скачать <b>Фругл');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_11','Скачать <b>Артикул/Атрибуты');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_12','файл');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_13','Создать в директории');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_14','Генерировать файл и затем сохранить во временной директории.');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_15','Создать <b>Полный</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_16','Создать <b>Артикул/Цена/Количество</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_17','Создать <b>Артикул/Категория</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_18','Создать <b>Фругл</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_19','Создать <b>Артикул/Атрибуты</b>');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_20','Временная директория:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_21','Временная директория доступная для записи');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_22','Временная директория только для чтения, установите права доступа на запись');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_23','Значение опции magic_quotes:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_24','Значение опции register_globals:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_25','Разделять файл на части по:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_26','Размер поля артикул товара:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_27','Цена с налогом:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_28','Знаков после запятой:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_29','Заменять кавычки:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_30','Разделитель колонок:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_31','табулятор');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_32','запятая');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_33','точка с запятой');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_34','тильда');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_35','тире');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_36','звёздочка');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_37','Сохранение excel файла:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_38','Сохранять табулятор, перевод каретки:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_39','Глубина категорий:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_40','Разрешить атрибуты:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_41','SEO URL в Фругле:');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_42','вкл.');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_43','выкл.');
+define('TEXT_EASYPOPULATE_QUICK_LINKS_44','записей');
+
+define('EASY_ERROR_1', 'Странно, но язык по умолчанию не установлен... Ничего страшного, просто предупреждение... ');
+define('EASY_FILE_LOCATE', 'Вы можете взять Ваш файл в папке ');
+
+define('TEXT_EASYPOPULATE_CLEAR','Вернуться');
+
 define('EASY_UPLOAD_FILE', 'Файл загружен. ');
 define('EASY_UPLOAD_TEMP', 'Имя временного файла: ');
 define('EASY_UPLOAD_USER_FILE', 'Имя файла пользователя: ');
 define('EASY_SIZE', 'Размер: ');
 define('EASY_FILENAME', 'Имя файла: ');
-define('EASY_SPLIT_DOWN', 'Вы можете импортировать разделённые файлы из папки temp');
-define('EASY_UPLOAD_EP_FILE', 'Импортировать файл');
-define('EASY_SPLIT_EP_FILE', 'Загрузить и разделить файл на части');
-define('EASY_INSERT', 'Импортировать');
-define('EASY_SPLIT', 'Разделить');
-define('EASY_LIMIT', 'Настройка экспорта:');
 
-define('TEXT_IMPORT_TEMP', 'Импортирование данных из папки %s<br>');
-define('TEXT_INSERT_INTO_DB', 'Импортировать');
-define('TEXT_SELECT_ONE', 'Выберите файл для импортирования');
-define('TEXT_SPLIT_FILE', 'Выберите файл');
-define('EASY_LABEL_CREATE', 'Экспорт:');
-define('EASY_LABEL_IMPORT', 'Импорт:');
-define('EASY_LABEL_EXPORT_CHARSET', 'Кодировка экспортируемого файла: ');
-define('EASY_LABEL_IMPORT_CHARSET', 'Кодировка импортируемого файла: ');
-define('EASY_LABEL_CREATE_SELECT', 'Способ сохранения экспортируемого файла: ');
-define('EASY_LABEL_CREATE_SAVE', 'Сохранить в папке temp на сервере');
-define('EASY_LABEL_SELECT_DOWN', 'Выберите поля для загрузки: ');
-define('EASY_LABEL_SORT', 'Выберите порядок сортировки: ');
-define('EASY_LABEL_PRODUCT_RANGE', 'Экспортировать товары с ID номером ');
-define('EASY_LABEL_LIMIT_CAT', 'Экспортировать товары из категории: ');
-define('EASY_LABEL_LIMIT_MAN', 'Экспортировать товары производителя: ');
+define('EASY_ERROR_7', 'Не могу открыть файл для чтения.<br />');
 
-define('EASY_LABEL_PRODUCT_AVAIL', 'Доступный диапазон ID номеров: ');
-define('EASY_LABEL_PRODUCT_FROM', ' от ');
-define('EASY_LABEL_PRODUCT_TO', ' до ');
-define('EASY_LABEL_PRODUCT_RECORDS', 'Всего записей: ');
-define('EASY_LABEL_PRODUCT_BEGIN', 'от: ');
-define('EASY_LABEL_PRODUCT_END', 'до: ');
-define('EASY_LABEL_PRODUCT_START', 'Экспортировать');
+define('EASY_LABEL_FILE_COUNT_1', 'Создать файл EP_Split');
 
-define('EASY_FILE_LOCATE', 'Вы можете взять Ваш файл в папке ');
-define('EASY_FILE_LOCATE_2', '');
-define('EASY_FILE_RETURN', ' Вы можете вернуться, нажав на эту ссылку.');
-define('EASY_IMPORT_TEMP_DIR', 'Импортировать и папки temp ');
-define('EASY_LABEL_DOWNLOAD', 'Скачать файл');
-define('EASY_LABEL_COMPLETE', 'Все поля');
-define('EASY_LABEL_TAB', 'tab-delimited .txt file to edit');
-define('EASY_LABEL_MPQ', 'Код товара/Картинка/Название/Цена/Количество');
-define('EASY_LABEL_EP_MC', 'Код товара/Категория');
-define('EASY_LABEL_EP_FROGGLE', 'Файл с данными для системы Фругл');
-define('EASY_LABEL_EP_ATTRIB', 'Атрибуты товара');
-define('EASY_LABEL_EXTRA_FIELDS', 'Дополнительные поля товаров');
-define('EASY_LABEL_NONE', 'Нет');
-define('EASY_LABEL_CATEGORY', 'Корневой раздел');
-define('PULL_DOWN_MANUFACTURES', 'Все производители');
-define('EASY_LABEL_PRODUCT', 'ID номер товара');
-define('EASY_LABEL_MANUFACTURE', 'ID номер производителя');
-define('EASY_LABEL_EP_FROGGLE_HEADER', 'Скачать файл с данными или фругл файл');
-define('EASY_LABEL_EP_MA', 'Код товара/Атрибуты');
-define('EASY_LABEL_EP_FR_TITLE', 'Создать файл с данными или фругл файл в папке temp ');
-define('EASY_LABEL_EP_DOWN_TAB', 'Create <b>Complete</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_MPQ', 'Create <b>Model/Price/Qty</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_MC', 'Create <b>Model/Category</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_MA', 'Create <b>Model/Attributes</b> tab-delimited .txt file in temp dir');
-define('EASY_LABEL_EP_DOWN_FROOGLE', 'Create <b>Froogle</b> tab-delimited .txt file in temp dir');
+define('EASY_LABEL_LINE_COUNT_1', 'Добавлено ');
+define('EASY_LABEL_LINE_COUNT_2', ' записей и файл закрыт... ');
 
-define('EASY_LABEL_NEW_PRODUCT', '<font color=blue> Товар добавлен</font><br>');
+define('EASY_SPLIT_DOWN', 'Вы можете импортировать разделённые файлы из папки');
+
+define('EASY_TEXT_DELETED', 'Удалён товар ');
+define('EASY_TEXT_DELETED1', '.');
+define('EASY_TEXT_NOT_DELETE', 'Не удалён ');
+define('EASY_TEXT_NOT_DELETE1', ' потому что товар не уникальный.');
+
+define('EASY_ERROR_2', '... ОШИБКА! - Слишком много символов в поле код товара.<br>
+			12 символов - это максимальное количество.<br>
+			Максимальная длина поля product_model, установленная в настройках модуля: ');
+define('EASY_ERROR_2A', ' <br>Вы можете либо укоротить код товара, либо увеличить длину поля в базе данных, а затем в /admin/easypopulate.php поменять значение опции EP_MODEL_NUMBER_SIZE.</font>');
+
+define('EASY_LABEL_NEW_PRODUCT', "<font color=blue> Товар добавлен</font><br>");
 define('EASY_LABEL_UPDATED', "<font color=green> Товар обновлён</font><br>");
 define('EASY_LABEL_DELETE_STATUS_1', '<font color=red>Товар</font><font color=black> ');
 define('EASY_LABEL_DELETE_STATUS_2', ' </font><font color=red> удалён!</font>');
-define('EASY_LABEL_LINE_COUNT_1', 'Добавлено ');
-define('EASY_LABEL_LINE_COUNT_2', ' записей и файл закрыт... ');
-define('EASY_LABEL_FILE_COUNT_1', 'Создать файл EP_Split ');
-define('EASY_LABEL_FILE_COUNT_2', '.txt ...  ');
-define('EASY_LABEL_FILE_CLOSE_1', 'Добавлено ');
-define('EASY_LABEL_FILE_CLOSE_2', ' записей и файл закрыт...');
-//errormessages
-define('EASY_ERROR_1', 'Странно, но язык по умолчанию не установлен... Ничего страшного, просто предупреждение... ');
-define('EASY_ERROR_2', '... ОШИБКА! - Слишком много символов в поле код товара.<br>
-			12 символов это максимальное количество в стандартном OsCommerce.<br>
-			Максимальная длина поля product_model, установленная в настройках модуля: ');
-define('EASY_ERROR_2A', ' <br>Вы можете либо укоротить код товара, либо увеличить длину поля в базе данных.</font>');
-define('EASY_ERROR_2B',  "<font color='red'>");
-define('EASY_ERROR_3', '<p class=smallText>Не заполнено поле products_id. Данная строка не была импортирована. <br><br>');
-define('EASY_ERROR_4', '<font color=red>ОШИБКА! - v_customer_group_id and v_customer_price must occur in pairs</font>');
-define('EASY_ERROR_5', '</b><font color=red>ОШИБКА! - You are trying to use a file created with EP Advanced, please try with Easy Populate Advanced </font>');
-define('EASY_ERROR_5a', '<font color=red><b><u>  Click here to return to Easy Populate Basic </u></b></font>');
-define('EASY_ERROR_6', '</b><font color=red>ОШИБКА! - You are trying to use a file created with EP Basic, please try with Easy Populate Basic </font>');
-define('EASY_ERROR_6a', '<font color=red><b><u>  Click here to return to Easy Populate Advanced </u></b></font>');
 
-define('EASY_LABEL_FILE_COUNT_1A', 'Создаём файл EPA_Split ');
+define('EASY_LABEL_TEXT_NO_MODEL', 'Не найден артикул товара. Данная строка не импортирована: ');
+
+define('EASY_EXTRA_FIELD_UPDATED', 'Дополнительные поля обновлены');
+define('EASY_EXTRA_FIELD_ADDED', 'Дополнительные поля добавлены');
 
 ?>
