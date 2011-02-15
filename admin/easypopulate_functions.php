b3b20c3640261dbae3eb5129bb65db1aaacedac7
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-02-15 17:30:47 +0000

    Корректировка в модуле Excel импорт/экспорт

--- admin/easypopulate_functions.php
+++ admin/easypopulate_functions.php
@@ -1,16 +1,21 @@
 <?php
-/*
-  $Id: easypopulate_functions.php,v 2.76a 2006/10/16 22:50:52 surfalot Exp $
+/* --------------------------------------------------------------
+   $Id: easypopulate_functions.php 899 2011-02-07 17:36:57 oleg_vamsoft $
 
-  Designed for osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
 
-  Copyright (c) 2003 osCommerce
+   Copyright (c) 2011 VaM Shop
+   --------------------------------------------------------------
+   based on: 
+   (c) 2003	 osCommerce (easypopulate.php,v 1.4 2003/08/14); oscommerce.com
 
-  Released under the GNU General Public License
-*/
-if (!function_exists(tep_get_uploaded_file)){
-function tep_get_uploaded_file($filename) {
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+   
+if (!function_exists(vam_get_uploaded_file)){
+function vam_get_uploaded_file($filename) {
 	if (isset($_FILES[$filename])) {
 		$uploaded_file = array('name' => $_FILES[$filename]['name'],
 		'type' => $_FILES[$filename]['type'],
@@ -36,7 +41,7 @@ return $uploaded_file;
 
 // the $filename parameter is an array with the following elements:
 // name, type, size, tmp_name
-function tep_copy_uploaded_file($filename, $target) {
+function vam_copy_uploaded_file($filename, $target) {
 	if (substr($target, -1) != '/') $target .= '/';
 
 	$target .= $filename['name'];
@@ -47,25 +52,25 @@ function tep_copy_uploaded_file($filename, $target) {
 ////
 // Recursively go through the categories and retreive all sub-categories IDs
 // TABLES: categories
-if (!function_exists(tep_get_sub_categories)) {
-  function tep_get_sub_categories(&$categories, $categories_id) {
-    $sub_categories_query = tep_db_query("select categories_id from " . TABLE_CATEGORIES . " where parent_id = '" . (int)$categories_id . "'");
-    while ($sub_categories = tep_db_fetch_array($sub_categories_query)) {
+if (!function_exists(vam_get_sub_categories)) {
+  function vam_get_sub_categories(&$categories, $categories_id) {
+    $sub_categories_query = vam_db_query("select categories_id from " . TABLE_CATEGORIES . " where parent_id = '" . (int)$categories_id . "'");
+    while ($sub_categories = vam_db_fetch_array($sub_categories_query)) {
       if ($sub_categories['categories_id'] == 0) return true;
       $categories[sizeof($categories)] = $sub_categories['categories_id'];
       if ($sub_categories['categories_id'] != $categories_id) {
-        tep_get_sub_categories($categories, $sub_categories['categories_id']);
+        vam_get_sub_categories($categories, $sub_categories['categories_id']);
       }
     }
   }
 }
 
-if (!function_exists(tep_get_tax_class_rate)){
-function tep_get_tax_class_rate($tax_class_id) {
+if (!function_exists(vam_get_tax_class_rate)){
+function vam_get_tax_class_rate($tax_class_id) {
 	$tax_multiplier = 0;
-	$tax_query = tep_db_query("select SUM(tax_rate) as tax_rate from " . TABLE_TAX_RATES . " WHERE  tax_class_id = '" . $tax_class_id . "' GROUP BY tax_priority");
-	if (tep_db_num_rows($tax_query)) {
-		while ($tax = tep_db_fetch_array($tax_query)) {
+	$tax_query = vam_db_query("select SUM(tax_rate) as tax_rate from " . TABLE_TAX_RATES . " WHERE  tax_class_id = '" . $tax_class_id . "' GROUP BY tax_priority");
+	if (vam_db_num_rows($tax_query)) {
+		while ($tax = vam_db_fetch_array($tax_query)) {
 			$tax_multiplier += $tax['tax_rate'];
 		}
 	}
@@ -73,10 +78,10 @@ function tep_get_tax_class_rate($tax_class_id) {
 }
 }
 
-if (!function_exists(tep_get_tax_title_class_id)){
-function tep_get_tax_title_class_id($tax_class_title) {
-	$classes_query = tep_db_query("select tax_class_id from " . TABLE_TAX_CLASS . " WHERE tax_class_title = '" . $tax_class_title . "'" );
-	$tax_class_array = tep_db_fetch_array($classes_query);
+if (!function_exists(vam_get_tax_title_class_id)){
+function vam_get_tax_title_class_id($tax_class_title) {
+	$classes_query = vam_db_query("select tax_class_id from " . TABLE_TAX_CLASS . " WHERE tax_class_title = '" . $tax_class_title . "'" );
+	$tax_class_array = vam_db_fetch_array($classes_query);
 	$tax_class_id = $tax_class_array['tax_class_id'];
 	return $tax_class_id ;
 }
