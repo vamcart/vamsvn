9220ba18053640f6983d9088ebcfe94538018930
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-02-19 11:02:05 +0000

    Добавлен jQuery Validation на страницу регистрации клиента

--- trunk/create_account.php
+++ trunk/create_account.php
@@ -429,33 +429,28 @@ if (ACCOUNT_GENDER == 'true') {
 
 	$vamTemplate->assign('INPUT_MALE', vam_draw_radio_field(array ('name' => 'gender', 'suffix' => MALE), 'm', '', 'id="gender" checked="checked"'));
 	$vamTemplate->assign('INPUT_FEMALE', vam_draw_radio_field(array ('name' => 'gender', 'suffix' => FEMALE, 'text' => (vam_not_null(ENTRY_GENDER_TEXT) ? '<span class="Requirement">'.ENTRY_GENDER_TEXT.'</span>' : '')), 'f', '', 'id="gender"'));
-   $vamTemplate->assign('ENTRY_GENDER_ERROR', ENTRY_GENDER_ERROR);
 
 } else {
 	$vamTemplate->assign('gender', '0');
 }
 
 $vamTemplate->assign('INPUT_FIRSTNAME', vam_draw_input_fieldNote(array ('name' => 'firstname', 'text' => '&nbsp;'. (vam_not_null(ENTRY_FIRST_NAME_TEXT) ? '<span class="Requirement">'.ENTRY_FIRST_NAME_TEXT.'</span>' : '')), '', 'id="firstname"'));
-$vamTemplate->assign('ENTRY_FIRST_NAME_ERROR', ENTRY_FIRST_NAME_ERROR);
 if (ACCOUNT_SECOND_NAME == 'true') {
 	$vamTemplate->assign('secondname', '1');
 $vamTemplate->assign('INPUT_SECONDNAME', vam_draw_input_fieldNote(array ('name' => 'secondname', 'text' => '&nbsp;'. (vam_not_null(ENTRY_SECOND_NAME_TEXT) ? '<span class="Requirement">'.ENTRY_SECOND_NAME_TEXT.'</span>' : '')), '', 'id="secondname"'));
 }
 $vamTemplate->assign('INPUT_LASTNAME', vam_draw_input_fieldNote(array ('name' => 'lastname', 'text' => '&nbsp;'. (vam_not_null(ENTRY_LAST_NAME_TEXT) ? '<span class="Requirement">'.ENTRY_LAST_NAME_TEXT.'</span>' : '')), '', 'id="lastname"'));
-$vamTemplate->assign('ENTRY_LAST_NAME_ERROR', ENTRY_LAST_NAME_ERROR);
 
 if (ACCOUNT_DOB == 'true') {
 	$vamTemplate->assign('birthdate', '1');
 
 	$vamTemplate->assign('INPUT_DOB', vam_draw_input_fieldNote(array ('name' => 'dob', 'text' => '&nbsp;'. (vam_not_null(ENTRY_DATE_OF_BIRTH_TEXT) ? '<span class="Requirement">'.ENTRY_DATE_OF_BIRTH_TEXT.'</span>' : '')), '', 'id="dob"'));
-   $vamTemplate->assign('ENTRY_DATE_OF_BIRTH_ERROR', ENTRY_DATE_OF_BIRTH_ERROR);
 
 } else {
 	$vamTemplate->assign('birthdate', '0');
 }
 
 $vamTemplate->assign('INPUT_EMAIL', vam_draw_input_fieldNote(array ('name' => 'email_address', 'text' => '&nbsp;'. (vam_not_null(ENTRY_EMAIL_ADDRESS_TEXT) ? '<span class="Requirement">'.ENTRY_EMAIL_ADDRESS_TEXT.'</span>' : '')), '', 'id="email_address"'));
-$vamTemplate->assign('ENTRY_EMAIL_ADDRESS_ERROR', ENTRY_EMAIL_ADDRESS_ERROR);
 
 $vamTemplate->assign('INPUT_NEWSLETTER', vam_draw_checkbox_field('newsletter', '1', true));
 
@@ -476,7 +471,6 @@ if (ACCOUNT_COMPANY_VAT_CHECK == 'true') {
 if (ACCOUNT_STREET_ADDRESS == 'true') {
    $vamTemplate->assign('street_address', '1');
    $vamTemplate->assign('INPUT_STREET', vam_draw_input_fieldNote(array ('name' => 'street_address', 'text' => '&nbsp;'. (vam_not_null(ENTRY_STREET_ADDRESS_TEXT) ? '<span class="Requirement">'.ENTRY_STREET_ADDRESS_TEXT.'</span>' : '')), '', 'id="street_address"'));
-   $vamTemplate->assign('ENTRY_STREET_ADDRESS_ERROR', ENTRY_STREET_ADDRESS_ERROR);
 } else {
 	$vamTemplate->assign('street_address', '0');
 }
@@ -492,7 +486,6 @@ if (ACCOUNT_SUBURB == 'true') {
 if (ACCOUNT_POSTCODE == 'true') {
 	$vamTemplate->assign('postcode', '1');
    $vamTemplate->assign('INPUT_CODE', vam_draw_input_fieldNote(array ('name' => 'postcode', 'text' => '&nbsp;'. (vam_not_null(ENTRY_POST_CODE_TEXT) ? '<span class="Requirement">'.ENTRY_POST_CODE_TEXT.'</span>' : '')), '', 'id="postcode"'));
-   $vamTemplate->assign('ENTRY_POST_CODE_ERROR', ENTRY_POST_CODE_ERROR);
 
 } else {
 	$vamTemplate->assign('postcode', '0');
@@ -501,7 +494,6 @@ if (ACCOUNT_POSTCODE == 'true') {
 if (ACCOUNT_CITY == 'true') {
 	$vamTemplate->assign('city', '1');
    $vamTemplate->assign('INPUT_CITY', vam_draw_input_fieldNote(array ('name' => 'city', 'text' => '&nbsp;'. (vam_not_null(ENTRY_CITY_TEXT) ? '<span class="Requirement">'.ENTRY_CITY_TEXT.'</span>' : '')), '', 'id="city"'));
-   $vamTemplate->assign('ENTRY_CITY_ERROR', ENTRY_CITY_ERROR);
 } else {
 	$vamTemplate->assign('city', '0');
 }
@@ -560,7 +552,6 @@ if (ACCOUNT_STATE == 'true') {
 //	}
 
 	$vamTemplate->assign('INPUT_STATE', $state_input);
-   $vamTemplate->assign('ENTRY_STATE_ERROR_SELECT', ENTRY_STATE_ERROR_SELECT);
 } else {
 	$vamTemplate->assign('state', '0');
 }
@@ -579,8 +570,6 @@ if (ACCOUNT_COUNTRY == 'true') {
    
 //   $vamTemplate->assign('SELECT_COUNTRY_NOSCRIPT', '<noscript><br />' . vam_image_submit('button_update.gif', IMAGE_BUTTON_UPDATE, 'name=loadStateXML') . '<br />' . ENTRY_STATE_RELOAD . '</noscript>');
 
-   $vamTemplate->assign('ENTRY_COUNTRY_ERROR', ENTRY_COUNTRY_ERROR);
-
 } else {
 	$vamTemplate->assign('country', '0');
 }
@@ -588,7 +577,6 @@ if (ACCOUNT_COUNTRY == 'true') {
 if (ACCOUNT_TELE == 'true') {
 	$vamTemplate->assign('telephone', '1');
    $vamTemplate->assign('INPUT_TEL', vam_draw_input_fieldNote(array ('name' => 'telephone', 'text' => '&nbsp;'. (vam_not_null(ENTRY_TELEPHONE_NUMBER_TEXT) ? '<span class="Requirement">'.ENTRY_TELEPHONE_NUMBER_TEXT.'</span>' : '')), '', 'id="telephone"'));
-   $vamTemplate->assign('ENTRY_TELEPHONE_NUMBER_ERROR', ENTRY_TELEPHONE_NUMBER_ERROR);
 } else {
 	$vamTemplate->assign('telephone', '0');
 }
@@ -604,9 +592,7 @@ if (ACCOUNT_FAX == 'true') {
    $vamTemplate->assign('INPUT_CUSTOMERS_EXTRA_FIELDS', vam_get_extra_fields($_SESSION['customer_id'],$_SESSION['languages_id']));
 
 $vamTemplate->assign('INPUT_PASSWORD', vam_draw_password_fieldNote(array ('name' => 'password', 'text' => '&nbsp;'. (vam_not_null(ENTRY_PASSWORD_TEXT) ? '<span class="Requirement">'.ENTRY_PASSWORD_TEXT.'</span>' : '')), '', 'id="pass"'));
-$vamTemplate->assign('ENTRY_PASSWORD_ERROR', ENTRY_PASSWORD_ERROR);
 $vamTemplate->assign('INPUT_CONFIRMATION', vam_draw_password_fieldNote(array ('name' => 'confirmation', 'text' => '&nbsp;'. (vam_not_null(ENTRY_PASSWORD_CONFIRMATION_TEXT) ? '<span class="Requirement">'.ENTRY_PASSWORD_CONFIRMATION_TEXT.'</span>' : '')), '', 'id="confirmation"'));
-$vamTemplate->assign('ENTRY_PASSWORD_ERROR_NOT_MATCHING', ENTRY_PASSWORD_ERROR_NOT_MATCHING);
 $vamTemplate->assign('FORM_END', '</form>');
 $vamTemplate->assign('language', $_SESSION['language']);
 $vamTemplate->caching = 0;
--- trunk/includes/form_check.js.php
+++ trunk/includes/form_check.js.php
@@ -19,279 +19,119 @@
 
 
 ?>
+<script type="text/javascript" src="jscript/jquery/plugins/validate/jquery.validate.pack.js"></script>
 <script type="text/javascript"><!--
-var form = "";
-var submitted = false;
-var error = false;
-var error_message = "";
 
-function check_input(field_name, field_size, message) {
-  if (form.elements[field_name] && (form.elements[field_name].type != "hidden")) {
-    var field_value = form.elements[field_name].value;
-
-    if (field_value == '' || field_value.length < field_size) {
-      error_message = error_message + "* " + message + "\n";
-      error = true;
-    }
-  }
-}
-
-function check_radio(field_name, message) {
-  var isChecked = false;
-
-  if (form.elements[field_name] && (form.elements[field_name].type != "hidden")) {
-    var radio = form.elements[field_name];
-
-    for (var i=0; i<radio.length; i++) {
-      if (radio[i].checked == true) {
-        isChecked = true;
-        break;
-      }
-    }
-
-    if (isChecked == false) {
-      error_message = error_message + "* " + message + "\n";
-      error = true;
-    }
-  }
-}
-
-function check_select(field_name, field_default, message) {
-  if (form.elements[field_name] && (form.elements[field_name].type != "hidden")) {
-    var field_value = form.elements[field_name].value;
-
-    if (field_value == field_default) {
-      error_message = error_message + "* " + message + "\n";
-      error = true;
-    }
-  }
-}
-
-function check_password(field_name_1, field_name_2, field_size, message_1, message_2) {
-  if (form.elements[field_name_1] && (form.elements[field_name_1].type != "hidden")) {
-    var password = form.elements[field_name_1].value;
-    var confirmation = form.elements[field_name_2].value;
-
-    if (password == '' || password.length < field_size) {
-      error_message = error_message + "* " + message_1 + "\n";
-      error = true;
-    } else if (password != confirmation) {
-      error_message = error_message + "* " + message_2 + "\n";
-      error = true;
-    }
-  }
-}
-
-function check_password_new(field_name_1, field_name_2, field_name_3, field_size, message_1, message_2, message_3) {
-  if (form.elements[field_name_1] && (form.elements[field_name_1].type != "hidden")) {
-    var password_current = form.elements[field_name_1].value;
-    var password_new = form.elements[field_name_2].value;
-    var password_confirmation = form.elements[field_name_3].value;
-
-    if (password_current == '' || password_current.length < field_size) {
-      error_message = error_message + "* " + message_1 + "\n";
-      error = true;
-    } else if (password_new == '' || password_new.length < field_size) {
-      error_message = error_message + "* " + message_2 + "\n";
-      error = true;
-    } else if (password_new != password_confirmation) {
-      error_message = error_message + "* " + message_3 + "\n";
-      error = true;
-    }
-  }
-}
-
-function check_form(form_name) {
-  if (submitted == true) {
-    alert(unescape("<?php echo vam_js_lang(JS_ERROR_SUBMITTED); ?>"));
-    return false;
-  }
-  
-  
-
-  error = false;
-  form = form_name;
-  error_message = unescape("<?php echo vam_js_lang(JS_ERROR); ?>");
-
-<?php if (ACCOUNT_GENDER == 'true') echo '  check_radio("gender", "' . ENTRY_GENDER_ERROR . '");' . "\n"; ?>
-
-  check_input("firstname", <?php echo ENTRY_FIRST_NAME_MIN_LENGTH; ?>, "<?php echo vam_js_lang(ENTRY_FIRST_NAME_ERROR); ?>");
-  check_input("lastname", <?php echo ENTRY_LAST_NAME_MIN_LENGTH; ?>, "<?php echo vam_js_lang(ENTRY_LAST_NAME_ERROR); ?>");
-
-<?php if (ACCOUNT_DOB == 'true') echo '  check_input("dob", ' . ENTRY_DOB_MIN_LENGTH . ', "' . vam_js_lang(ENTRY_DATE_OF_BIRTH_ERROR) . '");' . "\n"; ?>
-
-  check_input("email_address", <?php echo ENTRY_EMAIL_ADDRESS_MIN_LENGTH; ?>, "<?php echo vam_js_lang(ENTRY_EMAIL_ADDRESS_ERROR); ?>");
-
-<?php if (ACCOUNT_STREET_ADDRESS == 'true') echo '  check_input("street_address", ' . ENTRY_STREET_ADDRESS_MIN_LENGTH . ', "' . vam_js_lang(ENTRY_STREET_ADDRESS_ERROR) . '");' . "\n"; ?>
-
-<?php if (ACCOUNT_POSTCODE == 'true') echo '  check_input("postcode", ' . ENTRY_POSTCODE_MIN_LENGTH . ', "' . vam_js_lang(ENTRY_POST_CODE_ERROR) . '");' . "\n"; ?>
-
-<?php if (ACCOUNT_CITY == 'true') echo '  check_input("city", ' . ENTRY_CITY_MIN_LENGTH . ', "' . vam_js_lang(ENTRY_CITY_ERROR) . '");' . "\n"; ?>
-
-<?php if (ACCOUNT_STATE == 'true') echo '  check_input("state", ' . ENTRY_STATE_MIN_LENGTH . ', "' . vam_js_lang(ENTRY_STATE_ERROR) . '");' . "\n"; ?>
-
-<?php if (ACCOUNT_COUNTRY == 'true') echo ' check_select("country", "", "' . vam_js_lang(ENTRY_COUNTRY_ERROR) . '");' . "\n"; ?>
-
-<?php if (ACCOUNT_TELE == 'true') echo '  check_input("telephone", ' . ENTRY_TELEPHONE_MIN_LENGTH . ', "' . vam_js_lang(ENTRY_TELEPHONE_NUMBER_ERROR) . '");' . "\n"; ?>
-
-  check_password("password", "confirmation", <?php echo ENTRY_PASSWORD_MIN_LENGTH; ?>, "<?php echo vam_js_lang(ENTRY_PASSWORD_ERROR); ?>", "<?php echo vam_js_lang(ENTRY_PASSWORD_ERROR_NOT_MATCHING); ?>");
-  check_password_new("password_current", "password_new", "password_confirmation", <?php echo vam_js_lang(ENTRY_PASSWORD_MIN_LENGTH); ?>, "<?php echo vam_js_lang(ENTRY_PASSWORD_ERROR); ?>", "<?php echo vam_js_lang(ENTRY_PASSWORD_NEW_ERROR); ?>", "<?php echo vam_js_lang(ENTRY_PASSWORD_NEW_ERROR_NOT_MATCHING); ?>");
-
-  if (error == true) {
-    alert(unescape(error_message));
-    return false;
-  } else {
-    submitted = true;
-    return true;
-  }
-}
-
-		function checkform(of)
-		{
-		// Test if DOM is available and there is an element called required
-			if(!document.getElementById || !document.createTextNode){return;}
-			if(!document.getElementById('required')){return;}
-
-		// Define error messages and split the required fields
-			var errorID='errormsg';
-			var errorClass='error'
-/* changes for linked list */
-			var errorMsg='<?php echo JS_ERROR; ?>';
-/* end changes for linked list */
-			var errorImg='images/icons/warning.gif';
-			var errorAlt='<?php echo ICON_ERROR; ?>';
-			var errorTitle='<?php echo ICON_ERROR; ?>';
-			var reqfields=document.getElementById('required').value.split(',');
-
-		// Cleanup old mess
-			// if there is an old errormessage field, delete it
-			if(document.getElementById(errorID))
-			{
-				var em=document.getElementById(errorID);
-				em.parentNode.removeChild(em);
-			}
-			// remove old images and classes from the required fields
-			for(var i=0;i<reqfields.length;i++)
-			{
-				var f=document.getElementById(reqfields[i]);
-				if(!f){continue;}
-				if(f.previousSibling && /img/i.test(f.previousSibling.nodeName))
-				{
-					f.parentNode.removeChild(f.previousSibling);
-				}
-				f.className='';
-			}
-		// loop over required fields
-			for(var i=0;i<reqfields.length;i++)
-			{
-		// check if required field is there
-				var f=document.getElementById(reqfields[i]);
-				if(!f){continue;}
-		// test if the required field has an error, 
-		// according to its type
-				switch(f.type.toLowerCase())
-				{
-					case 'text':
-						if(f.name=='firstname' && f.value.length<<?php echo ENTRY_FIRST_NAME_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='lastname' && f.value.length<<?php echo ENTRY_FIRST_NAME_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='dob' && f.value=='' && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='street_address' && f.value.length<<?php echo ENTRY_STREET_ADDRESS_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='postcode' && f.value.length<<?php echo ENTRY_POSTCODE_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='city' && f.value.length<<?php echo ENTRY_CITY_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='telephone' && f.value.length<<?php echo ENTRY_TELEPHONE_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='state' && f.value.length<<?php echo ENTRY_STATE_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-		// email is a special field and needs checking
-						if(f.id=='email' && !cf_isEmailAddr(f.value)){cf_adderr(f)}							
-					break;
-					case 'password':
-						if(f.name=='password' && f.value.length<<?php echo ENTRY_PASSWORD_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='confirmation' && f.value.length<<?php echo ENTRY_PASSWORD_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='password_current' && f.value.length<<?php echo ENTRY_PASSWORD_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='password_new' && f.value.length<<?php echo ENTRY_PASSWORD_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-						if(f.name=='password_confirmation' && f.value.length<<?php echo ENTRY_PASSWORD_MIN_LENGTH; ?> && f.id!='email'){cf_adderr(f)}				
-					break;
-					case 'textarea':
-						if(f.value==''){cf_adderr(f)}							
-					break;
-					case 'checkbox':
-						if(!f.checked){cf_adderr(f)}							
-					break;
-					case 'radio':
-						if(f.value=='f' && !f.checked){cf_adderr(f)}							
-					break;
-					case 'select-one':
-						if(!f.selectedIndex && f.selectedIndex==1){cf_adderr(f)}							
-					break;
-				}
-			}
-			return !document.getElementById(errorID);
-
-			/* Tool methods */
-			function cf_adderr(o)
-			{
-				// create image, add to and colourise the error fields
-				var errorIndicator=document.createElement('img');
-				errorIndicator.alt=errorAlt;
-				errorIndicator.src=errorImg;
-				errorIndicator.title=errorTitle;
-				o.className=errorClass;
-				o.parentNode.insertBefore(errorIndicator,o);
-
-			// Check if there is no error message
-				if(!document.getElementById(errorID))
-				{
-				// create errormessage and insert before submit button
-					var em=document.createElement('div');
-					em.id=errorID;
-					var newp=document.createElement('p');
-					newp.appendChild(document.createTextNode(errorMsg))
-					em.appendChild(newp);
-/* added for linked list */
-					var newul=document.createElement('ul');		
-					em.appendChild(newul);
-/* end added for linked list */
-					// find the submit button 
-					for(var i=0;i<of.getElementsByTagName('input').length;i++)
-					{
-						if(/image/i.test(of.getElementsByTagName('input')[i].type))
-						{
-							var sb=of.getElementsByTagName('input')[i];
-							break;
-						}
-					}
-					if(sb)
-					{
-						sb.parentNode.insertBefore(em,sb);
-					}	
-				} 
-/* added for linked list */
-				var em=document.getElementById(errorID).getElementsByTagName('ul')[0];
-				var newli=document.createElement('li');
-				var newa=document.createElement('a');
-				for(var i=0;i<of.getElementsByTagName('label').length;i++)
-				{
-					if(of.getElementsByTagName('label')[i].htmlFor==o.id)
-					{
-						var txt=of.getElementsByTagName('label')[i].firstChild.nodeValue + ' ' + of.getElementsByTagName('label')[i].title;
-						break;
-					}
-				}
-	
-				newa.appendChild(document.createTextNode(txt));
-				newa.href='#'+f.id;
-				newa.onclick=function()
-				{
-					var loc=this.href.match(/#(\w.+)/)[1];
-					document.getElementById(loc).focus();
-					return false;
-				}
-				newli.appendChild(newa);
-				em.appendChild(newli);
-/* end added for linked list */
-			}
-			function cf_isEmailAddr(str) 
-			{
-			    return str.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/);
+jQuery(document).ready(function() {
+	// validate signup form on keyup and submit
+	jQuery("#create_account").validate({
+		rules: {
+			gender: "required",
+			firstname: {
+				required: true,
+				minlength: <?php echo ENTRY_FIRST_NAME_MIN_LENGTH; ?>
+			},
+			lastname: {
+				required: true,
+				minlength: <?php echo ENTRY_LAST_NAME_MIN_LENGTH; ?>
+			},
+			dob: {
+				required: true,
+				minlength: <?php echo ENTRY_DOB_MIN_LENGTH; ?>
+			},
+			email_address: {
+				required: true,
+				minlength: <?php echo ENTRY_EMAIL_ADDRESS_MIN_LENGTH; ?>,
+				email: true
+			},
+			street_address: {
+				required: true,
+				minlength: <?php echo ENTRY_STREET_ADDRESS_MIN_LENGTH; ?>
+			},
+			postcode: {
+				required: true,
+				minlength: <?php echo ENTRY_POSTCODE_MIN_LENGTH; ?>
+			},
+			city: {
+				required: true,
+				minlength: <?php echo ENTRY_CITY_MIN_LENGTH; ?>
+			},
+			//state: {
+				//required: true,
+				//minlength: <?php echo ENTRY_STATE_MIN_LENGTH; ?>
+			//},
+			//country: {
+				//required: true,
+				//minlength: <?php echo ENTRY_STATE_MIN_LENGTH; ?>
+			//},
+			telephone: {
+				required: true,
+				minlength: <?php echo ENTRY_TELEPHONE_MIN_LENGTH; ?>
+			},
+			password: {
+				required: true,
+				minlength: <?php echo ENTRY_PASSWORD_MIN_LENGTH; ?>
+			},
+			confirmation: {
+				required: true,
+				minlength: <?php echo ENTRY_PASSWORD_MIN_LENGTH; ?>,
+				equalTo: "#pass"
+			},
+		},
+		messages: {
+			gender: "<?php echo ENTRY_GENDER_ERROR; ?>",
+			firstname: {
+				required: "<?php echo ENTRY_FIRST_NAME_ERROR; ?>",
+				minlength: "<?php echo ENTRY_FIRST_NAME_ERROR; ?>"
+			},
+			lastname: {
+				required: "<?php echo ENTRY_LAST_NAME_ERROR; ?>",
+				minlength: "<?php echo ENTRY_LAST_NAME_ERROR; ?>"
+			},
+			dob: {
+				required: "<?php echo ENTRY_DATE_OF_BIRTH_ERROR; ?>",
+				minlength: "<?php echo ENTRY_DATE_OF_BIRTH_ERROR; ?>"
+			},
+			email_address: {
+				required: "<?php echo ENTRY_EMAIL_ADDRESS_ERROR; ?>",
+				minlength: "<?php echo ENTRY_EMAIL_ADDRESS_ERROR; ?>"
+			},
+			street_address: {
+				required: "<?php echo ENTRY_STREET_ADDRESS_ERROR; ?>",
+				minlength: "<?php echo ENTRY_STREET_ADDRESS_ERROR; ?>"
+			},
+			postcode: {
+				required: "<?php echo ENTRY_POST_CODE_ERROR; ?>",
+				minlength: "<?php echo ENTRY_POST_CODE_ERROR; ?>"
+			},
+			city: {
+				required: "<?php echo ENTRY_CITY_ERROR; ?>",
+				minlength: "<?php echo ENTRY_CITY_ERROR; ?>"
+			},
+			//state: {
+				//required: "<?php echo ENTRY_STATE_ERROR_SELECT; ?>",
+				//minlength: "<?php echo ENTRY_STATE_ERROR_SELECT; ?>"
+			//},
+			//country: {
+				//required: "<?php echo ENTRY_COUNTRY_ERROR; ?>",
+				//minlength: "<?php echo ENTRY_COUNTRY_ERROR; ?>"
+			//},
+			telephone: {
+				required: "<?php echo ENTRY_TELEPHONE_NUMBER_ERROR; ?>",
+				minlength: "<?php echo ENTRY_TELEPHONE_NUMBER_ERROR; ?>"
+			},
+			password: {
+				required: "<?php echo ENTRY_PASSWORD_ERROR; ?>",
+				minlength: "<?php echo ENTRY_PASSWORD_CURRENT_ERROR; ?>"
+			},
+			confirmation: {
+				required: "<?php echo ENTRY_PASSWORD_ERROR_NOT_MATCHING; ?>",
+				minlength: "<?php echo ENTRY_PASSWORD_CURRENT_ERROR; ?>",
+				equalTo: "<?php echo ENTRY_PASSWORD_ERROR_NOT_MATCHING; ?>"
 			}
 		}
+	});
+	
+});
+
 //--></script>
