b6c7f1bccbaf8cb9fba3a0339ea02949c096450b
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-03-13 09:36:58 +0000

    Корректировка в модуле оплаты LiqPAY

--- liqpay.php
+++ liqpay.php
@@ -34,9 +34,11 @@ require (DIR_WS_CLASSES.'order.php');
 
 // variables prepearing
 
-$xml_decoded=base64_decode($_POST['operation_xml']);
+$xml_decoded=base64_decode($_POST['xml']);
 
 $xml = simplexml_load_string($xml_decoded);
+
+$order = new order($xml->order_id);
  
 // checking and handling
 if ($xml->status == 'success') {
