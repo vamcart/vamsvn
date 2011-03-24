fef91b2910ee55b35f79d791706a54f9ebd8c1b1
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-03-24 08:50:47 +0000

    Корректировка кода

--- trunk/templates/vamshop/source/boxes/params_filters.php
+++ trunk/templates/vamshop/source/boxes/params_filters.php
@@ -12,129 +12,88 @@
    ---------------------------------------------------------------------------------------*/
 
 require_once(DIR_WS_FUNCTIONS."params_filters.php");
- 
+
 //$categories_id = intval($_GET['cat']);
 $categories_id = intval($current_category_id);
 $parametersNames = getAllParameters($categories_id);
-
 $selected = get_selected();
-//var_dump($selected);
 $selectedGroups = get_selected_groups( $selected );
-
 //$items = get_products($categories_id, $selectedGroups);
 $filterParams = get_parameters_by_categories($categories_id);
 
 $query = $_GET['q'];
-$query1 = $_GET['p'];
 if(empty($query))
 { $blocks = array(); }
-else { $blocks = preg_split('/-/', $query );}  
-if(empty($query1))
-{ $blocks1 = array(); }
-else { $blocks1 = preg_split('/-/', $query1 );}
+else { $blocks = preg_split('/-/', $query );}
 //print "<pre>";
 //print_r($filterParams);
 //*
 $price_query = "";
 if(isset($_GET['price_min']) && intval($_GET['price_min']) != 0 )
 {
-    $price_query .= "&price_min=".intval($_GET['price_min']);
+	$price_query .= "&price_min=".intval($_GET['price_min']);
 }
 if(isset($_GET['price_max']) && intval($_GET['price_max']) != 0 )
 {
-    $price_query .= "&price_max=".intval($_GET['price_max']);
+	$price_query .= "&price_max=".intval($_GET['price_max']);
 }
 
 $selectedParamsFilters = array();
 $j = 0;
-$title_line = ""; 
-if( (count($blocks) > 0) and ((count($blocks1) > 0)) ){ 
+$title_line = "";
 foreach($selected as $block => $blockItems)
 {
-    $title_block = array();
-    for($i = 0; $i < count($blockItems); $i++)
-    {   
-     if(in_array( $blockItems[$i]["products_parameters_id"], $blocks1) ){
-            $set_query1 = str_replace("-".$blockItems[$i]["products_parameters_id"]."-", "-", "-".$query1."-");
-            $set_query1 = str_replace("--", "-", $set_query1);
-            $set_query1 = trim($set_query1, "-");
-            $poz = array_search($blockItems[$i]["products_parameters_id"], $blocks1);
-        }else{
-           // $set_query1 = $query1;
-        }     
-            $set_query=$blocks;
-            $set_query[$poz] = "";
-            $set_query = implode("-",$set_query);
-            $set_query = str_replace("--", "-", $set_query);
-            $set_query = str_replace("--", "-", $set_query);
-            $set_query = trim($set_query, "-");
- 
-     
-        $blockItems[$i]['set_query'] = $set_query.$price_query;
-        $blockItems[$i]['url'] = vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories['categories_name']) .'&q='.$set_query.$price_query.'&p='.$set_query1.$price_query);
-        $title_block[] = $blockItems[$i]["parameters_value"];
-    }
-    $selectedParamsFilters[$j]["name"] = $parametersNames[$block];
-    $selectedParamsFilters[$j]["list"] = $blockItems;
-    $title_line = $parametersNames[$block] . " " . join(', ', $title_block);
-    $j++;
+	$title_block = array();
+	for($i = 0; $i < count($blockItems); $i++)
+	{		
+		if(in_array( $blockItems[$i]["products_parameters_values_id"], $blocks) ){
+			$set_query = str_replace("-".$blockItems[$i]["products_parameters_values_id"]."-", "-", "-".$query."-");
+			$set_query = str_replace("--", "-", $set_query);
+			$set_query = trim($set_query, "-");
+		}else{
+			$set_query = $query;
+		}	
+		$blockItems[$i]['set_query'] = $set_query.$price_query;
+		$blockItems[$i]['url'] = vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories['categories_name']) .'&q='.$set_query.$price_query);
+		$title_block[] = $blockItems[$i]["parameters_value"];
+	}
+	$selectedParamsFilters[$j]["name"] = $parametersNames[$block];
+	$selectedParamsFilters[$j]["list"] = $blockItems;
+	$title_line = $parametersNames[$block] . " " . join(', ', $title_block);
+	$j++;
 }
-}
-//var_dump($filterParams);
 
-$mainParamsFilters = array(); 
-foreach ($filterParams as $key => $value) {
-
-for($i = 0; $i < count($filterParams[$key]); $i++) 
+$mainParamsFilters = array();
+for($i = 0; $i < count($filterParams); $i++)
 {
-    $count_opened = $filterParams[$key][$i]["products_parameters_maxopened"];
-    $values = get_parameters_block( $filterParams[$key][$i]["products_parameters_id"] , $selectedGroups); 
-     if( count($values) == 0 )
-    {
-        continue;
-    }
-    for($j = 0; $j < count($values); $j++)
-    {
-
-        if(!in_array( $values[$j]["products_parameters_values_id"], $blocks) ){
-            $set_query = $query.'-'.$values[$j]["products_parameters_values_id"];
-            $set_query = str_replace("--", "-", $set_query);
-            $set_query = trim($set_query, "-");
-        }else{
-            $set_query = $query.'-'.$values[$j]["products_parameters_values_id"];
-            $set_query = str_replace("--", "-", $set_query);
-            $set_query = trim($set_query, "-");
-        }
-         if(!in_array( $values[$j]["products_parameters_id"], $blocks1) ){
-            $set_query1 = $query1.'-'.$values[$j]["products_parameters_id"];
-            $set_query1 = str_replace("--", "-", $set_query1);
-            $set_query1 = trim($set_query1, "-");
-        }else{
-             $set_query1 = $query.'-'.$values[$j]["products_parameters_id"];
-            $set_query1 = str_replace("--", "-", $set_query1);
-            $set_query1 = trim($set_query1, "-");
-        }
-       // var_dump($set_query1);
-        
-        $znak = (array_key_exists( $filterParams[$key][$i]["products_parameters_id"], $selectedGroups)) ? "+" : "" ;
-        $values[$j]['znak'] = $znak;
-        $values[$j]['set_query'] = $set_query.$price_query ;
-        if (!(array_key_exists( $filterParams[$key][$i]["products_parameters_id"], $selectedGroups)))
-        $values[$j]['url'] = vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories['categories_name']) .'&q='.$set_query.$price_query.'&p='.$set_query1.$price_query);
-        else
-        $values[$j]['url'] = "";
-        $values[$j]['opened'] = ($count_opened == $j && $count_opened != 0) ? true : false ;
-    }
-    $mainParamsFilters[$key][$i] = $filterParams[$key][$i];
-    $mainParamsFilters[$key][$i]['blockValues'] = $values;
-    $mainParamsFilters[$key][$i]['opened'] = ($count_opened != 0 && $count_opened < count($values)) ? true : false ;
+	$count_opened = $filterParams[$i]["products_parameters_maxopened"];
+	$values = get_parameters_block( $filterParams[$i]["products_parameters_id"] , $selectedGroups);
+	if( count($values) == 0 )
+	{
+		continue;
+	}
+	for($j = 0; $j < count($values); $j++)
+	{
+		if(!in_array( $values[$j]["products_parameters_values_id"], $blocks) ){
+			$set_query = $query.'-'.$values[$j]["products_parameters_values_id"];
+			$set_query = str_replace("--", "-", $set_query);
+			$set_query = trim($set_query, "-");
+		}else{
+			$set_query = $query;
+		}
+		$znak = ( array_key_exists( $filterParams[$i]["products_parameters_id"], $selectedGroups)) ? "+" : "" ;
+		$values[$j]['znak'] = $znak;
+		$values[$j]['set_query'] = $set_query.$price_query ;
+		$values[$j]['url'] = vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories['categories_name']) .'&q='.$set_query.$price_query);
+		$values[$j]['opened'] = ($count_opened == $j && $count_opened != 0) ? true : false ;
+	}
+	$mainParamsFilters[$i] = $filterParams[$i];
+	$mainParamsFilters[$i]['blockValues'] = $values;
+	$mainParamsFilters[$i]['opened'] = ($count_opened != 0 && $count_opened < count($values)) ? true : false ;
 }
-}
-
-//print "</pre>";
-//print_r($mainParamsFilters);
+//print_r($filterParams);
 //*/
-
+//print "</pre>";
 
 // reset var
 $box = new vamTemplate;
@@ -144,15 +103,15 @@ $box->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
 $is_params_selected = ( count($selectedParamsFilters) > 0) ? true : false;
 $box->assign('categories_id', $categories_id);
 $box->assign('all_query', $query);
-$box->assign('all_query1', $query1);
 $box->assign('price_min', $_GET['price_min']);
 $box->assign('price_max', $_GET['price_max']);
 $box->assign('is_params_selected', $is_params_selected);
 $box->assign('filterParams', $mainParamsFilters);
 $box->assign('selectedParamsFilters', $selectedParamsFilters);
-$box->assign('priceForm', vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories_name) .'&q='.$query. '&p='.$query1));
-$box->assign('cansel_sel', vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories_name)));
+$box->assign('priceForm', vam_href_link(FILENAME_DEFAULT, vam_category_link($categories_id, $categories_name) .'&q='.$query));
+
 $box->assign('BUTTON_FILTER', vam_image_submit('button_filter.gif', TEXT_PRODUCT_FILTER));
+
 $box->caching = 0;
 $box->assign('language', $_SESSION['language']);
 $box_admin= $box->fetch(CURRENT_TEMPLATE.'/boxes/box_filters.html');
