0fedb1b3d175fcfa5d67b294055bfe314f289a2c
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-06-11 14:34:24 +0000

    Добавлен новый модуль доставки - PickPoint.Ru

---
+++ trunk/lang/russian/modules/shipping/pickpoint.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: pickpoint.php 899 2009/02/07 13:24:46 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(pickpoint.php,v 1.6 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (pickpoint.php,v 1.4 2003/08/13); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (pickpoint.php,v 1.4 2003/08/13); xt-commerce.com
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('MODULE_SHIPPING_PICKPOINT_TEXT_TITLE', 'Постаматы PickPoint.Ru');
+define('MODULE_SHIPPING_PICKPOINT_TEXT_DESCRIPTION', 'Постаматы PickPoint.Ru');
+define('MODULE_SHIPPING_PICKPOINT_TEXT_WAY', 'Вы самостоятельно забираете свой заказа в постомате PickPoint.Ru.');
+
+define('MODULE_SHIPPING_PICKPOINT_STATUS_TITLE' , 'Разрешить модуль pickpoint');
+define('MODULE_SHIPPING_PICKPOINT_STATUS_DESC' , 'Вы хотите разрешить модуль pickpoint?');
+define('MODULE_SHIPPING_PICKPOINT_ALLOWED_TITLE' , 'Разрешённые страны');
+define('MODULE_SHIPPING_PICKPOINT_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+define('MODULE_SHIPPING_PICKPOINT_COST_TITLE' , 'Стоимость доставки');
+define('MODULE_SHIPPING_PICKPOINT_COST_DESC' , 'Стоимость доставки данным способом.');
+define('MODULE_SHIPPING_PICKPOINT_TAX_CLASS_TITLE' , 'Налог');
+define('MODULE_SHIPPING_PICKPOINT_TAX_CLASS_DESC' , 'Использовать налог.');
+define('MODULE_SHIPPING_PICKPOINT_ZONE_TITLE' , 'Зона');
+define('MODULE_SHIPPING_PICKPOINT_ZONE_DESC' , 'Если выбрана зона, то данный модуль доставки будет виден только покупателям из выбранной зоны.');
+define('MODULE_SHIPPING_PICKPOINT_SORT_ORDER_TITLE' , 'Порядок сортировки');
+define('MODULE_SHIPPING_PICKPOINT_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
+?>
