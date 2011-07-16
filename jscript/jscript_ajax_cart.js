1a6cbe4759ce6271871c1df1956e1203724ca894
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-07-16 18:39:43 +0000

    Корректировка кода

--- jscript/jscript_ajax_cart.js
+++ jscript/jscript_ajax_cart.js
@@ -17,12 +17,51 @@ function doBuyNow( id, quantity ) {
 
 jQuery.noConflict();
 
+// prepare the form when the DOM is ready
+jQuery(document).ready(function() {
+
+  // Setup the ajax indicator
+  jQuery('body').append('<div id="ajaxLoading"><img src="images/loading.gif"></div>');
+
+
+  jQuery('#ajaxLoading').css({
+    display:"none",
+    margin:"0px auto",
+    paddingLeft:"0px",
+    paddingRight:"0px",
+    paddingTop:"0px",
+    paddingBottom:"0px",
+    position:"absolute",
+    right:"50%",
+    top:"50%",
+    width:"30px"
+  });
+
+});
+
+// Ajax activity indicator bound to ajax start/success/stop document events
+jQuery(document).ajaxSend(function(){
+  jQuery('#ajaxLoading').show();
+});
+
+jQuery(document).ajaxSuccess(function(){
+  jQuery('#ajaxLoading').hide();
+});
+
+jQuery(document).ajaxStop(function(){
+  jQuery('#ajaxLoading').remove();
+});
+
       jQuery.ajax({
                      url: "index_ajax.php",             
                      dataType : "html",                       
                      data: "q=includes/modules/ajax/ajaxCart.php&products_qty=1&action=add_product&products_id="+id,
                      type: "GET",   
-    	               success: function(msg){ jQuery("#divShoppingCart").html(msg);}       
+    	               success: function(msg){ 
+    	               
+    	               jQuery("#divShoppingCart").html(msg);
+    	               
+    	               }       
 
                    });                     
                        
