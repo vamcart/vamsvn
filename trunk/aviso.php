ab806fd8069b426bbce4ab12b1df448273331a2d
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-08-05 13:34:50 +0000

    Добавлен обрабочик платежей для AvisoSMS

--- trunk/aviso.php
+++ trunk/aviso.php
@@ -14,16 +14,25 @@
   Released under the GNU General Public License
 ------------------------------------------------------------------------------*/
 
-    // logging
-$fp = fopen('1.log', 'a+');
-$str=date('Y-m-d H:i:s').' - ';
-foreach ($_REQUEST as $vn=>$vv) {
-  $str.=$vn.'='.$vv.';';
+require('includes/application_top.php');
+require (DIR_WS_CLASSES.'order.php');
+
+// checking and handling
+$aviso_data = json_decode(implode("", file('php://input')),true);
+
+if ($aviso_data['access_key'] == MODULE_PAYMENT_AVISO_ACCESS_KEY) {
+if ($aviso_data['order_status'] == 'success') {
+  $sql_data_array = array('orders_status' => MODULE_PAYMENT_AVISO_ORDER_STATUS_ID);
+  vam_db_perform('orders', $sql_data_array, 'update', "orders_id='".$aviso_data['merchant_order_id']."'");
+
+  $sql_data_arrax = array('orders_id' => $aviso_data['merchant_order_id'],
+                          'orders_status_id' => MODULE_PAYMENT_AVISO_ORDER_STATUS_ID,
+                          'date_added' => 'now()',
+                          'customer_notified' => '0',
+                          'comments' => 'AvisoSMS accepted this order payment');
+  vam_db_perform('orders_status_history', $sql_data_arrax);
+
 }
-
-  $str.='test'.var_dump($response);
-fwrite($fp, $str."\n");
-fclose($fp);
-
+}
 
 ?>
