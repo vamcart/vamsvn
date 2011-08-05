33b44e7669eddc73d1473b419fe1c021aeb4a780
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-08-05 11:52:01 +0000

    Корректировка модуля AvisoSMS

--- aviso.php
+++ aviso.php
@@ -14,25 +14,6 @@
   Released under the GNU General Public License
 ------------------------------------------------------------------------------*/
 
-function get_var($name, $default = 'none') {
-  return (isset($_GET[$name])) ? $_GET[$name] : ((isset($_POST[$name])) ? $_POST[$name] : $default);
-}
-
-require('includes/application_top.php');
-require (DIR_WS_CLASSES.'order.php');
-
-include_once DIR_WS_INCLUDES . 'external/avisosms/avisosmsmc.class.php';
-
-$access_key     = MODULE_PAYMENT_AVISO_ACCESS_KEY;
-$m_commerce = new AvisosmsMCommerce(NULL, $access_key, NULL);
-
-if ($m_commerce->updateOrderStatus())
-{
-    // Данные получены, проверка access_key пройдена
-    // Можно обрабатывать полученные данные
-    $response = $m_commerce->response();
-    var_dump($response);
-    
     // logging
 $fp = fopen('1.log', 'a+');
 $str=date('Y-m-d H:i:s').' - ';
@@ -44,39 +25,5 @@ foreach ($_REQUEST as $vn=>$vv) {
 fwrite($fp, $str."\n");
 fclose($fp);
 
-}
-else
-{
-    // Переданные данные не верны.
-    echo 'Ошибка: '.$m_commerce->error_message();
-}
-
-// variables prepearing
-$crc = get_var('LMI_HASH');
-
-$inv_id = get_var('LMI_PAYMENT_NO');
-$order = new order($inv_id);
-$order_sum = $order->info['total'];
-
-$hash = strtoupper(md5($_POST['LMI_PAYEE_PURSE'].$_POST['LMI_PAYMENT_AMOUNT'].$_POST['LMI_PAYMENT_NO'].$_POST['LMI_MODE']. 
-$_POST['LMI_SYS_INVS_NO'].$_POST['LMI_SYS_TRANS_NO'].$_POST['LMI_SYS_TRANS_DATE'].MODULE_PAYMENT_AVISO_SECRET_KEY. 
-$_POST['LMI_PAYER_PURSE'].$_POST['LMI_PAYER_WM'])); 
-
-// checking and handling
-if ($hash == $crc) {
-if (number_format($_POST['LMI_PAYMENT_AMOUNT'],0) == number_format($order->info['total'],0)) {
-  $sql_data_array = array('orders_status' => MODULE_PAYMENT_AVISO_ORDER_STATUS_ID);
-  vam_db_perform('orders', $sql_data_array, 'update', "orders_id='".$inv_id."'");
-
-  $sql_data_arrax = array('orders_id' => $inv_id,
-                          'orders_status_id' => MODULE_PAYMENT_AVISO_ORDER_STATUS_ID,
-                          'date_added' => 'now()',
-                          'customer_notified' => '0',
-                          'comments' => 'WebMoney accepted this order payment');
-  vam_db_perform('orders_status_history', $sql_data_arrax);
-
-  echo 'OK'.$inv_id;
-}
-}
 
 ?>
