6ccdf64d4bf6c0b65e92c97931f589b8d7615af8
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-08-22 15:22:57 +0000

    Корректировка кода в модуле сравнения товара

--- trunk/includes/modules/products_filter.php
+++ trunk/includes/modules/products_filter.php
@@ -106,8 +106,10 @@
           $filter_id = $filters_array['filter'];
 
           if ($specs_array['products_column_name'] == 'products_price' || $specs_array['products_column_name'] == 'final_price') {
-            $previous_filter = $currencies->format ($previous_filter);
-            $filter_text = $currencies->format ($filters_array['filter']);
+            //$previous_filter = $currencies->format ($previous_filter);
+            //$filter_text = $currencies->format ($filters_array['filter']);
+            $previous_filter = $previous_filter;
+            $filter_text = $filters_array['filter'];
           } else {
             $filter_text = $specs_array['specification_prefix'] . ' ' . $filters_array['filter'] . ' ' . $specs_array['specification_suffix'];
           }
@@ -135,7 +137,8 @@
         // For range class only, create a filter for maximum value +
         if ($specs_array['filter_class'] == 'range') {
           if ($specs_array['products_column_name'] == 'products_price' || $specs_array['products_column_name'] == 'final_price') {
-            $previous_filter = $currencies->format ($previous_filter);
+            //$previous_filter = $currencies->format ($previous_filter);
+            $previous_filter = $previous_filter;
           }
 
           $count = 1;
