6e9e49b5e2bbcf0e1f16676eb031817cd61a41d5
vamshop <vamshop@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-08-22 14:11:46 +0000

    Удален код старого модуля фильтров товара

--- includes/modules/product_listing.php
+++ includes/modules/product_listing.php
@@ -23,101 +23,14 @@ $result = true;
 // include needed functions
 require_once (DIR_FS_INC.'vam_get_all_get_params.inc.php');
 require_once (DIR_FS_INC.'vam_get_vpe_name.inc.php');
-require_once (DIR_WS_FUNCTIONS.'params_filters.php');
 if (isset($_GET['on_page']) && is_numeric($_GET['on_page'])) {
  $num_page =  $_GET['on_page'];
  } else {
  $num_page =  MAX_DISPLAY_SEARCH_RESULTS;
  }
 
-/* all products list */
-$current_manufacturers_id = 0;
-$where_manufacturers = "";
-if(isset($_GET["filter_id"]))
-{
-    $current_manufacturers_id = intval($_GET["filter_id"]);
-}
-if($current_manufacturers_id != 0){
-    $where_manufacturers = " and p.manufacturers_id = '" . $current_manufacturers_id . "' ";
-}
-
-if($current_manufacturers_id != 0){
-
-    $product_list_rs = vamDBquery("select p.products_model,
-                                    p.products_ean,
-                                    pd.products_name,
-                                    p.products_id
-
-                                from ".TABLE_PRODUCTS_DESCRIPTION." pd,
-                                    ".TABLE_PRODUCTS_TO_CATEGORIES." p2c,
-                                    ".TABLE_PRODUCTS." p
-
-                                where p.products_status = '1'
-                                    " . $where_manufacturers . "
-                                    and p.products_id = p2c.products_id
-                                    and pd.products_id = p2c.products_id
-                                    and pd.language_id = '1'
-                                    and p2c.categories_id = '" . $current_category_id . "'
-                                ORDER BY pd.products_name ");
-
-    $product_list = array();
-    while($product_row = vam_db_fetch_array($product_list_rs,true))
-    {
-        $product_list[] = $product_row;
-    }
-    $product_list_info = "TEST";
-    $module->assign('product_list', $product_list);
-
-} elseif (!empty($search_by_params_ids)) {
-    $product_list_rs = vamDBquery("select p.products_model,
-                                    p.products_ean,
-                                    pd.products_name,
-                                    p.products_id
-
-                                from ".TABLE_PRODUCTS_DESCRIPTION." pd,
-                                    ".TABLE_PRODUCTS_TO_CATEGORIES." p2c,
-                                    ".TABLE_PRODUCTS." p
-
-                                where p.products_status = '1' and
-                                    " . $search_by_params_ids . "
-                                    p.products_id = p2c.products_id
-                                    and pd.products_id = p2c.products_id
-                                    and pd.language_id = '1'
-                                    and p2c.categories_id = '" . $current_category_id . "'
-                                ORDER BY pd.products_name ");
-    $product_list = array();
-    while($product_row = vam_db_fetch_array($product_list_rs,true))
-    {
-        $product_list[] = $product_row;
-    }
-    $module->assign('product_list', $product_list);
-
-} else {
-
-}
-/* */
-
 $module->assign('LINK_PAGE',vam_href_link(basename($PHP_SELF),vam_get_all_get_params(array ('page','on_page','sort', 'direction', 'info','x','y')) . 'on_page='));
 
-$listing_sql = str_replace("where p.products_status = '1'", "where $search_by_params_ids p.products_status = '1'", $listing_sql);
-
-if (isset($_GET['status']))
-{
-
-    if ('all' === $_GET['status'])
-    {
-        $listing_sql = str_replace("p.products_status = '1'", "1", $listing_sql);
-    }
-    else
-    {
-        $_GET['status'] = intval($_GET['status']);
-        $listing_sql = str_replace("p.products_status = '1'", "p.products_status = '".$_GET['status']."'", $listing_sql);
-    }
-}
-$listing_sql = get_params_listing_sql($listing_sql, intval($current_category_id), $selectedGroups);
-
-//print "[".$listing_sql."]";
-//exit;
 $listing_split = new splitPageResults($listing_sql, (int)$_GET['page'], $num_page, 'p.products_id');
 $module_content = array ();
 if ($listing_split->number_of_rows > 0) {
@@ -150,46 +63,19 @@ if ($listing_split->number_of_rows > 0) {
 	$module->assign('CATEGORIES_IMAGE', $image);
 	$module->assign('CATEGORIES_DESCRIPTION', $category['categories_description']);
 
-	$rows = 0;
+	$query = "SELECT manufacturers_description FROM ".TABLE_MANUFACTURERS_INFO." where manufacturers_id = '" . (int)$_GET['manufacturers_id'] . "' and languages_id = '".$_SESSION['languages_id']."'";
 
-	$listing_query = vamDBquery($listing_split->sql_query);
-    $ids = array();
-    while ($listing = vam_db_fetch_array($listing_query, true)) {
-        $rows ++;
-
-        $module_content[] =  $product->buildDataArray($listing);
+		$open_query = vamDBquery($query);
+		$open_data = vam_db_fetch_array($open_query, true);
+		$manufacturers_description = $open_data["manufacturers_description"]; 
+		$module->assign('MANUFACTURERS_DESCRIPTION', $manufacturers_description);
 		
-        $ids[] = $module_content[sizeof($module_content) - 1]['PRODUCTS_ID'];
-   }
-
-// Parameters start
-
-    if (is_array($ids) && sizeof($ids) > 0)
-    {
-        //$cats = vam_db_query("SELECT products_id, categories_id FROM ".TABLE_PRODUCTS_TO_CATEGORIES." WHERE products_id IN (".implode(", ", $ids).")");
-        //$temp = array();
-	    //while ($c = vam_db_fetch_array($cats, true))
-	    //{
-	        //if ($temp[$c['products_id']] < 1) $temp[$c['products_id']] =  $c['categories_id'];
-	    //}
-
-        $p_list = array();
-        $params_r = vamDBquery("SELECT products_id, categories_id, products_parameters_title, products_parameters2products_value, products_parameters_titlesuff FROM ".TABLE_PRODUCTS_PARAMETERS2PRODUCTS." LEFT JOIN ".TABLE_PRODUCTS_PARAMETERS." USING(products_parameters_id) WHERE products_id IN (".implode(", ", $ids).") AND products_parameters_useinsdesc = 1 ORDER BY products_parameters_order ASC");
-        while($p = vam_db_fetch_array($params_r,true))
-        {
-            //if ($temp[$p['products_id']] == $p['categories_id'])
-            	$p_list[$p['products_id']][] = array('name' => $p['products_parameters_title'], 'value' => $p['products_parameters2products_value'], 'suff' => $p['products_parameters_titlesuff']);
-        }
-        foreach($module_content as $k => $m)
-        {
-            $module_content[$k]['params'] = $p_list[$m['PRODUCTS_ID']];
-        }
-    }
-
-	$module->assign('BUTTON_COMPARE', vam_image_submit('button_compare.gif', TEXT_PRODUCT_COMPARE));
-
-// Parameters end
-
+	$rows = 0;
+	$listing_query = vamDBquery($listing_split->sql_query);
+	while ($listing = vam_db_fetch_array($listing_query, true)) {
+		$rows ++;
+		$module_content[] =  $product->buildDataArray($listing);		
+	}
 } else {
 
 	// no product found
@@ -211,6 +97,7 @@ if ($category['listing_template'] == '' or $category['listing_template'] == 'def
 }
 
 if ($result != false) {
+
 
 	$module->assign('MANUFACTURER_DROPDOWN', $manufacturer_dropdown);
 	$module->assign('MANUFACTURER_SORT', $manufacturer_sort);
