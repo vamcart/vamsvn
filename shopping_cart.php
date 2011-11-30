4cdc8829fab1baa85828baf3661769b6e9d511f4
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-11-30 15:52:47 +0000

    Корректировка кода в корзине

--- shopping_cart.php
+++ shopping_cart.php
@@ -41,6 +41,7 @@ include (DIR_WS_MODULES.'gift_cart.php');
 
 if ($_SESSION['cart']->count_contents() > 0) {
 
+  if($_SESSION['error_cart_msg'] != '')
   $vamTemplate->assign('info_message', $_SESSION['error_cart_msg']);
 
 	$vamTemplate->assign('FORM_ACTION', vam_draw_form('cart_quantity', vam_href_link(FILENAME_SHOPPING_CART, 'action=update_product')));
