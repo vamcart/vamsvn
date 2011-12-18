20f7cb7c49ec6df96ff0e0bacf3d5d51de88ffca
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-12-18 11:33:02 +0000

    Возвращён выбор кодировки при экспорт в Excel импорт/экспорт

--- admin/easypopulate.php
+++ admin/easypopulate.php
@@ -1019,6 +1019,10 @@ if ( !empty($_GET['download']) && ($_GET['download'] == 'stream' or $_GET['downl
             }
         }
 
+			if ($_GET['export_charset'] == 'cp1251'){
+    			$therow = Utf8ToWin($therow);
+   		}
+
         // lop off the trailing separator, then append the end of row indicator
         $therow = substr($therow,0,strlen($therow)-1) . $endofrow;
 
@@ -1490,6 +1494,13 @@ if (!empty($_POST['localfile']) or (isset($_FILES['usrfl']) && isset($_GET['spli
                       <option value="update"><?php echo TEXT_EASYPOPULATE_UPDATE; ?></option>
                       <option value="delete"><?php echo TEXT_EASYPOPULATE_DELETE; ?></option>
                   </select>
+                  
+			<select name="import_charset">
+			<option selected value ="cp1251" size="5">cp1251</option>
+			<option value="utf8" size="5">utf8</option>
+			</select>
+
+                  
                   <span class="button"><button type="submit" name="buttoninsert" value="<?php echo EASY_INSERT;?>"><?php echo vam_image(DIR_WS_IMAGES . 'icons/buttons/submit.png', '', '12', '12'); ?>&nbsp;<?php echo EASY_INSERT; ?></button></span>
                 <br />
                 </p>
@@ -1564,6 +1575,12 @@ if (!empty($_POST['localfile']) or (isset($_FILES['usrfl']) && isset($_GET['spli
           
           echo vam_draw_pull_down_menu('download',array( 0 => array( "id" => 'activestream', 'text' => TEXT_EASYPOPULATE_ON_THE_FLY), 1 => array( "id" => 'stream', 'text' => TEXT_EASYPOPULATE_CREATE_THEN_DOWNLOAD), 2 => array( "id" => 'tempfile', 'text' => TEXT_EASYPOPULATE_CREATE_IN_TEMP)));
           echo '&nbsp;' . TEXT_EASYPOPULATE_TYPE . '&nbsp;' . vam_draw_pull_down_menu('dltype',array( 0 => array( "id" => 'full', 'text' => TEXT_EASYPOPULATE_COMPLETE), 1 => array( "id" => 'custom', 'text' => TEXT_EASYPOPULATE_CUSTOM), 2 => array( "id" => 'priceqty', 'text' => TEXT_EASYPOPULATE_PRICE_QTY), 3 => array( "id" => 'category', 'text' => TEXT_EASYPOPULATE_CATEGORIES), 4 => array( "id" => 'attrib', 'text' => TEXT_EASYPOPULATE_ATTRIBUTES), 5 => array( "id" => 'froogle', 'text' => TEXT_EASYPOPULATE_FROOGLE)),'custom','onChange="return switchForm(this);"');
+          
+          echo '<select name="export_charset">
+			<option selected value ="cp1251" size="5">cp1251</option>
+			<option value="utf8" size="5">utf8</option>
+			</select>';
+			
           echo '&nbsp;' . ((EP_EXCEL_SAFE_OUTPUT == true)?".csv":".txt") . TEXT_EASYPOPULATE_FILE_FORMAT; 
 
           $cells = array();
