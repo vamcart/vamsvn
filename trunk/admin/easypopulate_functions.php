89e8960ee9c751fc33b25ae78959ac5b6d27f319
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-12-18 11:33:32 +0000

    Корректировка функции

--- trunk/admin/easypopulate_functions.php
+++ trunk/admin/easypopulate_functions.php
@@ -110,4 +110,88 @@ function prepare_image($image) {
 	return $products_image_name;
 }
 
+// utf8cp1251 and cp1251toutf8 functions
+
+function Utf8ToWin($fcontents) {
+
+    if (function_exists('iconv')) {
+       return iconv('UTF-8', 'CP1251', $fcontents); 
+    } else {
+
+    $out = $c1 = '';
+    $byte2 = false;
+    for ($c = 0;$c < strlen($fcontents);$c++) {
+        $i = ord($fcontents[$c]);
+        if ($i <= 127) {
+            $out .= $fcontents[$c];
+        }
+        if ($byte2) {
+            $new_c2 = ($c1 & 3) * 64 + ($i & 63);
+            $new_c1 = ($c1 >> 2) & 5;
+            $new_i = $new_c1 * 256 + $new_c2;
+            if ($new_i == 1025) {
+                $out_i = 168;
+            } else {
+                if ($new_i == 1105) {
+                    $out_i = 184;
+                } else {
+                    $out_i = $new_i - 848;
+                }
+            }
+            $out .= chr($out_i);
+            $byte2 = false;
+        }
+        if (($i >> 5) == 6) {
+            $c1 = $i;
+            $byte2 = true;
+        }
+    }
+    return $out;
+
+
+    }
+
+}
+
+function CP1251toUTF8($str){
+
+    if (function_exists('iconv')) {
+       return iconv('CP1251', 'UTF-8', $str); 
+    } else {
+
+static $table = array("\xA8" => "\xD0\x81", 
+"\xB8" => "\xD1\x91", 
+// украинские символы
+"\xA1" => "\xD0\x8E", 
+"\xA2" => "\xD1\x9E", 
+"\xAA" => "\xD0\x84", 
+"\xAF" => "\xD0\x87", 
+"\xB2" => "\xD0\x86", 
+"\xB3" => "\xD1\x96", 
+"\xBA" => "\xD1\x94", 
+"\xBF" => "\xD1\x97", 
+// чувашские символы
+"\x8C" => "\xD3\x90", 
+"\x8D" => "\xD3\x96", 
+"\x8E" => "\xD2\xAA", 
+"\x8F" => "\xD3\xB2", 
+"\x9C" => "\xD3\x91", 
+"\x9D" => "\xD3\x97", 
+"\x9E" => "\xD2\xAB", 
+"\x9F" => "\xD3\xB3", 
+);
+return preg_replace('#[\x80-\xFF]#se',
+' "$0" >= "\xF0" ? "\xD1".chr(ord("$0")-0x70) :
+("$0" >= "\xC0" ? "\xD0".chr(ord("$0")-0x30) :
+(isset($table["$0"]) ? $table["$0"] : "")
+)',
+$str
+);
+
+    }
+
+}
+
+// utf8cp1251 and cp1251toutf8 functions
+
 ?>
