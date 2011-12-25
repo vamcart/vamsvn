c4307937ca9181f7b625445ba6b66174bf8bd390
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2011-12-25 14:13:25 +0000

    Корректировка перевода

--- trunk/lang/russian/admin/products_specifications.php
+++ trunk/lang/russian/admin/products_specifications.php
@@ -1,7 +1,7 @@
 <?php
 /*
   $Id: products_specifications.php v1.0 20090909 kymation $
-  Based on: categories.php 1739 2011-12-20 00:52:16Z hpdl $
+  Based on: categories.php 1739 2011-12-21 00:52:16Z hpdl $
   $Loc: catalog/admin/includes/languages/english/ $
 
   osCommerce, Open Source E-Commerce Solutions
@@ -53,7 +53,7 @@
   define ('TEXT_INFO_HEADING_EDIT_GROUP', 'Редактирование группы');
   define ('TEXT_NEW_GROUP_INTRO', 'Пожалуйста, заполните форму для создания новой группы.');
   define ('TEXT_GROUP_NAME', 'Название группы:');
-  define ('TEXT_SHOW_COMPARISON', 'Показывать страницу сравнения:');
+  define ('TEXT_SHOW_COMPARISON', 'Показывать на странице сравнения:');
   define ('TEXT_SHOW_ON_PRODUCTS', 'Показывать на странице карточки товара');
   define ('TEXT_SHOW_FILTER', 'Показывать фильтр:');
   define ('TEXT_SHOW', 'Да');
