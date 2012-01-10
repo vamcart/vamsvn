639d178dfdb9128cdb565064f54003c9e8469a5a
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-01-10 16:08:39 +0000

    Корректировка перевода

--- trunk/lang/russian/affiliate_russian.php
+++ trunk/lang/russian/affiliate_russian.php
@@ -1,6 +1,6 @@
 <?php
 /*------------------------------------------------------------------------------
-   $Id: affiliate_english.php,v 1.1 2003/12/21 20:13:07 hubi74 Exp $
+   $Id: affiliate_english.php,v 1.1 2012/12/21 20:13:07 hubi74 Exp $
 
    XTC-Affiliate - Contribution for XT-Commerce http://www.xt-commerce.com
    modified by http://www.netz-designer.de
