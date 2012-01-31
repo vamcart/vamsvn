71847506dd50e1cb837b56f27d438f7994fc71e0
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-01-31 08:33:39 +0000

    Задать вопрос о товаре и Версия для печати открываются через FancyBox

--- trunk/includes/header.php
+++ trunk/includes/header.php
@@ -99,10 +99,27 @@ if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO)) {
 <script type="text/javascript">
 	$(document).ready(function() {
 		$("a.zoom").fancybox({
-		"zoomOpacity"			: true,
-		"zoomSpeedIn"			: 500,
-		"zoomSpeedOut"			: 500
-	});
+			"zoomOpacity"			: true,
+			"zoomSpeedIn"			: 500,
+			"zoomSpeedOut"			: 500
+		});
+
+		$("a.print, a.ask").fancybox({
+			"padding" : 20, // отступ контента от краев окна
+			"imageScale" : false, // Принимает значение true - контент(изображения) масштабируется по размеру окна, или false - окно вытягивается по размеру контента. По умолчанию - TRUE
+			"zoomOpacity" : true, // изменение прозрачности контента во время анимации (по умолчанию false)
+			"zoomSpeedIn" : 500, // скорость анимации в мс при увеличении фото (по умолчанию 0)
+			"zoomSpeedOut" : 500, // скорость анимации в мс при уменьшении фото (по умолчанию 0)
+			"zoomSpeedChange" : 500, // скорость анимации в мс при смене фото (по умолчанию 0)
+			"frameWidth" : 1000,  // ширина окна, px (425px - по умолчанию)
+			"frameHeight" : 700, // высота окна, px(355px - по умолчанию)
+			"frameScale" : false, // Принимает значение true - контент(изображения) масштабируется по размеру окна, или false - окно вытягивается по размеру контента. По умолчанию - TRUE
+			"overlayShow" : true, // если true затеняят страницу под всплывающим окном. (по умолчанию true). Цвет задается в jquery.fancybox.css - div#fancy_overlay
+			"overlayOpacity" : 0.8,  // Прозрачность затенения  (0.3 по умолчанию)
+			"hideOnContentClick" :true, // Если TRUE  закрывает окно по клику по любой его точке (кроме элементов навигации). Поумолчанию TRUE 
+			"centerOnScroll" : false // Если TRUE окно центрируется на экране, когда пользователь прокручивает страницу 
+		});
+	
 	});
 </script>
 <?php
