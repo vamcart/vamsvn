7e62bb1ef7f9a04c9790bb15ca2b47780a528e24
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-05-14 08:31:46 +0000

    Добавлена возможность задавать атрибуты сразу всем товарам внутри категории

--- admin/includes/modules/new_attributes_change.php
+++ admin/includes/modules/new_attributes_change.php
@@ -18,67 +18,94 @@
    --------------------------------------------------------------
    Third Party contributions:
    New Attribute Manager v4b				Autor: Mike G | mp3man@internetwork.net | http://downloads.ephing.com
-
+	Multiple attributes change (all items of category)   Author: DaneSoul | http://danesoul.info
+	
    Released under the GNU General Public License 
    --------------------------------------------------------------*/ 
    defined('_VALID_VAM') or die('Direct Access to this location is not allowed.');
    require_once(DIR_FS_INC .'vam_get_tax_rate.inc.php');
    require_once(DIR_FS_INC .'vam_get_tax_class_id.inc.php');
- //  require_once(DIR_FS_INC .'vam_format_price.inc.php');
-  // I found the easiest way to do this is just delete the current attributes & start over =)
-  // download function start
-  $delete_sql = vam_db_query("SELECT products_attributes_id FROM ".TABLE_PRODUCTS_ATTRIBUTES." WHERE products_id = '" . $_POST['current_product_id'] . "'");
-  while($delete_res = vam_db_fetch_array($delete_sql)) {
-      $delete_download_sql = vam_db_query("SELECT products_attributes_filename FROM ".TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD." WHERE products_attributes_id = '" . $delete_res['prducts_attributes_id'] . "'");
-      $delete_download_file = vam_db_fetch_array($delete_download_sql);
-      vam_db_query("DELETE FROM ".TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD." WHERE products_attributes_id = '" . $delete_res['products_attributes_id'] . "'");
-  }
-  // download function end
-  vam_db_query("DELETE FROM ".TABLE_PRODUCTS_ATTRIBUTES." WHERE products_id = '" . $_POST['current_product_id'] . "'" );
-
-  // Simple, yet effective.. loop through the selected Option Values.. find the proper price & prefix.. insert.. yadda yadda yadda.
-  for ($i = 0; $i < sizeof($_POST['optionValues']); $i++) {
-    $query = "SELECT * FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS." where products_options_values_id = '" . $_POST['optionValues'][$i] . "'";
-    $result = vam_db_query($query);
-    $matches = vam_db_num_rows($result);
-    while ($line = vam_db_fetch_array($result)) {
-      $optionsID = $line['products_options_id'];
-    }
-
-    $cv_id = $_POST['optionValues'][$i];
-    $value_price =  $_POST[$cv_id . '_price'];
-
-    if (PRICE_IS_BRUTTO=='true'){
-
-    $value_price= ($value_price/((vam_get_tax_rate(vam_get_tax_class_id($_POST['current_product_id'])))+100)*100);
-    }
-          $value_price=vam_round($value_price,PRICE_PRECISION);
-
-
-    $value_prefix = $_POST[$cv_id . '_prefix'];
-    $value_sortorder = $_POST[$cv_id . '_sortorder'];
-    $value_weight_prefix = $_POST[$cv_id . '_weight_prefix'];
-    $value_model =  $_POST[$cv_id . '_model'];
-    $value_stock =  $_POST[$cv_id . '_stock'];
-    $value_weight =  $_POST[$cv_id . '_weight'];
-
-
-      vam_db_query("INSERT INTO ".TABLE_PRODUCTS_ATTRIBUTES." (products_id, options_id, options_values_id, options_values_price, price_prefix ,attributes_model, attributes_stock, options_values_weight, weight_prefix,sortorder) VALUES ('" . $_POST['current_product_id'] . "', '" . $optionsID . "', '" . $_POST['optionValues'][$i] . "', '" . $value_price . "', '" . $value_prefix . "', '" . $value_model . "', '" . $value_stock . "', '" . $value_weight . "', '" . $value_weight_prefix . "','".$value_sortorder."')") or die(mysql_error());
-
-    $products_attributes_id = vam_db_insert_id();
-
-        if ($_POST[$cv_id . '_download_file'] != '') {
-        	if (DOWNLOAD_ENABLED == 'true') {
-        $value_download_file = $_POST[$cv_id . '_download_file'];
-        $value_download_expire = $_POST[$cv_id . '_download_expire'];
-        $value_download_count = $_POST[$cv_id . '_download_count'];
-
-      $value_is_pin = $_POST[$cv_id . '_ispin'];
-      $products_attributes_is_pin = isset($value_is_pin)?1:0;
-
-        vam_db_query("INSERT INTO ".TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD." (products_attributes_id, products_attributes_filename, products_attributes_maxdays, products_attributes_maxcount, products_attributes_is_pin) VALUES ('" . $products_attributes_id . "', '" . $value_download_file . "', '" . $value_download_expire . "', '" . $value_download_count . "', '" . $products_attributes_is_pin . "')") or die(mysql_error());
-    }
-   }
-  }
-
+ 	//  require_once(DIR_FS_INC .'vam_format_price.inc.php');
+ 
+function ItemAttributsSetUp($current_product_id){
+	   // I found the easiest way to do this is just delete the current attributes & start over =)
+	  // download function start
+	  $delete_sql = vam_db_query("SELECT products_attributes_id FROM ".TABLE_PRODUCTS_ATTRIBUTES." WHERE products_id = '" . $current_product_id . "'");
+	  while($delete_res = vam_db_fetch_array($delete_sql)) {
+	      $delete_download_sql = vam_db_query("SELECT products_attributes_filename FROM ".TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD." WHERE products_attributes_id = '" . $delete_res['prducts_attributes_id'] . "'");
+	      $delete_download_file = vam_db_fetch_array($delete_download_sql);
+	      vam_db_query("DELETE FROM ".TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD." WHERE products_attributes_id = '" . $delete_res['products_attributes_id'] . "'");
+	  }
+	  // download function end
+	  vam_db_query("DELETE FROM ".TABLE_PRODUCTS_ATTRIBUTES." WHERE products_id = '" . $current_product_id . "'" );
+	
+	  // Simple, yet effective.. loop through the selected Option Values.. find the proper price & prefix.. insert.. yadda yadda yadda.
+	  for ($i = 0; $i < sizeof($_POST['optionValues']); $i++) {
+	    $query = "SELECT * FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS." where products_options_values_id = '" . $_POST['optionValues'][$i] . "'";
+	    $result = vam_db_query($query);
+	    $matches = vam_db_num_rows($result);
+	    while ($line = vam_db_fetch_array($result)) {
+	      $optionsID = $line['products_options_id'];
+	    }
+	
+	    $cv_id = $_POST['optionValues'][$i];
+	    $value_price =  $_POST[$cv_id . '_price'];
+	
+	    if (PRICE_IS_BRUTTO=='true'){
+	
+	    $value_price= ($value_price/((vam_get_tax_rate(vam_get_tax_class_id($current_product_id)))+100)*100);
+	    }
+	          $value_price=vam_round($value_price,PRICE_PRECISION);
+	
+	
+	    $value_prefix = $_POST[$cv_id . '_prefix'];
+	    $value_sortorder = $_POST[$cv_id . '_sortorder'];
+	    $value_weight_prefix = $_POST[$cv_id . '_weight_prefix'];
+	    $value_model =  $_POST[$cv_id . '_model'];
+	    $value_stock =  $_POST[$cv_id . '_stock'];
+	    $value_weight =  $_POST[$cv_id . '_weight'];
+	
+	
+	      vam_db_query("INSERT INTO ".TABLE_PRODUCTS_ATTRIBUTES." (products_id, options_id, options_values_id, options_values_price, price_prefix ,attributes_model, attributes_stock, options_values_weight, weight_prefix,sortorder) VALUES ('" . $current_product_id . "', '" . $optionsID . "', '" . $_POST['optionValues'][$i] . "', '" . $value_price . "', '" . $value_prefix . "', '" . $value_model . "', '" . $value_stock . "', '" . $value_weight . "', '" . $value_weight_prefix . "','".$value_sortorder."')") or die(mysql_error());
+	
+	    $products_attributes_id = vam_db_insert_id();
+	
+	        if ($_POST[$cv_id . '_download_file'] != '') {
+	        	if (DOWNLOAD_ENABLED == 'true') {
+	        $value_download_file = $_POST[$cv_id . '_download_file'];
+	        $value_download_expire = $_POST[$cv_id . '_download_expire'];
+	        $value_download_count = $_POST[$cv_id . '_download_count'];
+	
+	      $value_is_pin = $_POST[$cv_id . '_ispin'];
+	      $products_attributes_is_pin = isset($value_is_pin)?1:0;
+	
+	        vam_db_query("INSERT INTO ".TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD." (products_attributes_id, products_attributes_filename, products_attributes_maxdays, products_attributes_maxcount, products_attributes_is_pin) VALUES ('" . $products_attributes_id . "', '" . $value_download_file . "', '" . $value_download_expire . "', '" . $value_download_count . "', '" . $products_attributes_is_pin . "')") or die(mysql_error());
+	    }
+	   }
+	  }	
+}
+ 
+if(isset($_POST['category'])){
+	$current_category = $_POST['current_product_id'];
+	// Recieve all items in this category
+	$items_query = "SELECT products_id FROM ".TABLE_PRODUCTS_TO_CATEGORIES." WHERE categories_id = " . $current_category;
+	$items_result = vam_db_query($items_query);
+   $items_matches = vam_db_num_rows($items_result);
+   
+  if ($items_matches) {
+  		while ($items_line = vam_db_fetch_array($items_result)) {
+  			//echo('<pre>'); print_r($items_line);  echo('</pre>');
+  			$current_product_id = $items_line['products_id'];
+  			//echo('<pre>'); print_r($current_product_id);  echo('</pre>');  			
+  			ItemAttributsSetUp($current_product_id);
+  			}
+  } else {
+ 		echo "You have no items in selected category / Нет товаров в выбранной категории";
+  } 	
+} 
+else{
+	$current_product_id = $_POST['current_product_id'];
+	//echo('<pre>'); print_r($current_product_id);  echo('</pre>');
+	ItemAttributsSetUp($current_product_id);
+	} 
 ?>
--- admin/includes/modules/new_attributes_select.php
+++ admin/includes/modules/new_attributes_select.php
@@ -19,6 +19,7 @@
    Third Party contributions:
    New Attribute Manager v4b				Autor: Mike G | mp3man@internetwork.net | http://downloads.ephing.com
    copy attributes                          Autor: Hubi | http://www.netz-designer.de
+	Multiple attributes change (all items of category)   Author: DaneSoul | http://danesoul.info
 
    Released under the GNU General Public License 
    --------------------------------------------------------------*/
@@ -26,52 +27,89 @@ defined('_VALID_VAM') or die('Direct Access to this location is not allowed.');
 $adminImages = DIR_WS_CATALOG . "lang/". $_SESSION['language'] ."/admin/images/buttons/";
 ?>
     <h1 class="contentBoxHeading"><?php echo $pageTitle; ?></h1>
-   
-    <table border="0" width="100%" cellspacing="0" cellpadding="2">
+
+<div align='left'>
+
+<!-- START FORM -->
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" name="SELECT_PRODUCT" method="post"><input type="hidden" name="action" value="edit">
+
 <?php
+	// HIDDEN FIELD
 echo vam_draw_hidden_field(vam_session_name(), vam_session_id());
-  echo "<TR>";
-  echo "<TD class=\"main\"><br /><B>".SELECT_PRODUCT."<br /></TD>";
-  echo "</TR>";
-  echo "<TR>";
-  echo "<TD class=\"main\"><SELECT NAME=\"current_product_id\">";
+	// <input type="hidden" name="sid" value="d11f22b93719bcd78f0c9030c184a70f">
 
-  $query = "SELECT * FROM  ".TABLE_PRODUCTS_DESCRIPTION."  where products_id LIKE '%' AND language_id = '" . $_SESSION['languages_id'] . "' ORDER BY products_name ASC";
+if(isset($_GET['category'])){
+		// http://_vamshop/admin/new_attributes.php?group=true
+	echo "<input type='hidden' name='category' value='true'>";		
+		
+	echo '<h2>Выберите категорию, для которой задаете атрибуты</h2>';		
+	  
+	  // FIRST SELECT LIST START (CATEGORIES)	
+   $catsort    = 'c.sort_order, cd.categories_name ASC';	  
+	
+	// SELECT ALL CATEGORIES (not only for current parent)  
+	$categories_query = "select c.categories_id, cd.categories_name, c.categories_image, c.parent_id, c.sort_order, c.date_added, c.last_modified, c.yml_enable, c.categories_status from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.categories_id = cd.categories_id and cd.language_id = '" . (int)$_SESSION['languages_id'] . "' order by " . $catsort;
+	// echo 	$categories_query; //--DEBUG
+	  
+   $categories_result = vam_db_query($categories_query);
+ 
+	echo "<SELECT NAME=\"current_product_id\">";
+	// USE same name as for items, to avoid changing new_attributes_include.php
+	// So category name is passed to new_attributes_change as itm name
+	// Special <input type="hidden" name="category" value="true"> passed for use item as category
+ 	
+   $categories_matches = vam_db_num_rows($categories_result);
+   
+ 	if ($categories_matches) {
+	    while ($categories_line = vam_db_fetch_array($categories_result)) {
+	      	//$title = $line['products_name'];
+	      	//$current_product_id = $line['products_id'];
+	
+	      	echo '<OPTION VALUE="' . $categories_line['categories_id'] . '">' . 
+	      									$categories_line['categories_name'] .'</OPTION>';     	
+	      	
+	      	//echo('<pre>'); print_r($categories_line);  echo('</pre>');
+	    }
+	  } else {
+	    		echo "You have no categories at this time / У Вас нет категорий в данный момент";
+	  } 
+	  
+	 echo "</SELECT>";    
+}
+else{
+	  echo '<h2>Выберите товар, для которого задаете атрибуты</h2>';		
+	
+	  // FIRST SELECT LIST START (ITEMS)
+	  echo "<SELECT NAME=\"current_product_id\">";
+	
+	  $item_query = "SELECT * FROM  ".TABLE_PRODUCTS_DESCRIPTION."  where products_id LIKE '%' AND language_id = '" . $_SESSION['languages_id'] . "' ORDER BY products_name ASC";
+	
+	  $item_result = vam_db_query($item_query);
+	
+	  $item_matches = vam_db_num_rows($item_result);
+	
+	  if ($item_matches) {
+	    while ($item_line = vam_db_fetch_array($item_result)) {
+	
+	      echo "<OPTION VALUE=\"" . $item_line['products_id'] . "\">" . $item_line['products_name'] .'</OPTION>';
+	    }
+	  } else {
+	      echo "You have no products at this time / У Вас нет продуктов в данный момент";
+	  }
+	
+	  echo "</SELECT>";	
+}
 
-  $result = vam_db_query($query);
 
-  $matches = vam_db_num_rows($result);
-
-  if ($matches) {
-    while ($line = vam_db_fetch_array($result)) {
-      $title = $line['products_name'];
-      $current_product_id = $line['products_id'];
-
-      echo "<OPTION VALUE=\"" . $current_product_id . "\">" . $title;
-    }
-  } else {
-    echo "You have no products at this time.";
-  }
-
-  echo "</SELECT>";
-  echo "</TD></TR>";
-
-  echo "<TR>";
-  echo "<TD class=\"main\">";
-  echo '<span class="button"><button type="submit" value="' . BUTTON_EDIT . '">' . vam_image(DIR_WS_IMAGES . 'icons/buttons/edit.png', '', '12', '12') . '&nbsp;' . BUTTON_EDIT . '</button></span>';
-
-  echo "</TD>";
-  echo "</TR>";
-  // start change for Attribute Copy
 ?>
+
 <br /><br />
+
 <?php
-  echo "<TR>";
-  echo "<TD class=\"main\"><br /><B>".SELECT_COPY."<br /></TD>";
-  echo "</TR>";
-  echo "<TR>";
-  echo "<TD class=\"main\"><SELECT NAME=\"copy_product_id\">";
+  echo '<h2>Выберите товар, с которого копируете атрибуты</h2>';	
+
+// SECOND SELECT LIST START
+  echo "<SELECT NAME=\"copy_product_id\">";
 
   $copy_query = vam_db_query("SELECT pd.products_name, pd.products_id FROM  ".TABLE_PRODUCTS_DESCRIPTION."  pd, ".TABLE_PRODUCTS_ATTRIBUTES." pa where pa.products_id = pd.products_id AND pd.products_id LIKE '%' AND pd.language_id = '" . $_SESSION['languages_id'] . "' GROUP BY pd.products_id ORDER BY pd.products_name ASC");
   $copy_count = vam_db_num_rows($copy_query);
@@ -83,13 +121,21 @@ echo vam_draw_hidden_field(vam_session_name(), vam_session_id());
       }
   }
   else {
-      echo 'No products to copy attributes from';
+      echo 'No products to copy attributes from / Нет продуктов с которых копировать атрибты';
   }
-  echo '</select></td></tr>';
-  echo "<TR>";
-  echo "<TD class=\"main\">".'<span class="button"><button type="submit" value="' . BUTTON_EDIT . '">' . vam_image(DIR_WS_IMAGES . 'icons/buttons/edit.png', '', '12', '12') . '&nbsp;' . BUTTON_EDIT . '</button></span>'."</TD>";
-  echo "</TR>";
+  echo '</select>';
+  
+  
+  	// SINGLE SUBMIT BUTTON
+	  echo '<br><span class="button"><button type="submit" value="' . BUTTON_EDIT . '">' . vam_image(DIR_WS_IMAGES . 'icons/buttons/edit.png', '', '12', '12') . '&nbsp;' . BUTTON_EDIT . '</button></span>';
 
+if(!isset($_GET['category'])){
+	
+  echo '<h2>Установка атрибутов категории</h2>';	
+
+	  echo '<a class="button" href="' . vam_href_link(FILENAME_NEW_ATTRIBUTES, 'category=true', 'NONSSL')  .'"><span>'.vam_image(DIR_WS_IMAGES . 'icons/buttons/edit.png', '', '12', '12') . '&nbsp;' .BOX_ATTRIBUTES_MANAGER_CATEGORIES.'</span></a>';
+
+}
 ?>
 </form>
-</table>
+</div>
