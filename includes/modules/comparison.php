f446282f14060186d80e4ad163497c5dc1ddd257
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-06-11 13:20:49 +0000

    Корректирока страницы сравнения товаров

--- includes/modules/comparison.php
+++ includes/modules/comparison.php
@@ -131,6 +131,31 @@
       $module_contents .= '    </table>' . PHP_EOL;
       $module_contents .= '  </div>' . PHP_EOL;
 
+if (is_array($_REQUEST["products"]))
+{
+    foreach($_REQUEST["products"] as $v)
+    {
+        if (is_numeric(intval(trim($v)))) $temp[] = $v;
+    }
+}
+
+    $products_query = vamDBquery("SELECT
+                                     *
+                                     FROM ".TABLE_PRODUCTS." p,
+                                     ".TABLE_PRODUCTS_DESCRIPTION." pd
+                                     WHERE p.products_id IN (".implode(", ", $temp).") and 
+                                     p.products_id = pd.products_id
+                                     and pd.language_id = '".(int) $_SESSION['languages_id']."'
+                                     and p.products_status=1 order by products_name limit 5");
+    $c_id = array();
+    $temp_parameters = array();
+    while ($products_data = vam_db_fetch_array($products_query, true))
+    {
+        $module_content[] =  $product->buildDataArray($products_data);
+        $temp_parameters[$products_data['products_id']] = array();
+        $c_id[] = $products_data['products_id'];
+    }
+    
       ////
       // Table rows
       $products_query_raw = "
@@ -149,6 +174,7 @@
             on (pd.products_id = p2c.products_id)
         where
           p.products_status = 1
+          and p.products_id IN (".implode(", ", $c_id).")
           and p2c.categories_id = '" . (int) $current_category_id . "'
           and s2c.specification_group_id = '" . (int) $group_id . "'
         order by
