02ce5602234c67c025b741984f6fd94ebbea8c7a
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-06-13 09:06:23 +0000

    Не подключался html-редактор в инорфмационных страницах для товара, исправлено

--- trunk/inc/vam_wysiwyg_tiny.inc.php
+++ trunk/inc/vam_wysiwyg_tiny.inc.php
@@ -511,6 +511,76 @@ function toggleHTMLEditor(id) {
                         	   	';
                         break;
 
+                // WYSIWYG editor content manager
+                case 'products_content':
+                        $val ='<script type="text/javascript" src="'.$js_src.'"></script>
+<script type="text/javascript">
+tinyMCE.init({
+	mode : "none",
+	editor_deselector : "notinymce",
+	theme : "advanced",
+	language : "'.$lang.'",
+	paste_create_paragraphs : false,
+	paste_create_linebreaks : false,
+	paste_use_dialog : true,
+	convert_urls : false,
+
+	plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,typograf",
+
+	file_browser_callback : "'.$tiny_mce_file_browser_callback.'",
+	elements : "'.$tiny_mce_file_browser_callback.'",
+
+	spellchecker_languages : "+Russian=ru,English=en",
+	spellchecker_rpc_url : "'.HTTP_SERVER . DIR_WS_CATALOG.'admin/includes/javascript/tiny_mce/plugins/spellchecker/rpc_proxy.php",
+
+	// Theme options
+	theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,typograf,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_toolbar_align : "left",
+	theme_advanced_statusbar_location : "bottom",
+	theme_advanced_resizing : true
+
+});
+
+  function ajaxfilemanager(field_name, url, type, win) {
+    var ajaxfilemanagerurl = "'.HTTP_SERVER . DIR_WS_CATALOG.'admin/includes/javascript/tiny_mce/plugins/ajaxfilemanager/ajaxfilemanager.php?language=' . strtolower($lang) . '&vam=' . trim(DIR_WS_ADMIN, '/') . '&sid=' . session_id() .'";
+    switch (type) {
+      case "image":
+        break;
+      case "media":
+        break;
+      case "flash":
+        break;
+      case "file":
+        break;
+      default:
+        return false;
+    }
+    tinyMCE.activeEditor.windowManager.open({
+      url: ajaxfilemanagerurl,
+      width: 782,
+      height: 440,
+      inline : "yes",
+      close_previous : "no"
+    },{
+      window : win,
+      input : field_name
+    });
+  }
+  
+function toggleHTMLEditor(id) {
+	if (!tinyMCE.get(id))
+		tinyMCE.execCommand("mceAddControl", false, id);
+	else
+		tinyMCE.execCommand("mceRemoveControl", false, id);
+}
+                        	   	</script>
+                        	   	';
+                        break;
+
                 // WYSIWYG editor products content manager
                         $val ='<script type="text/javascript" src="'.$js_src.'"></script>
 <script type="text/javascript">
