2f859ee68d10fbac8a84a60450ed3f2ec4883436
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-09-16 05:41:16 +0000

    Корректировка кода в модуле оплаты PayMaster

--- trunk/lang/russian/modules/payment/paymaster.php
+++ trunk/lang/russian/modules/payment/paymaster.php
@@ -1,12 +1,12 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: webmoney_merchant.php 998 2009/05/07 13:24:46 VaM $
+   $Id: paymaster.php 998 2012/09/16 13:24:46 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
    http://vamshop.com
 
-   Copyright (c) 2007 VaM Shop
+   Copyright (c) 2012 VamShop
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
@@ -17,30 +17,24 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_TITLE', 'WebMoney Merchant');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_PUBLIC_TITLE', 'WebMoney Merchant');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты WebMoney Merchant<br />Как правильно настроить модуль читайте <a href="http://vamshop.ru/faq.php/faq_id/70/question/Nastroika-modulya-oplaty-WebMoney-Merchant" target="_blank"><u>здесь</u></a>.');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
+  define('MODULE_PAYMENT_PAYMASTER_TEXT_TITLE', 'PayMaster');
+  define('MODULE_PAYMENT_PAYMASTER_TEXT_PUBLIC_TITLE', 'PayMaster');
+  define('MODULE_PAYMENT_PAYMASTER_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты PayMaster<br />Как правильно настроить модуль читайте <a href="http://vamshop.ru/faq.php/faq_id/70/question/Nastroika-modulya-oplaty-WebMoney-Merchant" target="_blank"><u>здесь</u></a>.');
+  define('MODULE_PAYMENT_PAYMASTER_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
   
-  define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_TYPE','Способ оплаты:');
-  define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_WMZ','WMZ');
-  define('MODULE_PAYMENT_WEBMONEYMERCHANT_TEXT_WMR','WMR');
-
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_STATUS_TITLE' , 'Разрешить модуль WebMoney');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ALLOWED_TITLE' , 'Разрешённые страны');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ID_TITLE' , 'WM ID:');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ID_DESC' , 'Укажите Ваш WM ID');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMR_TITLE' , 'Ваш WMR кошелёк:');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_WMR_DESC' , 'Укажите номер Вашего WMR кошелька');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SORT_ORDER_TITLE' , 'Порядок сортировки');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ZONE_TITLE' , 'Зона');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SECRET_KEY_TITLE' , 'Секретный ключ');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_SECRET_KEY_DESC' , 'В данной опции укажите Ваш ключ, указанный в опции Secret Key на сайте WebMoney Merchant.');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
-define('MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
+	define('MODULE_PAYMENT_PAYMASTER_STATUS_TITLE' , 'Разрешить модуль PayMaster');
+	define('MODULE_PAYMENT_PAYMASTER_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
+	define('MODULE_PAYMENT_PAYMASTER_ALLOWED_TITLE' , 'Разрешённые страны');
+	define('MODULE_PAYMENT_PAYMASTER_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+	define('MODULE_PAYMENT_PAYMASTER_ID_TITLE' , 'WM ID:');
+	define('MODULE_PAYMENT_PAYMASTER_ID_DESC' , 'Укажите Ваш WM ID');
+	define('MODULE_PAYMENT_PAYMASTER_SORT_ORDER_TITLE' , 'Порядок сортировки');
+	define('MODULE_PAYMENT_PAYMASTER_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
+	define('MODULE_PAYMENT_PAYMASTER_ZONE_TITLE' , 'Зона');
+	define('MODULE_PAYMENT_PAYMASTER_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
+	define('MODULE_PAYMENT_PAYMASTER_SECRET_KEY_TITLE' , 'Секретный ключ');
+	define('MODULE_PAYMENT_PAYMASTER_SECRET_KEY_DESC' , 'В данной опции укажите Ваш ключ, указанный в опции Secret Key на сайте PayMaster.');
+	define('MODULE_PAYMENT_PAYMASTER_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
+	define('MODULE_PAYMENT_PAYMASTER_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
   
 ?>
