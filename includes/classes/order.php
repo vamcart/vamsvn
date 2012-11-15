dfc7d8035c2d9e62929b7ba5ce064cb6f3007027
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2012-11-15 17:33:32 +0000

    Корректировка кода в модуле быстрого оформления заказов

--- includes/classes/order.php
+++ includes/classes/order.php
@@ -52,14 +52,16 @@
       $this->delivery = array();
 
 
-	  if (isset ($_SESSION['noaccount'])) {
-	  	$this->cart_noaccount();
-      } elseif (vam_not_null($order_id)) {
-        $this->query($order_id);
-      } else {
-        $this->cart();
-      }
-    }
+		// SMART CHECKOUT BOF
+			  if (vam_session_is_registered('noaccount')) {
+			  	$this->cart_noaccount();
+		      } elseif (vam_not_null($order_id)) {
+		        $this->query($order_id);
+		      } else {
+		        $this->cart();
+		      }
+		    }
+		// SMART CHECKOUT EOF
     
     function query($order_id) {
 
@@ -458,7 +460,7 @@
   
   
     function cart_noaccount() {
-      global $customer_id, $sendto, $billto, $cart, $languages_id, $currency, $currencies, $shipping, $payment, $comments, $customer_default_address_id;
+      global $customer_id, $sendto, $billto, $cart, $currency, $currencies, $shipping, $payment, $comments, $customer_default_address_id;
 
       $this->content_type = $cart->get_content_type();
 
@@ -617,7 +619,7 @@ if ($_SESSION['sc_payment_address_selected'] != '1') { //is unchecked - so payme
           $subindex = 0;
           reset($products[$i]['attributes']);
           while (list($option, $value) = each($products[$i]['attributes'])) {
-            $attributes_query = vam_db_query("select popt.products_options_name, poval.products_options_values_name, pa.options_values_price, pa.price_prefix from " . TABLE_PRODUCTS_OPTIONS . " popt, " . TABLE_PRODUCTS_OPTIONS_VALUES . " poval, " . TABLE_PRODUCTS_ATTRIBUTES . " pa where pa.products_id = '" . (int)$products[$i]['id'] . "' and pa.options_id = '" . (int)$option . "' and pa.options_id = popt.products_options_id and pa.options_values_id = '" . (int)$value . "' and pa.options_values_id = poval.products_options_values_id and popt.language_id = '" . (int)$languages_id . "' and poval.language_id = '" . (int)$languages_id . "'");
+            $attributes_query = vam_db_query("select popt.products_options_name, poval.products_options_values_name, pa.options_values_price, pa.price_prefix from " . TABLE_PRODUCTS_OPTIONS . " popt, " . TABLE_PRODUCTS_OPTIONS_VALUES . " poval, " . TABLE_PRODUCTS_ATTRIBUTES . " pa where pa.products_id = '" . (int)$products[$i]['id'] . "' and pa.options_id = '" . (int)$option . "' and pa.options_id = popt.products_options_id and pa.options_values_id = '" . (int)$value . "' and pa.options_values_id = poval.products_options_values_id and popt.language_id = '" . (int)$_SESSION['languages_id'] . "' and poval.language_id = '" . (int)$_SESSION['languages_id'] . "'");
             $attributes = vam_db_fetch_array($attributes_query);
 
             $this->products[$index]['attributes'][$subindex] = array('option' => $attributes['products_options_name'],
