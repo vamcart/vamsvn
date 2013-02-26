fd4ea6201f7d91d7a594db0f56a2ec7eb058aced
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2013-02-26 09:28:04 +0000

    Корректировка функций для работы с базой для правильной работы на php 5.5

--- trunk/install/includes/functions/compatibility.php
+++ trunk/install/includes/functions/compatibility.php
@@ -27,4 +27,73 @@
       return ereg('^[0-9]{1,50}.?[0-9]{0,50}$', $param);
     }
   }
+  
+ if ( !function_exists('mysqli_connect') ) {
+    define('MYSQLI_ASSOC', MYSQL_ASSOC);
+
+    function mysqli_connect($server, $username, $password, $database) {
+      if ( substr($server, 0, 2) == 'p:' ) {
+        $link = mysql_pconnect(substr($server, 2), $username, $password);
+      } else {
+        $link = mysql_connect($server, $username, $password);
+      }
+
+      if ( $link ) {
+        mysql_select_db($database, $link);
+      }
+
+      return $link;
+    }
+
+    function mysqli_close($link) {
+      return mysql_close($link);
+    }
+
+    function mysqli_query($link, $query) {
+      return mysql_query($query, $link);
+    }
+
+    function mysqli_errno($link) {
+      return mysql_errno($link);
+    }
+
+    function mysqli_error($link) {
+      return mysql_error($link);
+    }
+
+    function mysqli_fetch_array($query, $type) {
+      return mysql_fetch_array($query, $type);
+    }
+
+    function mysqli_num_rows($query) {
+      return mysql_num_rows($query);
+    }
+
+    function mysqli_data_seek($query, $offset) {
+      return mysql_data_seek($query, $offsetr);
+    }
+
+    function mysqli_insert_id($link) {
+      return mysql_insert_id($link);
+    }
+
+    function mysqli_free_result($query) {
+      return mysql_free_result($query);
+    }
+
+    function mysqli_fetch_field($query) {
+      return mysql_fetch_field($query);
+    }
+
+    function mysqli_real_escape_string($link, $string) {
+      if ( function_exists('mysql_real_escape_string') ) {
+        return mysql_real_escape_string($string, $link);
+      } elseif ( function_exists('mysql_escape_string') ) {
+        return mysql_escape_string($string);
+      }
+
+      return addslashes($string);
+    }
+  }  
+    
 ?>
