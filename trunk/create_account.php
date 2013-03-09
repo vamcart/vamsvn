6a913481d55f700bcd9a231b289aad713db37e2b
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2013-03-09 15:38:34 +0000

    Корректирока кода, переход с mysql на mysqli

--- trunk/create_account.php
+++ trunk/create_account.php
@@ -525,6 +525,8 @@ if (ACCOUNT_STATE == 'true') {
 			
 			$zone = vam_db_query("select distinct zone_id, zone_name from ".TABLE_ZONES." where zone_country_id = '".(int) $country."' and zone_code = '".vam_db_input($state)."'");
 
+		   $zone = $zone->fetch_array(MYSQLI_ASSOC);
+
 	      if (vam_db_num_rows($zone) > 0) {
 	        $zone_id = $zone['zone_id'];
 	        $zone_name = $zone['zone_name'];
@@ -532,6 +534,8 @@ if (ACCOUNT_STATE == 'true') {
 	      } else {
 
 		   $zone = vam_db_query("select distinct zone_id, zone_name from " . TABLE_ZONES . " where zone_country_id = '" . (int)$country . "'");
+		   
+		   $zone = $zone->fetch_array(MYSQLI_ASSOC);
 
 	      if (vam_db_num_rows($zone) > 0) {
 	          $zone_id = $zone['zone_id'];
