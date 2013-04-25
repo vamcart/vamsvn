bb1569461248fa1bca6b5c35f304fd9a88185750
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2013-04-25 17:51:51 +0000

    Добавлена ссылка отмеить фильтр в бокс и блок фильтры

--- trunk/templates/vamshop/source/boxes/products_filter.php
+++ trunk/templates/vamshop/source/boxes/products_filter.php
@@ -130,7 +130,11 @@ $box_content='';
         }
         $first = false;
         
-        $box_text .=  '<b>' . $specs_array['specification_name'] . '</b><br>';
+        if (isset ($_GET[$var]) && $_GET[$var] != '') {
+        $box_text .=  '<b>' . $specs_array['specification_name'] . '</b> <a href="'.vam_href_link (FILENAME_PRODUCTS_FILTERS, vam_get_all_get_params (array ('f' . $specs_array['specifications_id']) ) ).'"><span class="close-box">[X]</span></a><br />';
+        } else {
+        $box_text .=  '<b>' . $specs_array['specification_name'] . '</b>';
+        }
 
         $filter_index = 0;
         if ($specs_array['filter_show_all'] == 'True') {
