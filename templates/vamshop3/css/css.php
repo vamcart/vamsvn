57316d3ea67074b093699a088c9b698aa1fa1b77
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-01-19 15:46:13 +0000

    Добавлены новые файлы для шаблона vamshop3

--- templates/vamshop3/css/css.php
+++ templates/vamshop3/css/css.php
@@ -1,3 +1,8 @@
 <!--[if IE 7]>
 <link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/font-awesome-ie7.min.css'; ?>"/>
 <![endif]-->
+
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/bootstrap/bootstrap.css'; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/font-awesome.min.css'; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/lightbox/jquery.lightbox.css'; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/sequencejs.css'; ?>" />
--- templates/vamshop3/javascript/script.php
+++ templates/vamshop3/javascript/script.php
@@ -1,4 +1,9 @@
 <script type="text/javascript" src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/javascript/vamshop.js'; ?>"></script>
+
+<script type="text/javascript" src="jscript/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="jscript/jquery/plugins/easing/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="jscript/jquery/plugins/lightbox/jquery.lightbox.js"></script>
+
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
 <script type="text/javascript" src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/javascript/html5.js'; ?>"></script>
