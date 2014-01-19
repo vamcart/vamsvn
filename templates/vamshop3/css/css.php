adfcaaaf3dc7fd3014ad13ac790282c9657c436d
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-01-19 15:27:29 +0000

    Добавлен новый шаблон по умолчанию - vamshop3

---
+++ templates/vamshop3/css/css.php
@@ -0,0 +1,3 @@
+<!--[if IE 7]>
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/font-awesome-ie7.min.css'; ?>"/>
+<![endif]-->
---
+++ templates/vamshop3/javascript/script.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/javascript/vamshop.js'; ?>"></script>
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script type="text/javascript" src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/javascript/html5.js'; ?>"></script>
+<![endif]-->
