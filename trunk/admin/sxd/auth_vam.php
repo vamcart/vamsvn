ff6bfe4f66addc215ef8a12322dbdd99ce5fc8f5
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-01-21 18:44:47 +0000

    Скрипт резервного копирования Sypex Dumper 1 обновлён до текущей версии Sypex Dumper 2

---
+++ trunk/admin/sxd/auth_vam.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package dumper
+ * @copyright Copyright 2005-2010 Andrew Berezin eCommerce-Service.com
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2010 http://sypex.net/
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: auth_zen.php 1.0 20.08.2010 11:25:57 AndrewBerezin $
+ */
+function zen_sdxErrorHandler($errno, $errstr, $errfile, $errline) {
+  global $sxd_error_handler;
+  if ($errno == E_NOTICE) {
+    return null;
+  }
+  return $sxd_error_handler;
+}
+$sxd_error_handler = set_error_handler('zen_sdxErrorHandler', E_NOTICE);
+chdir(dirname(__FILE__) . '/..');
+$loaderPrefix = 'dumper';
+require('includes/application_top.php');
+chdir(dirname(__FILE__));
+set_error_handler($sxd_error_handler);
+if ($_SESSION['customers_status']['customers_status_id'] == 0) {
+	$auth = 1;
+  $this->CFG['my_host'] = DB_SERVER;
+//  $this->CFG['my_port'] = ;
+  $this->CFG['my_user'] = DB_SERVER_USERNAME;
+  $this->CFG['my_pass'] = DB_SERVER_PASSWORD;
+	$this->CFG['my_db']   = DB_DATABASE;
+	$this->CFG['backup_path'] = DIR_FS_ADMIN . 'backups/';
+  //if (ENABLE_SSL_ADMIN == 'true') {
+    //$link = HTTPS_SERVER . DIR_WS_HTTPS_ADMIN;
+  //} else {
+    $link = HTTP_SERVER . DIR_WS_ADMIN;
+  //}
+	$this->CFG['backup_url'] = $link . 'backups/';
+	$this->CFG['lang']    = $_SESSION['language_code'];
+	$this->CFG['exitURL'] = vam_href_link(FILENAME_DEFAULT);
+}
+
---
+++ trunk/admin/sxd/cfg.php
@@ -0,0 +1,23 @@
+<?php
+$CFG = array (
+  'charsets' => 'utf8 latin1',
+  'lang' => 'ru',
+  'time_web' => '600',
+  'time_cron' => '600',
+  'backup_path' => '/home/vamshop/www/admin/backups/',
+  'backup_url' => 'http://vamshop.loc/admin/backups/',
+  'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+  'globstat' => 0,
+  'my_host' => 'localhost',
+  'my_port' => 3306,
+  'my_user' => 'root',
+  'my_pass' => '',
+  'my_comp' => 0,
+  'my_db' => 'vamshop',
+  'auth' => 'vam',
+  'user' => '',
+  'pass' => '',
+  'confirm' => '6',
+  'exitURL' => 'http://vamshop.loc/admin/start.php',
+);
+?>
