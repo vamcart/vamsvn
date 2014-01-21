450000b8e9600b3b982269250647e5401f08a56b
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-01-21 19:38:22 +0000

    Корректировка шаблона vamshop1

--- includes/modules/gift_cart.php
+++ includes/modules/gift_cart.php
@@ -59,8 +59,8 @@ if (isset ($_SESSION['customer_id'])) {
 	$gift->assign('C_FLAG', 'true');
 }
 $gift->assign('LINK_ACCOUNT', vam_href_link(FILENAME_CREATE_ACCOUNT));
-$gift->assign('FORM_ACTION', vam_draw_form('gift_coupon', vam_href_link(FILENAME_SHOPPING_CART, 'action=check_gift', 'NONSSL')));
-$gift->assign('INPUT_CODE', vam_draw_input_field('gv_redeem_code'));
+$gift->assign('FORM_ACTION', vam_draw_form('gift_coupon', vam_href_link(FILENAME_SHOPPING_CART, 'action=check_gift', 'NONSSL'), 'post', 'class="form-inline"'));
+$gift->assign('INPUT_CODE', vam_draw_input_field('gv_redeem_code', '', 'class="input-small"'));
 $gift->assign('BUTTON_SUBMIT', vam_image_submit('submit.png', IMAGE_REDEEM_GIFT));
 $gift->assign('language', $_SESSION['language']);
 $gift->assign('FORM_END', '</form>');
