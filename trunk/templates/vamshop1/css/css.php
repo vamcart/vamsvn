f12556d011a9390036aac3b16fc9fa2d0cbd43ec
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-03-12 09:45:39 +0000

    FontAwesome обновлён до текущей версии 4.0.3

--- trunk/templates/vamshop1/css/css.php
+++ trunk/templates/vamshop1/css/css.php
@@ -1,6 +1,3 @@
 <link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/bootstrap/bootstrap.css'; ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/font-awesome.min.css'; ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/sequencejs.css'; ?>" />
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/font-awesome-ie7.min.css'; ?>"/>
-<![endif]-->
