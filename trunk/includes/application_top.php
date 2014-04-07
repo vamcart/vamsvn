ae845624269dadd3ff2e74a85c68f1501cdc6e3e
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-04-07 09:02:20 +0000

    Корректировка breadcrumb

--- trunk/includes/application_top.php
+++ trunk/includes/application_top.php
@@ -514,7 +514,7 @@ require (DIR_WS_CLASSES.'breadcrumb.php');
 $breadcrumb = new breadcrumb;
 
 //$breadcrumb->add(HEADER_TITLE_TOP, HTTP_SERVER);
-$breadcrumb->add(HEADER_TITLE_CATALOG, HTTP_SERVER . DIR_WS_CATALOG);
+//$breadcrumb->add(HEADER_TITLE_CATALOG, HTTP_SERVER . DIR_WS_CATALOG);
 
 // add category names or the manufacturer name to the breadcrumb trail
 if (isset ($cPath_array)) {
