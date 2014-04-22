ac0d23b69d0f260f8bd155db14badf8ea80441df
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-04-22 19:06:48 +0000

    Добавлен новый модуль оплаты QIWI (протокол REST)

---
+++ trunk/qiwi_rest.php
@@ -0,0 +1,55 @@
+<?php
+/*------------------------------------------------------------------------------
+  $Id: qiwi_rest.php 1310 2009-02-06 19:20:03 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+  -----------------------------------------------------------------------------
+   based on:
+   (c) 2005 Vetal (robox.php,v 1.48 2003/05/27); metashop.ru
+
+  Released under the GNU General Public License
+------------------------------------------------------------------------------*/
+
+function get_var($name, $default = 'none') {
+  return (isset($_GET[$name])) ? $_GET[$name] : ((isset($_POST[$name])) ? $_POST[$name] : $default);
+}
+
+require('includes/application_top.php');
+require (DIR_WS_CLASSES.'order.php');
+
+// logging
+//$fp = fopen('qiwi.log', 'a+');
+//$str=date('Y-m-d H:i:s').' - ';
+//foreach ($_POST as $vn=>$vv) {
+  //$str.=$vn.'='.$vv.';';
+//}
+
+//fwrite($fp, $str."\n");
+//fclose($fp);
+
+$inv_id = $_POST['bill_id'];
+$order = new order($inv_id);
+
+echo '<?xml version="1.0"?><result><result_code>0</result_code></result>';
+
+// checking and handling
+if ($_POST['status'] == 'paid') {
+if (number_format($_POST['amount'],0) == number_format($order->info['total'],0)) {
+  $sql_data_array = array('orders_status' => MODULE_PAYMENT_QIWI_REST_ORDER_STATUS_ID);
+  vam_db_perform('orders', $sql_data_array, 'update', "orders_id='".$inv_id."'");
+
+  $sql_data_arrax = array('orders_id' => $inv_id,
+                          'orders_status_id' => MODULE_PAYMENT_QIWI_REST_ORDER_STATUS_ID,
+                          'date_added' => 'now()',
+                          'customer_notified' => '0',
+                          'comments' => 'QIWI accepted this order payment');
+  vam_db_perform('orders_status_history', $sql_data_arrax);
+
+}
+}
+
+?>
