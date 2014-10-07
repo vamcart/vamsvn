e0df26d29e8e3ee8eaf37a108ee0578a241b725c
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-10-07 14:07:41 +0000

    VamShop 1.74

--- includes/application_top.php
+++ includes/application_top.php
@@ -55,7 +55,7 @@ if (defined('DIR_WS_INCLUDES') === false) header('Location: install');
 $php4_3_10 = (0 == version_compare(phpversion(), "4.3.10"));
 define('PHP4_3_10', $php4_3_10);
 // define the project version
-define('PROJECT_VERSION', 'VamShop 1.73');
+define('PROJECT_VERSION', 'VamShop 1.74');
 
 // set the type of request (secure or not)
 $request_type = (getenv('HTTPS') == '1' || getenv('HTTPS') == 'on') ? 'SSL' : 'NONSSL';
