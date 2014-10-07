320e134d79f6e45992fb4ea9625f050f3d36de1f
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-10-07 14:07:41 +0000

    VamShop 1.74

--- trunk/admin/includes/application_top.php
+++ trunk/admin/includes/application_top.php
@@ -58,7 +58,7 @@
   define('SQL_CACHEDIR',DIR_FS_CATALOG.'cache/');
 
   // Define the project version
-  define('PROJECT_VERSION', 'VamShop 1.73');
+  define('PROJECT_VERSION', 'VamShop 1.74');
 
   // Set the length of the redeem code, the longer the more secure
   define('SECURITY_CODE_LENGTH', '6');
