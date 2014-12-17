150f347caa1374f666c94b373bcd8121a557704e
vamshop@gmail.com <vamshop@gmail.com@92b022b8-f9d0-11de-8173-5df4dc7aba59>
2014-12-17 16:15:29 +0000

    Корректировка стилей главной страницы админки

--- admin/includes/modules/summary/orders.php
+++ admin/includes/modules/summary/orders.php
@@ -27,17 +27,19 @@ defined('_VALID_VAM') or die('Direct Access to this location is not allowed.');
 				  </tr>
 
 </table>
-<table border="0" width="100%" cellspacing="2" cellpadding="0" class="contentListingTable">				  
+<table border="0" width="100%" cellspacing="2" cellpadding="0" class="table table-hover">				  
 
-
-              <tr class="dataTableHeadingRow">
-                <td width="40%" class="dataTableHeadingContent"><?php echo TABLE_HEADING_CUSTOMER; ?></td>
-                <td width="10%" class="dataTableHeadingContent"><?php echo TABLE_HEADING_NUMBER; ?></td>
-                <td width="10%" class="dataTableHeadingContent"><?php echo TABLE_HEADING_ORDER_TOTAL; ?></td>
-                <td width="20%" class="dataTableHeadingContent"><?php echo TABLE_HEADING_STATUS; ?></td>
-                <td width="20%" class="dataTableHeadingContent"><?php echo TABLE_HEADING_DATE; ?></td>
+				<thead>
+              <tr>
+                <th><?php echo TABLE_HEADING_CUSTOMER; ?></th>
+                <th><?php echo TABLE_HEADING_NUMBER; ?></th>
+                <th><?php echo TABLE_HEADING_ORDER_TOTAL; ?></th>
+                <th><?php echo TABLE_HEADING_STATUS; ?></th>
+                <th><?php echo TABLE_HEADING_DATE; ?></th>
               </tr>
+				</thead>
 
+				<tbody>
 <?php
 
 		$orders_query_raw = "select o.orders_id, o.orders_status, o.customers_name, o.date_purchased, o.last_modified, o.currency, o.currency_value, s.orders_status_name, ot.text as order_total from ".TABLE_ORDERS." o left join ".TABLE_ORDERS_TOTAL." ot on (o.orders_id = ot.orders_id), ".TABLE_ORDERS_STATUS." s where (o.orders_status = s.orders_status_id and s.language_id = '".$_SESSION['languages_id']."' and ot.class = 'ot_total') or (o.orders_status = '0' and ot.class = 'ot_total' and  s.orders_status_id = '1' and s.language_id = '".$_SESSION['languages_id']."') order by o.date_purchased desc limit 20";
@@ -73,5 +75,6 @@ defined('_VALID_VAM') or die('Direct Access to this location is not allowed.');
 
 	}
 ?>
+				</tbody>
 
                 </table>
