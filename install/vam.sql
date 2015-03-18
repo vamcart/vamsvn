c1dd0eeacdd75114cc7202708848d5b6eb62c6fb
vamcart <vamshop@gmail.com>
2015-03-18 18:54:37 +0300

    VamShop 1.77

--- install/vam.sql
+++ install/vam.sql
@@ -10,8 +10,8 @@
 #  based on:
 #  (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
 #  (c) 2002-2003 osCommerce (oscommerce.sql,v 1.83); www.oscommerce.com
-#  (c) 2003  nextcommerce (nextcommerce.sql,v 1.76 2003/08/25); www.nextcommerce.org
-#  (c) 2005  xt:Commerce (nextcommerce.sql,v 1.76 2005/08/25); www.xt-commerce.com
+#  (c) 2003  nextcommerce (nextcommerce.sql,v 1.77 2003/08/25); www.nextcommerce.org
+#  (c) 2005  xt:Commerce (nextcommerce.sql,v 1.77 2005/08/25); www.xt-commerce.com
 #
 #  Released under the GNU General Public License
 #
@@ -1811,7 +1811,7 @@ create table cip_depend (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 #database Version
-INSERT INTO database_version(version) VALUES ('1.76');
+INSERT INTO database_version(version) VALUES ('1.77');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
