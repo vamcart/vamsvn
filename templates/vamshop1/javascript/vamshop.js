ac2c1a50d255565674ff8a19376beef735903138
vamcart <vamshop@gmail.com>
2015-05-28 11:14:10 +0300

    Отключены эффекты для полоски навигации

--- templates/vamshop1/javascript/vamshop.js
+++ templates/vamshop1/javascript/vamshop.js
@@ -113,13 +113,10 @@ $(function(){
 
             $(window).scroll(function(){
                 if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default')){
-                    menu.fadeOut('fast', function(){
-                        $(this).removeClass('default').addClass('fixed').fadeIn('fast');
-                    });
+                  $("#navigation").removeClass('default').addClass('fixed');
+         
                 } else if($(this).scrollTop() <= pos.top && menu.hasClass('fixed')){
-                    menu.fadeOut('fast', function(){
-                        $(this).removeClass('fixed').addClass('default').fadeIn('fast');
-                    });
+                  $("#navigation").removeClass('fixed').addClass('default');
                 }
             });
         }
