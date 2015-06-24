7088f8f46d6496ab182c255445f54a4f8eaa8d55
vamcart <vamshop@gmail.com>
2015-06-24 12:38:06 +0300

    Подключён слайдер sequence текущей версии

--- templates/vamshop1/javascript/vamshop.js
+++ templates/vamshop1/javascript/vamshop.js
@@ -124,6 +124,20 @@ $(function(){
 
 });
 
+// Sequence slider
+
+$(document).ready(function(){
+    var options = {
+        nextButton: true,
+        prevButton: true,
+        animateStartingFrameIn: true,
+        autoPlay: true,
+        autoPlayDelay: 4000,
+    };
+    
+    var mySequence = $("#sequence").sequence(options).data("sequence");
+});
+
 // ON DOCUMENT READY
 // -----------------------------------------------------------------------------
 $(document).ready(function(){
