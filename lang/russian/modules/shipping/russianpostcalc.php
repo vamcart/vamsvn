35baa6fcfe1a28954bfd682df88650acc7284acc
vamcart <vamshop@gmail.com>
2015-09-02 22:04:28 +0300

    Добавлен новый модуль доставки Почта России, использующий для расчёта доставки сервис russianpostcalc.ru

---
+++ lang/russian/modules/shipping/russianpostcalc.php
@@ -0,0 +1,41 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: flat.php 899 2009/02/07 13:24:46 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(flat.php,v 1.6 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (flat.php,v 1.4 2003/08/13); www.nextcommerce.org
+   (c) 2004	 xt:Commerce (flat.php,v 1.4 2003/08/13); xt-commerce.com
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_TEXT_TITLE', 'Почта России');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_TEXT_DESCRIPTION', 'Почта России. Расчёт доставки Почтой России с помощью веб-сервиса http://russianpostcalc.ru');
+
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_STATUS_TITLE' , 'Разрешить модуль Почта России');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_STATUS_DESC' , 'Вы хотите разрешить модуль Почта России?');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_ALLOWED_TITLE' , 'Разрешённые страны');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_COST_TITLE' , 'Стоимость доставки');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_COST_DESC' , 'Стоимость доставки данным способом.');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_API_KEY_TITLE' , 'API Ключ');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_API_KEY_DESC' , 'Ваш API Ключ сервиса http://russianpostcalc.ru');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_API_PASSWORD_TITLE' , 'API Пароль');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_API_PASSWORD_DESC' , 'Ваш API Пароль сервиса http://russianpostcalc.ru');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_ZIP_TITLE' , 'Почтовый индекс отправителя');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_ZIP_DESC' , 'Укажите почтовый индекс отправителя.');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_TAX_CLASS_TITLE' , 'Налог');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_TAX_CLASS_DESC' , 'Использовать налог.');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_ZONE_TITLE' , 'Зона');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_ZONE_DESC' , 'Если выбрана зона, то данный модуль доставки будет виден только покупателям из выбранной зоны.');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_SORT_ORDER_TITLE' , 'Порядок сортировки');
+define('MODULE_SHIPPING_RUSSIANPOSTCALC_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
+?>
