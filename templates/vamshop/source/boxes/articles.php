fd869a1e5458ba068937d18f5994711f18c42c23
vamcart <vamshop@gmail.com>
2016-01-03 14:05:01 +0300

    Корректировка бокса Статьи

--- templates/vamshop/source/boxes/articles.php
+++ templates/vamshop/source/boxes/articles.php
@@ -94,7 +94,7 @@ if (!$box->is_cached(CURRENT_TEMPLATE.'/boxes/box_articles.html', $cache_id) || 
   $topics_string = '';
   $tree = array();
 
-  $topics_query = vam_db_query("select t.topics_id, td.topics_name, t.parent_id from " . TABLE_TOPICS . " t, " . TABLE_TOPICS_DESCRIPTION . " td where t.parent_id = '0' and t.topics_id = td.topics_id and td.language_id = '" . (int)$languages_id . "' order by sort_order, td.topics_name");
+  $topics_query = vam_db_query("select t.topics_id, td.topics_name, t.parent_id from " . TABLE_TOPICS . " t, " . TABLE_TOPICS_DESCRIPTION . " td where t.parent_id = '0' and t.topics_id = td.topics_id and td.language_id = '" . (int)$_SESSION['languages_id'] . "' order by sort_order, td.topics_name");
   while ($topics = vam_db_fetch_array($topics_query))  {
     $tree[$topics['topics_id']] = array('name' => $topics['topics_name'],
                                         'parent' => $topics['parent_id'],
