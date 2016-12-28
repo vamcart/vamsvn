aff1528c1b5fcbb6bc356ea3ae60d8c4bb8d8720
vamcart <vamshop@gmail.com>
2016-12-28 18:32:17 +0300

    PHPMailer обновлён до текущей версии 5.2.20

--- includes/external/phpmailer/language/phpmailer.lang-ru.php
+++ includes/external/phpmailer/language/phpmailer.lang-ru.php
@@ -1,23 +1,27 @@
-<?php
-/**
- * PHPMailer language file.  
- * Russian Version
- */
-
-$PHPMAILER_LANG = array();
-
-$PHPMAILER_LANG["provide_address"] = 'Пожалуйста введите минимум один Email' .
-                                     'получателя.';
-$PHPMAILER_LANG["mailer_not_supported"] = ' mailer не поддерживается.';
-$PHPMAILER_LANG["execute"] = 'Невозможно выполнить эту команду: ';
-$PHPMAILER_LANG["instantiate"] = 'Произошла ошибка при инициализации Mail функции.';
-$PHPMAILER_LANG["authenticate"] = 'SMTP ошибка: ошибка авторизации.';
-$PHPMAILER_LANG["from_failed"] = 'Неверный адрес отправителя: ';
-$PHPMAILER_LANG["recipients_failed"] = 'SMTP ошибка: Следующие ' .
-                                       'адреса получателей неверны: ';
-$PHPMAILER_LANG["data_not_accepted"] = 'SMTP ошибка: Данные не были приняты.';
-$PHPMAILER_LANG["connect_host"] = 'SMTP ошибка: SMTP-Host недоступен.';
-$PHPMAILER_LANG["file_access"] = 'В доступе к следующему файлу было отказано: ';
-$PHPMAILER_LANG["file_open"] = 'Не могу открыть файл: ';
-$PHPMAILER_LANG["encoding"] = 'Неизвестный формат кодировки: ';
-?>
+<?php
+/**
+ * Russian PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer
+ * @author Alexey Chumakov <alex@chumakov.ru>
+ * @author Foster Snowhill <i18n@forstwoof.ru>
+ */
+
+$PHPMAILER_LANG['authenticate']         = 'Ошибка SMTP: ошибка авторизации.';
+$PHPMAILER_LANG['connect_host']         = 'Ошибка SMTP: не удается подключиться к серверу SMTP.';
+$PHPMAILER_LANG['data_not_accepted']    = 'Ошибка SMTP: данные не приняты.';
+$PHPMAILER_LANG['encoding']             = 'Неизвестный вид кодировки: ';
+$PHPMAILER_LANG['execute']              = 'Невозможно выполнить команду: ';
+$PHPMAILER_LANG['file_access']          = 'Нет доступа к файлу: ';
+$PHPMAILER_LANG['file_open']            = 'Файловая ошибка: не удается открыть файл: ';
+$PHPMAILER_LANG['from_failed']          = 'Неверный адрес отправителя: ';
+$PHPMAILER_LANG['instantiate']          = 'Невозможно запустить функцию mail.';
+$PHPMAILER_LANG['provide_address']      = 'Пожалуйста, введите хотя бы один адрес e-mail получателя.';
+$PHPMAILER_LANG['mailer_not_supported'] = ' — почтовый сервер не поддерживается.';
+$PHPMAILER_LANG['recipients_failed']    = 'Ошибка SMTP: отправка по следующим адресам получателей не удалась: ';
+$PHPMAILER_LANG['empty_message']        = 'Пустое сообщение';
+$PHPMAILER_LANG['invalid_address']      = 'Не отослано, неправильный формат email адреса: ';
+$PHPMAILER_LANG['signing']              = 'Ошибка подписи: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'Ошибка соединения с SMTP-сервером';
+$PHPMAILER_LANG['smtp_error']           = 'Ошибка SMTP-сервера: ';
+$PHPMAILER_LANG['variable_set']         = 'Невозможно установить или переустановить переменную: ';
+$PHPMAILER_LANG['extension_missing']    = 'Расширение отсутствует: ';
