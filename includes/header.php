30614bd93ec3eae886fc3c086a9c900991e0dd04
vam <vamshop@gmail.com>
2017-02-06 23:26:55 +0300

    Корректировка head заголовка html документа

--- includes/header.php
+++ includes/header.php
@@ -32,16 +32,12 @@
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="<?php echo $_SESSION['language_code']; ?>">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>" />
 
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"/>
 
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png"/>
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png"/>
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png"/>
-<link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png"/>
 <link rel="shortcut icon" href="favicon.ico"/>
 
 <?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
