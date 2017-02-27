93f39e0c55096be5a05cb3348dea33fae67a0acc
vam <vamshop@gmail.com>
2017-02-27 13:58:26 +0300

    Корректировка кода

--- advanced_search_result.php
+++ advanced_search_result.php
@@ -278,25 +278,14 @@ include ('includes/application_bottom.php');
 
 
 function super_unique($array,$key)
-
 {
-
    $temp_array = array();
-
    foreach ($array as &$v) {
-
        if (!isset($temp_array[$v[$key]]))
-
        $temp_array[$v[$key]] =& $v;
-
    }
-
    $array = array_values($temp_array);
-
    return $array;
-
-
-
 }
 
 ?>
