d4486a0f4680333f4916afe28cd3735fd4214ffc
vam <vamshop@gmail.com>
2017-04-26 22:12:23 +0300

    Новый модуль оплаты - Промсвязьбанк

---
+++ psbank.php
@@ -0,0 +1,68 @@
+<?php
+/*------------------------------------------------------------------------------
+  $Id: webmoney.php 1310 2009-02-06 19:20:03 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2007 VaM Shop
+  -----------------------------------------------------------------------------
+   based on:
+   (c) 2005 Vetal (robox.php,v 1.48 2003/05/27); metashop.ru
+
+  Released under the GNU General Public License
+------------------------------------------------------------------------------*/
+
+function get_var($name, $default = 'none') {
+  return (isset($_GET[$name])) ? $_GET[$name] : ((isset($_POST[$name])) ? $_POST[$name] : $default);
+}
+
+require('includes/application_top.php');
+require (DIR_WS_CLASSES.'order.php');
+
+// logging
+//$fp = fopen('webmoney.log', 'a+');
+//$str=date('Y-m-d H:i:s').' - ';
+//foreach ($_REQUEST as $vn=>$vv) {
+//  $str.=$vn.'='.$vv.';';
+//}
+
+//fwrite($fp, $str."\n");
+//fclose($fp);
+// variables prepearing
+$crc = get_var('P_SIGN');
+
+$inv_id = get_var('ORDER')-1000000;
+$order = new order($inv_id);
+$order_sum = $order->info['total'];
+
+$fields = array('AMOUNT', 'CURRENCY', 'ORDER', 'MERCH_NAME', 'MERCHANT', 'TERMINAL', 'EMAIL', 'TRTYPE', 'TIMESTAMP', 'NONCE', 'BACKREF', 'RESULT', 'RC', 'RCTEXT', 'AUTHCODE', 'RRN', 'INT_REF');
+$mac = '';
+foreach($fields as $f)
+{
+	if($_POST[$f] !== '')
+		$mac .= strlen($_POST[$f]).$_POST[$f];
+	else
+		$mac .= '-';
+}
+$sign = hash_hmac('sha1', $mac ,pack('H*', MODULE_PAYMENT_PSBANK_SECRET_KEY));
+
+// checking and handling
+if(strtoupper($sign) == $crc) {
+if (number_format($_POST['AMOUNT'],0) == number_format($order->info['total'],0)) {
+  $sql_data_array = array('orders_status' => MODULE_PAYMENT_PSBANK_ORDER_STATUS_ID);
+  vam_db_perform('orders', $sql_data_array, 'update', "orders_id='".$inv_id."'");
+
+  $sql_data_arrax = array('orders_id' => $inv_id,
+                          'orders_status_id' => MODULE_PAYMENT_PSBANK_ORDER_STATUS_ID,
+                          'date_added' => 'now()',
+                          'customer_notified' => '0',
+                          'comments' => 'PSBank accepted this order payment');
+  vam_db_perform('orders_status_history', $sql_data_arrax);
+
+  echo 'OK'.$inv_id;
+}
+}
+
+?>
