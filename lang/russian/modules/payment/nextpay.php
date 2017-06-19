76baff3614dc83aa7541cc85b86edd60165d7112
vam <vamshop@gmail.com>
2017-06-19 14:52:29 +0300

    Добавлена ссылка на запись в блоге про настройку модуля в описание модуля NextPay

--- lang/russian/modules/payment/nextpay.php
+++ lang/russian/modules/payment/nextpay.php
@@ -20,7 +20,7 @@
 define('MODULE_PAYMENT_NEXTPAY_TEXT_TITLE', 'Оплата картой через NextPay');
 define('MODULE_PAYMENT_NEXTPAY_TEXT_PUBLIC_TITLE', 'Оплата картой через NextPay');
 define('MODULE_PAYMENT_NEXTPAY_TEXT_DESCRIPTION', '');
-define('MODULE_PAYMENT_NEXTPAY_TEXT_ADMIN_DESCRIPTION', 'Модуль приёма оплаты NextPay.');
+define('MODULE_PAYMENT_NEXTPAY_TEXT_ADMIN_DESCRIPTION', 'Модуль приёма оплаты NextPay. Как настраивать модуль NextPay, читайте в нашем блоге <a href="http://blog.vamshop.ru/2017/06/18/%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD-%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9-%D0%BC%D0%BE%D0%B4%D1%83%D0%BB%D1%8C-%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D1%8B-nextpay/">http://blog.vamshop.ru</a>');
   
 define('MODULE_PAYMENT_NEXTPAY_STATUS_TITLE' , 'Разрешить модуль NextPay');
 define('MODULE_PAYMENT_NEXTPAY_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
