68e221dade91ff94a62e3eaf4ded7316b5492749
vam <vamshop@gmail.com>
2017-09-14 11:32:16 +0300

    Корректировка кода

--- templates/vamshop1/source/boxes/reviews.php
+++ templates/vamshop1/source/boxes/reviews.php
@@ -67,6 +67,7 @@ $box_content = '<p><a href="' . vam_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'pr
   $box->caching = 1;
   $box->cache_lifetime=CACHE_LIFETIME;
   $box->cache_modified_check=CACHE_CHECK;
+  (!isset($random_product['reviews_id'])) ? $random_product['reviews_id'] = 0 : $random_product['reviews_id'];
   $cache_id = $_SESSION['language'].$random_product['reviews_id'].$product->data['products_id'].$_SESSION['language'];
   $box_reviews= $box->fetch(CURRENT_TEMPLATE.'/boxes/box_reviews.html',$cache_id);
   }
