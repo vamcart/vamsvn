e6879b6574082317f27c6baa662c8a7872b5c835
vam <vamshop@gmail.com>
2018-05-02 20:37:23 +0300

    Корректировка кода для совместимости с php 7.2

--- inc/vam_db_query.inc.php
+++ inc/vam_db_query.inc.php
@@ -21,6 +21,9 @@
   include_once(DIR_FS_INC . 'vam_db_error.inc.php');
   
   function vam_db_query($query, $link = 'db_link') {
+
+  	if (!defined("STORE_DB_TRANSACTIONS")) define("STORE_DB_TRANSACTIONS", "false");
+
     global $$link;
     global $query_counts;
     $query_counts++; 
