635951931b0e6f17166c65eca77447d9030742dd
vam <vamshop@gmail.com>
2018-05-05 20:52:08 +0300

    Добавлена картинка для статей

--- lang/russian/russian.php
+++ lang/russian/russian.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: russian.php 1260 2014/08/12 13:25:47 VaM $
+   $Id: russian.php 1260 2014/08/13 13:25:47 VaM $
 
    VaM Shop - open source ecommerce solution
    http://vamshop.ru
