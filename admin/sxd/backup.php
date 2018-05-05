60bb7631190d9960aee48be4548fdb5278eefaf9
vam <vamshop@gmail.com>
2018-05-05 10:53:44 +0300

    Корректировка резервного копирования

--- admin/sxd/backup.php
+++ admin/sxd/backup.php
@@ -1852,14 +1852,3 @@ function sxd_antimagic($arr)
     return is_array($arr) ? array_map('sxd_antimagic', $arr) : stripslashes($arr);
 }
 
-function mysqli_result($res,$row=0,$col=0){ 
-    $numrows = mysqli_num_rows($res); 
-    if ($numrows && $row <= ($numrows-1) && $row >=0){
-        mysqli_data_seek($res,$row);
-        $resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
-        if (isset($resrow[$col])){
-            return $resrow[$col];
-        }
-    }
-    return false;
-}
