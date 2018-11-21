0a374705760c51b28e5c83183d262edf3cce8a76
vam <vamshop@gmail.com>
2018-11-21 12:25:28 +0300

    VamShop 1.93

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -60,7 +60,7 @@
   define('SQL_CACHEDIR',DIR_FS_CATALOG.'cache/');
 
   // Define the project version
-  define('PROJECT_VERSION', 'VamShop 1.92');
+  define('PROJECT_VERSION', 'VamShop 1.93');
 
   // Set the length of the redeem code, the longer the more secure
   define('SECURITY_CODE_LENGTH', '6');
