f75a049654c43fbde0153d1d38a63c68602b03dc
vam <vamshop@gmail.com>
2018-12-21 16:13:39 +0300

    VamShop 1.94

--- install/vam.sql
+++ install/vam.sql
@@ -1844,7 +1844,7 @@ create table cip_depend (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 #database Version
-INSERT INTO database_version(version) VALUES ('1.93');
+INSERT INTO database_version(version) VALUES ('1.94');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
