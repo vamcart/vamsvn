9b591ea8aee6eaac36a5c5eac1f9a52e9bb4fae3
vam <vamshop@gmail.com>
2018-12-28 20:45:40 +0300

    Добавлен новый модуль - Наценка на выбраный способ доставки

---
+++ lang/russian/modules/order_total/ot_surchargeshipping.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id: ot_surchargeshipping.php,v 1.0 2003/6/19 23:09:49 wib Exp $
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+  Copyright (c) 2008 osCommerce
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_TITLE', 'Наценка на способ доставки');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_DESCRIPTION', 'Наценка на способ доставки');
+
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_STATUS_TITLE','Разрешить модуль');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_STATUS_DESC','Разрешить модуль наценки на способ доставки?');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_SORT_ORDER_TITLE','Порядок сортировки');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_SORT_ORDER_DESC','Порядок сортировки.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_SHIPPING_TITLE','Учитывать доставку');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_SHIPPING_DESC','Учитывать доставку при расчёте.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_TAX_TITLE','Учитывать налоги');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_INC_TAX_DESC','Учитывать налоги при расчёте.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_PERCENTAGE_TITLE','Процент наценки');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_PERCENTAGE_DESC','Процент наценки (процент от стоимости заказа).');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_CALC_TAX_TITLE','Считать налог');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_CALC_TAX_DESC','Пересчитывать налог при наценке.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_MINIMUM_TITLE','Минимальная сумма заказа');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_MINIMUM_DESC','Минимальная сумма заказа, к которому будет добавлена наценка.');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_TYPE_TITLE','Модуль доставки');
+  define('MODULE_ORDER_TOTAL_SURCHARGESHIPPING_TYPE_DESC','Выбранный модуль доставки для наценки');
+  
+?>
