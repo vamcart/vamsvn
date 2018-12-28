a392a273cc2dd867f86d76bddfddc40517e35633
vam <vamshop@gmail.com>
2018-12-28 20:41:04 +0300

    Добавлено новый модуль - Скидка на модуль оплаты

---
+++ includes/modules/order_total/ot_discountpayment.php
@@ -0,0 +1,140 @@
+<?php
+/*
+  $Id: ot_discountpayment.php,v 1.0 2003/06/19 01:13:43 hpdl wib $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  class ot_discountpayment {
+    var $title, $output;
+
+    function ot_discountpayment() {
+      $this->code = 'ot_discountpayment';
+      $this->title = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TITLE;
+      $this->description = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_DESCRIPTION;
+      $this->enabled = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS;
+      $this->sort_order = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER;
+      $this->include_shipping = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING;
+      $this->include_tax = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX;
+      $this->percentage = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE;
+      $this->minimum = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM;
+      $this->calculate_tax = MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX;
+//      $this->credit_class = true;
+      $this->output = array();
+    }
+
+    function process() {
+     global $order, $vamPrice;
+
+      $od_amount = $this->calculate_fee($this->get_order_total());
+      if ($od_amount>0) {
+      $this->addition = $od_amount;
+      $this->output[] = array('title' => $this->title . ':',
+                              'text' => '<b>-' . $vamPrice->format($od_amount,true) . '</b>',
+                              'value' => $od_amount);
+    $order->info['total'] = $order->info['total'] - $od_amount;  
+}
+    }
+    
+
+  function calculate_fee($amount) {
+    global $order;
+    $customer_id = $_SESSION['customer_id'];
+    $payment = $_SESSION['payment'];
+    $od_amount=0;
+    $od_pc = $this->percentage; //this is percentage plus the base fee
+    $do = false;
+    if ($amount > $this->minimum) {
+    $table = preg_split("[,]" , MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE);
+    for ($i = 0; $i < count($table); $i++) {
+          if ($payment == $table[$i]) $do = true;
+        }
+    if ($do) {
+// Calculate tax reduction if necessary
+    if($this->calculate_tax == 'true') {
+// Calculate main tax reduction
+      $tod_amount = round($order->info['tax']*10)/10*$od_pc/100;
+      $order->info['tax'] = $order->info['tax'] + $tod_amount;
+// Calculate tax group deductions
+      reset($order->info['tax_groups']);
+      while (list($key, $value) = each($order->info['tax_groups'])) {
+        $god_amount = round($value*10)/10*$od_pc/100;
+        $order->info['tax_groups'][$key] = $order->info['tax_groups'][$key] + $god_amount;
+      }  
+    }
+    $od_amount = round($amount*10)/10*$od_pc/100;
+    $od_amount = $od_amount + $tod_amount;
+    }
+    }
+    return $od_amount;
+  }
+
+   
+  function get_order_total() {
+    global  $order;
+    $cart = $_SESSION['cart'];
+    $order_total = $order->info['total'];
+// Check if gift voucher is in cart and adjust total
+    $products = $cart->get_products();
+    for ($i=0; $i<sizeof($products); $i++) {
+      $t_prid = vam_get_prid($products[$i]['id']);
+      $gv_query = vam_db_query("select products_price, products_tax_class_id, products_model from " . TABLE_PRODUCTS . " where products_id = '" . $t_prid . "'");
+      $gv_result = vam_db_fetch_array($gv_query);
+      if (preg_match('/^GIFT/', addslashes($gv_result['products_model']))) { 
+        $qty = $cart->get_quantity($t_prid);
+        $products_tax = vam_get_tax_rate($gv_result['products_tax_class_id']);
+        if ($this->include_tax =='false') {
+           $gv_amount = $gv_result['products_price'] * $qty;
+        } else {
+          $gv_amount = ($gv_result['products_price'] + vam_calculate_tax($gv_result['products_price'],$products_tax)) * $qty;
+        }
+        $order_total=$order_total - $gv_amount;
+      }
+    }
+    if ($this->include_tax == 'false') $order_total=$order_total-$order->info['tax'];
+    if ($this->include_shipping == 'false') $order_total=$order_total-$order->info['shipping_cost'];
+    return $order_total;
+  }  
+
+    
+    function check() {
+      if (!isset($this->check)) {
+        $check_query = vam_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS'");
+        $this->check = vam_db_num_rows($check_query);
+      }
+
+      return $this->check;
+    }
+
+    function keys() {
+      return array('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS', 'MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER','MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE','MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM', 'MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE', 'MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING', 'MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX', 'MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX');
+    }
+
+    function install() {
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS', 'true', '6', '1','vam_cfg_select_option(array(\'true\', \'false\'), ', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER', '93', '6', '2', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function ,date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING', 'true', '6', '5', 'vam_cfg_select_option(array(\'true\', \'false\'), ', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function ,date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX', 'true', '6', '6','vam_cfg_select_option(array(\'true\', \'false\'), ', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE', '3', '6', '7', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function ,date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX', 'false', '6', '5','vam_cfg_select_option(array(\'true\', \'false\'), ', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM', '', '6', '2', now())");
+      vam_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE', 'cod', '6', '2', now())");
+    }
+
+    function remove() {
+      $keys = '';
+      $keys_array = $this->keys();
+      for ($i=0; $i<sizeof($keys_array); $i++) {
+        $keys .= "'" . $keys_array[$i] . "',";
+      }
+      $keys = substr($keys, 0, -1);
+
+      vam_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key in (" . $keys . ")");
+    }
+  }
+?>
---
+++ lang/russian/modules/order_total/ot_discountpayment.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $Id: ot_discountpayment.php,v 1.0 2003/6/19 23:09:49 wib Exp $
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+  Copyright (c) 2008 osCommerce
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TITLE', 'Скидка на способ оплаты');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_DESCRIPTION', 'Скидка на способ оплаты');
+
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS_TITLE','Разрешить модуль');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_STATUS_DESC','Разрешить модуль скидки на способ оплаты?');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER_TITLE','Порядок сортировки');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_SORT_ORDER_DESC','Порядок сортировки.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING_TITLE','Учитывать доставку');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_SHIPPING_DESC','Учитывать доставку при расчёте.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX_TITLE','Учитывать налоги');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_INC_TAX_DESC','Учитывать налоги при расчёте.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE_TITLE','Процент скидки');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_PERCENTAGE_DESC','Процент скидки (процент от стоимости заказа).');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX_TITLE','Считать налог');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_CALC_TAX_DESC','Пересчитывать налог при скидке.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM_TITLE','Минимальная сумма заказа');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_MINIMUM_DESC','Минимальная сумма заказа, к которому будет добавлена скидка.');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE_TITLE','Модуль оплаты');
+  define('MODULE_ORDER_TOTAL_DISCOUNTPAYMENT_TYPE_DESC','Выбранный модуль оплаты для скидки');
+  
+?>
