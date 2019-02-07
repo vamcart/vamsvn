7aef7c2826232bc68372fd9da5610017c0285a51
vam <vamshop@gmail.com>
2019-02-07 23:45:29 +0300

    Корректировка кода в модуле доп. полей покупателя, для совместимости с php 7.2

--- includes/functions/customers_extra_fields.php
+++ includes/functions/customers_extra_fields.php
@@ -15,7 +15,7 @@
 
   function vam_get_extra_fields($customer_id,$languages_id){
           $extra_fields_query = vam_db_query("select ce.fields_id, ce.fields_input_type, ce.fields_input_value, ce.fields_required_status, cei.fields_name, ce.fields_status, ce.fields_input_type from " . TABLE_EXTRA_FIELDS . " ce, " . TABLE_EXTRA_FIELDS_INFO . " cei where ce.fields_status=1 and cei.fields_id=ce.fields_id and cei.languages_id =" . $languages_id);
-          $extra_fields_string ='';
+          $extra_fields_string = array();
           $extra_fields_string_name ='';
           $extra_fields_string_value .='';
           if(vam_db_num_rows($extra_fields_query)>0){
@@ -44,7 +44,7 @@
 
 									switch($extra_fields['fields_input_type'])
 									{
-									  case  0: $extra_fields_string_value = vam_draw_input_field('fields_' . $extra_fields['fields_id'],$value). (($extra_fields['fields_required_status']==1) ? '&nbsp;<span class="Requirement">*</span>': ''); break;
+									  case  0: $extra_fields_string_value = vam_draw_input_field('fields_' . $extra_fields['fields_id'],$value, 'id="fields_'.$extra_fields['fields_id'].'"'). (($extra_fields['fields_required_status']==1) ? '&nbsp;<span class="Requirement">*</span>': ''); break;
 									  case  1: $extra_fields_string_value = vam_draw_textarea_field('fields_' . $extra_fields['fields_id'], 'soft', 50, 6,$value,'style="width:400px;"'). (($extra_fields['fields_required_status']==1) ? '&nbsp;<span class="Requirement">*</span>': ''); break;
 									  case  2:
 									      $extra_fields_string_value = '';
