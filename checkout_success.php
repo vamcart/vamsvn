4b095d991ee3cc1b14b615e4572e048855839ed9
vam <vamshop@gmail.com>
2019-03-21 22:09:43 +0300

    Корректировка кода яндекс метрики

--- checkout_success.php
+++ checkout_success.php
@@ -235,8 +235,6 @@ dataLayer.push({
         ecommerce:"dataLayer"
    });
 </script>
-
-<noscript><div><img src="https://mc.yandex.ru/watch/'.YANDEX_METRIKA_ID.'?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 		    ';
 
