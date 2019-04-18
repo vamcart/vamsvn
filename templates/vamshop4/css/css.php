a6882e32879d91ccfe3da8bc4a2a114cadfc6344
vam <vamshop@gmail.com>
2019-04-18 19:19:15 +0300

    Корректировка шаблона vamshop4

--- templates/vamshop4/css/css.php
+++ templates/vamshop4/css/css.php
@@ -1,25 +1,14 @@
-<!--
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/bootstrap/bootstrap.css'; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/font-awesome.min.css'; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/sequencejs.css'; ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/jpushmenu.css'; ?>" />
-
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/stylesheet.css'; ?>" />
--->
-
 <link href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/bootstrap.min.css'; ?>" rel="stylesheet">
-
-<link href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/hover-min.css'; ?>" rel="stylesheet">
-
-<link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" rel="stylesheet">
-
 <link href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/bootstrap-ecommerce/ui.css'; ?>" rel="stylesheet">
 <link href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/bootstrap-ecommerce/responsive.css'; ?>" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/vamshop4.css.php'; ?>" />
+
+<link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" rel="stylesheet">
 
 <link rel="stylesheet" href="jscript/jquery/plugins/owl/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="jscript/jquery/plugins/owl/assets/owl.theme.default.min.css">
 
-<link rel="stylesheet" type="text/css" href="<?php echo 'templates/'.CURRENT_TEMPLATE.'/css/vamshop4.css.php'; ?>" />
 
 <?php
 if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO)) {
