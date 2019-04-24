94cd940659217e016d4dfe400d908327e74c97d7
vam <vamshop@gmail.com>
2019-04-24 20:24:13 +0300

    Новый дизайн блока отзывов на странице карточки товара в шаблоне vamshop4

--- includes/classes/product.php
+++ includes/classes/product.php
@@ -133,16 +133,30 @@ class product {
 		if (vam_db_num_rows($reviews_query)) {
 			$row = 0;
 			$data_reviews = array ();
+			$star_rating = '';
 			while ($reviews = vam_db_fetch_array($reviews_query)) {
 				$row ++;
+
+				$star_rating = '';
+				for($i=0;$i<number_format($reviews['reviews_rating']);$i++)	{
+				$star_rating .= '<span class="rating"><i class="fa fa-star"></i></span> ';
+				}
+		
 				$data_reviews[] = array (
 				
+				'PRODUCTS_LINK' => vam_href_link(FILENAME_PRODUCT_INFO, 'products_id='.$reviews['products_id']), 
+				'REVIEWS_LINK' => vam_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id='.$reviews['products_id'].'&reviews_id='.$reviews['reviews_id']), 
+				'REVIEWS_ALL_LINK' => vam_href_link(FILENAME_PRODUCT_REVIEWS, 'products_id='.$reviews['products_id']), 
 				'AUTHOR' => $reviews['customers_name'], 
+				'ID' => $reviews['reviews_id'], 
+				'URL' => vam_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id='.$reviews['products_id'].'&reviews_id='.$reviews['reviews_id']), 
 				'DATE' => vam_date_short($reviews['date_added']), 
-				'RATING' => vam_image('templates/'.CURRENT_TEMPLATE.'/img/stars_'.$reviews['reviews_rating'].'.gif', sprintf(TEXT_OF_5_STARS, $reviews['reviews_rating'])), 
-				'RATING_TXT' => $reviews['reviews_rating'], 
-				'TEXT' => $reviews['reviews_text'],
-				'TEXT_BREAK' => vam_break_string(nl2br(htmlspecialchars($reviews['reviews_text'])), 60, '-<br />')
+				//'TEXT_COUNT' => '('.sprintf(TEXT_REVIEW_WORD_COUNT, vam_word_count($reviews['reviews_text'], ' ')).')<br />'.vam_break_string(htmlspecialchars($reviews['reviews_text']), 60, '-<br />').'..', 
+				'TEXT' => $reviews['reviews_text'], 
+				'RATING' => $reviews['reviews_rating'],
+				'STAR_RATING' => $star_rating,
+				'RATING_IMG' => vam_image('templates/'.CURRENT_TEMPLATE.'/img/stars_'.$reviews['reviews_rating'].'.gif', sprintf(TEXT_OF_5_STARS, $reviews['reviews_rating']))
+
 				
 				);
 				if ($row == PRODUCT_REVIEWS_VIEW)
