9c71bb65621d3f1485c1a4acace24f599a44668b
vam <vamshop@gmail.com>
2019-06-02 13:36:42 +0300

    Скрипты пересены в footer в шаблоне vamshop1, в шапке оставлен только jQuery

--- templates/vamshop1/javascript/script.php
+++ templates/vamshop1/javascript/script.php
@@ -1,10 +1,4 @@
 <script src="jscript/jquery/jquery.js"></script>
-<script src="jscript/bootstrap/bootstrap.min.js"></script>
-<script src="jscript/jquery/plugins/easing/jquery.easing.1.3.js"></script>
-<script src="jscript/jquery/plugins/jpushmenu/v2p.js"></script>
-<script src="jscript/jquery/plugins/jpushmenu/jpushmenu.js"></script>
-<script src="jscript/jquery/plugins/scrollup/jquery.scrollup.min.js"></script>
-<script src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/javascript/vamshop.js'; ?>"></script>
 
 <?php
 if (file_exists(dirname(__FILE__) . '/local.js.php')) include('local.js.php');
