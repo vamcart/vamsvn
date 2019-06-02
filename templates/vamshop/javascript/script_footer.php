95f56a664eb3d934c1b81a2fecc64644cf1e2f03
vam <vamshop@gmail.com>
2019-06-02 13:33:36 +0300

    Часть скриптов в шаблоне vamshop перенесена в footer

---
+++ templates/vamshop/javascript/script_footer.php
@@ -0,0 +1,101 @@
+<?php if (AJAX_CART == 'true') { ?>
+<script src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/javascript/jscript_ajax_cart.js'; ?>"></script>
+<script>
+function cartPopupOn(){ 
+$('body').append('<div id="load_status_bg"></div><div class="cart_popup"><div class="cart_popuptext"><?php echo TEXT_POPUP_CART_ADD; ?></div><div class="cart_popuplink"><a href="checkout.php" class="button"><span><img src="<?php echo DIR_WS_CATALOG; ?>images/icons/buttons/buy.png" alt="" title="" width="12" height="12" />&nbsp;<?php echo TEXT_POPUP_CART_CHECKOUT; ?></span></a><br /><br /><a href="javascript:cartPopupOff()" class="button"><span><img src="<?php echo DIR_WS_CATALOG; ?>images/icons/buttons/back.png" alt="" title="" width="12" height="12" />&nbsp;<?php echo TEXT_POPUP_CART_CONTINUE; ?></span></a></div></div>'); 
+$('#load_status_bg').show().css({'filter' : 'alpha(opacity=80)'}); 
+$('.cart_popup').show(); 
+$(document).click(function (){
+cartPopupOff();
+});
+};
+
+function cartPopupOff(){ 
+$('.cart_popup').hide(); 
+$('#load_status_bg').remove('#load_status_bg'); 
+};
+</script>
+<?php } ?>
+<?php
+if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO)) {
+?>
+<script src="jscript/jquery/plugins/colorbox/jquery.colorbox-min.js"></script>
+<?php
+if (file_exists(DIR_FS_CATALOG.'jscript/jquery/plugins/colorbox/i18n/jquery.colorbox-'.$_SESSION['language_code'].'.js')) {
+?>
+<script src="jscript/jquery/plugins/colorbox/i18n/jquery.colorbox-ru.js"></script>
+<?php } ?>
+<script>
+// Make ColorBox responsive
+	jQuery.colorbox.settings.maxWidth  = '95%';
+	jQuery.colorbox.settings.maxHeight = '95%';
+
+	// ColorBox resize function
+	var resizeTimer;
+	function resizeColorBox()
+	{
+		if (resizeTimer) clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+				if (jQuery('#cboxOverlay').is(':visible')) {
+						jQuery.colorbox.load(true);
+				}
+		}, 300);
+	}
+
+	// Resize ColorBox when resizing window or changing mobile device orientation
+	jQuery(window).resize(resizeColorBox);
+	
+$(document).ready(function(){
+  $(".lightbox").colorbox({rel:"lightbox", title: false});
+  $(".iframe").colorbox({iframe:true, width:"30%", height:"80%"});
+});
+</script>
+<?php
+ }
+?>
+<script>
+
+// Register service worker to control making site work offline
+
+$(function(){
+	
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/sw.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
+
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+const addBtn = document.querySelector('.a2hs-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
+});
+
+</script>
