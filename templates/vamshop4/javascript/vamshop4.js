e577dfad2de5d3cece8c980e5e019a4e4af3cd7c
vam <vamshop@gmail.com>
2019-06-19 22:48:35 +0300

    Добавлены файлы

---
+++ templates/vamshop4/javascript/vamshop4.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	
+$(".owl-carousel").owlCarousel({
+    margin: 10,
+    nav: true,
+    loop:false,
+    dots: false,
+    navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        576:{
+            items:2,
+            nav:true
+        },
+        768:{
+            items:3,
+            nav:true
+        },
+        992:{
+            items:3,
+            nav:true,
+            loop:false
+        },
+        1200:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+// Scroll to top button 
+	$(function () {
+		$.scrollUp({
+	        scrollName: 'scrollup', // Element ID
+	        scrollDistance: 200, // Distance from top/bottom before showing element (px)
+	        scrollFrom: 'top', // 'top' or 'bottom'
+	        scrollSpeed: 500, // Speed back to top (ms)
+	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
+	        animation: 'fade', // Fade, slide, none
+	        animationSpeed: 500, // Animation in speed (ms)
+	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
+					//scrollTarget: false, // Set a custom target element for scrolling to the top
+	        scrollText: '<i class="fa fa-chevron-up"></i>', // Text for element, can contain HTML
+	        scrollTitle: false, // Set a custom <a> title if required.
+	        scrollImg: false, // Set true to use image
+	        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+	        zIndex: 2147483647 // Z-Index for the overlay
+		});
+	});
+
+
+});
