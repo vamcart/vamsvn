a25d2743c03d6dd4c3cebe9beb4f407e99aaad7c
vam <vamshop@gmail.com>
2019-06-30 21:18:31 +0300

    Корректировка js кода

--- templates/vamshop4/javascript/jscript_ajax_cart.js
+++ templates/vamshop4/javascript/jscript_ajax_cart.js
@@ -13,7 +13,7 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-function doBuyNow( id, quantity, update, get_cart = 0 ) {
+function doBuyNow( id, quantity, update, get_cart ) {
 
   // Setup the ajax indicator
  $('body').append('<div id="ajaxLoading"><img src="images/loading.gif"></div>');
