b534174d7c30c52084ddc478e9d918e8f7eb5da3
vam <vamshop@gmail.com>
2019-07-03 19:39:25 +0300

    Удалён артикул из meta title

--- includes/modules/metatags.php
+++ includes/modules/metatags.php
@@ -42,7 +42,7 @@ $cat_query = vamDBquery("SELECT
                                  );
 $cat_data = vam_db_fetch_array($cat_query, true);         
 ?>	
-<title><?php echo $title.' '.$product->data['products_model'] . ' - ' . $cat_data['categories_name']; ?></title>
+<title><?php echo $title.' '.' - ' . $cat_data['categories_name']; ?></title>
 <meta name="description" content="<?php echo $description; ?>" />
 <meta name="keywords" content="<?php echo $product->data['products_meta_keywords']; ?>" />
 <meta property="og:title" content="<?php echo vam_parse_input_field_data($product->data['products_name'], array('"' => '&quot;')); ?>" />
