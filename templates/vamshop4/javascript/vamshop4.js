c604fd852e90be4aabc6e881dbfeae8d0d11cab1
vam <vamshop@gmail.com>
2019-09-26 21:16:51 +0300

    Корректировка слайдера доп. картинок

--- templates/vamshop4/javascript/vamshop4.js
+++ templates/vamshop4/javascript/vamshop4.js
@@ -77,6 +77,23 @@ $(".owl-carousel").owlCarousel({
 	 });     
 	   });
 
+// Bootstrap carousel equal heights
+function normalizeSlideHeights() {
+    $('.carousel').each(function(){
+      var items = $('.carousel-item', this);
+      // reset the height
+      items.css('min-height', 0);
+      // set the height
+      var maxHeight = Math.max.apply(null, 
+          items.map(function(){
+              return $(this).outerHeight()}).get() );
+      items.css('min-height', maxHeight + 'px');
+    })
+}
+
+$(window).on(
+    'load resize orientationchange', 
+    normalizeSlideHeights);
 // Ajax quick search
   $("#quick_find_keyword").keyup(function(){
       var searchString = $("#quick_find_keyword").val(); 
