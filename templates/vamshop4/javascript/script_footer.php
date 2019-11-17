89fef44a2ebc629b31a07d711834fd5824c04fda
vam <vamshop@gmail.com>
2019-11-17 22:05:49 +0300

    Корректирока подключения jQuery UI

--- templates/vamshop4/javascript/script_footer.php
+++ templates/vamshop4/javascript/script_footer.php
@@ -4,7 +4,7 @@ $bender = new Bender();
 $bender->enqueue("templates/".CURRENT_TEMPLATE."/javascript/popper.min.js");
 $bender->enqueue("templates/".CURRENT_TEMPLATE."/javascript/bootstrap.min.js");
 $bender->enqueue("jscript/jquery/plugins/owl/owl.carousel.min.js");
-$bender->enqueue("jscript/jquery/plugins/ui/jquery-ui-min.js");
+$bender->enqueue("jscript/jquery/plugins/jquery-ui/jquery-ui-min.js");
 $bender->enqueue("jscript/jquery/plugins/cookie/jquery.cookie.js");
 $bender->enqueue("jscript/jquery/plugins/scrollup/jquery.scrollup.min.js");
 $bender->enqueue("templates/".CURRENT_TEMPLATE."/javascript/vamshop4.js");
