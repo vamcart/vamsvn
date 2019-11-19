be7ad926dd04e4ba213d09f5544d437b46494d04
vam <vamshop@gmail.com>
2019-11-19 17:32:19 +0300

    Корректирока версии в базе данных

--- install/vam.sql
+++ install/vam.sql
@@ -1861,7 +1861,7 @@ create table cip_depend (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 #database Version
-INSERT INTO database_version(version) VALUES ('1.99.8');
+INSERT INTO database_version(version) VALUES ('1.99.9');
 
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('0', 'information');
 INSERT INTO cm_file_flags (file_flag, file_flag_name) VALUES ('1', 'content');
