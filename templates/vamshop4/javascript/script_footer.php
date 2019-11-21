e0cadf209dec565751e43c6e2839d3639fe6fdcc
vam <vamshop@gmail.com>
2019-11-21 17:28:49 +0300

    Корректировка кода a2hs кнопки

--- templates/vamshop4/javascript/script_footer.php
+++ templates/vamshop4/javascript/script_footer.php
@@ -81,55 +81,5 @@ $(document).ready(function(){
  }
 ?>
 <?php
-if (ENABLE_SERVICE_WORKER == 'true') {
-?>
-<script>
-// Register service worker to control making site work offline
-
-$(function(){
-	
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-           .then(function() { console.log('Service Worker Registered'); });
-}
-
-// Code to handle install prompt on desktop
-
-let deferredPrompt;
-const addBtn = document.querySelector('.a2hs-button');
-addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
-});
-
-});
-</script>
-<?php
-}
-?>
-<?php
 if (file_exists(dirname(__FILE__) . '/local_footer.js.php')) include('local_footer.js.php');
 ?>
