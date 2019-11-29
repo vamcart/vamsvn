52c72ea58552db3b74f7b425394489448257e3b2
vam <vamshop@gmail.com>
2019-11-29 14:42:08 +0300

    Корректировка смены статуса заказа

--- admin/orders.php
+++ admin/orders.php
@@ -178,6 +178,7 @@ if (isset($_POST['submit']) && isset($_POST['multi_orders'])){
               } else {
               $customer_id1 = $customer['customers_id'];
               }
+              if ($customer_id1 > 0) {
               $statuses_groups_query = vam_db_query("select orders_status_id from " . TABLE_CUSTOMERS_STATUS_ORDERS_STATUS . " where customers_status_id = " . (int)$groups['customers_status_id']);
               $purchase_query = "select sum(ot.value) as total from " . TABLE_ORDERS_TOTAL . " as ot, " . TABLE_ORDERS . " as o where ot.orders_id = o.orders_id and o.customers_id = " . $customer_id1 . " and ot.class = 'ot_total' and (";
               $statuses = vam_db_fetch_array($statuses_groups_query);
@@ -232,6 +233,7 @@ if (isset($_POST['submit']) && isset($_POST['multi_orders'])){
            $customers_groups_name = @mysqli_result($groups_query, 0, "customers_status_name");
            $limit = @mysqli_result($groups_query, 0, "customers_status_accumulated_limit");
            $current_discount = @mysqli_result($groups_query, 0, "customers_status_discount");
+           }
 
 			if ($customer['customers_status'] != $customers_groups_id) {           
 
@@ -363,11 +365,7 @@ switch ($_GET['action']) {
 				$notify_comments = '';
 				if ($_POST['notify_comments'] == 'on') {
 					//$notify_comments = sprintf(EMAIL_TEXT_COMMENTS_UPDATE, $comments)."\n\n";
-					if (EMAIL_USE_HTML == 'true') {
-					$notify_comments = nl2br($comments);
-					} else {
 					$notify_comments = $comments;
-					}
 				} else {
 					$notify_comments = '';
 				}
