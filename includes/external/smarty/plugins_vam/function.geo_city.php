8b91d85075d67da69ee383477e048fb6075e55ab
vam <vamshop@gmail.com>
2020-01-22 23:13:13 +0300

    Корректирока метки geo_city для cloudflare, что б правильно определял город для пользователей cloudflare

--- includes/external/smarty/plugins_vam/function.geo_city.php
+++ includes/external/smarty/plugins_vam/function.geo_city.php
@@ -12,6 +12,8 @@ function smarty_function_geo_city($params, $template)
   require_once('vendor/GeoCity/SxGeo.php');
   $SxGeo= new SxGeo('vendor/GeoCity/SxGeo.dat');
   $city = $SxGeo->get($_SERVER['REMOTE_ADDR']);
+  // Cloud flare original user ip
+  if ($_SERVER['HTTP_CF_CONNECTING_IP'] != '') $city = $SxGeo->get($_SERVER['HTTP_CF_CONNECTING_IP']);
   
   $city_name = ($_SESSION['language'] == 'russian') ? $city['city']['name_ru'] : $city['city']['name_en'];
 
