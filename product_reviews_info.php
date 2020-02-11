7cd386ae41b46494cdfb7359fd4cedeb4f48e87a
vam <vamshop@gmail.com>
2020-02-11 11:23:26 +0300

    Корректировка семантической разметки

--- product_reviews_info.php
+++ product_reviews_info.php
@@ -84,7 +84,62 @@ $reviews = vam_db_fetch_array($reviews_query, true);
 		)
 		);	
 
-$vamTemplate->assign('PRODUCTS_NAME', $reviews['products_name']);
+$products_price = $vamPrice->GetPrice($product->data['products_id'], $format = true, 1, $product->data['products_tax_class_id'], $product->data['products_price'], 1);
+		
+$vamTemplate->assign('PRODUCTS_PRICE', $products_price['formated']);
+$vamTemplate->assign('PRODUCTS_PRICE_PLAIN', $products_price['plain']);
+if ($product->data['products_vpe_status'] == 1 && $product->data['products_vpe_value'] != 0.0 && $products_price['plain'] > 0)
+$vamTemplate->assign('PRODUCTS_VPE', $vamPrice->Format($products_price['plain'] * (1 / $product->data['products_vpe_value']), true).TXT_PER.vam_get_vpe_name($product->data['products_vpe']));
+$vamTemplate->assign('PRODUCTS_ID', $product->data['products_id']);
+if ($vamPrice->CheckSpecial($product->data['products_id']) > 0) {
+$vamTemplate->assign('PRODUCTS_SPECIAL', 100-($vamPrice->CheckSpecial($product->data['products_id'])*100/$vamPrice->GetPprice($product->data['products_id'])));
+}
+$vamTemplate->assign('LABEL_ID', $product->data['label_id']);
+$vamTemplate->assign('PRODUCT_LABEL', vam_get_label_name($product->data['label_id']));
+$vamTemplate->assign('PRODUCTS_NAME', vam_parse_input_field_data($product->data['products_name'], array('"' => '&quot;')));
+if ($_SESSION['customers_status']['customers_status_show_price'] != 0) {
+// price incl tax
+$tax_rate = $vamPrice->TAX[$product->data['products_tax_class_id']];				
+$tax_info = $main->getTaxInfo($tax_rate);
+$vamTemplate->assign('PRODUCTS_TAX_INFO', $tax_info);
+$vamTemplate->assign('PRODUCTS_SHIPPING_LINK',$main->getShippingLink());
+}
+$vamTemplate->assign('PRODUCTS_MODEL', $product->data['products_model']);
+$vamTemplate->assign('PRODUCTS_URL_INFO', vam_href_link(FILENAME_PRODUCT_INFO, vam_product_link($product->data['products_id'], $product->data['products_name'])));
+$vamTemplate->assign('PRODUCTS_REVIEWS_URL', vam_href_link(FILENAME_PRODUCT_REVIEWS, 'products_id='.$product->data['products_id']));
+$vamTemplate->assign('PRODUCTS_EAN', $product->data['products_ean']);
+$vamTemplate->assign('PRODUCTS_QUANTITY', $product->data['products_quantity']);
+$vamTemplate->assign('PRODUCTS_LENGTH', $product->data['products_length']);
+$vamTemplate->assign('PRODUCTS_WIDTH', $product->data['products_width']);
+$vamTemplate->assign('PRODUCTS_HEIGHT', $product->data['products_height']);
+$vamTemplate->assign('PRODUCTS_VOLUME', $product->data['products_volume']);
+$vamTemplate->assign('PRODUCTS_WEIGHT', $product->data['products_weight']);
+$vamTemplate->assign('PRODUCTS_STATUS', $product->data['products_status']);
+$vamTemplate->assign('PRODUCTS_ORDERED', $product->data['products_ordered']);
+$vamTemplate->assign('PRODUCTS_PRINT', '<img src="images/icons/buttons/print.png" alt="" />');
+$vamTemplate->assign('PRODUCTS_PRINT_LINK', vam_href_link(FILENAME_PRINT_PRODUCT_INFO, 'products_id='.$product->data['products_id']));      
+if ($_SESSION['customers_status']['customers_status_id'] == 0) $vamTemplate->assign('PRODUCTS_EDIT_LINK', vam_href_link_admin(FILENAME_EDIT_PRODUCTS, 'cPath=' . $cPath . '&amp;pID=' . $product->data['products_id'] . '&amp;action=new_product'));      
+$vamTemplate->assign('PRODUCTS_DESCRIPTION', stripslashes($product->data['products_description']));
+$vamTemplate->assign('PRODUCTS_SHORT_DESCRIPTION', stripslashes($product->data['products_short_description']));
+
+$manufacturer_query = vamDBquery("select m.manufacturers_id, m.manufacturers_name, m.manufacturers_image, mi.manufacturers_url from " . TABLE_MANUFACTURERS . " m left join " . TABLE_MANUFACTURERS_INFO . " mi on (m.manufacturers_id = mi.manufacturers_id and mi.languages_id = '" . (int)$_SESSION['languages_id'] . "'), " . TABLE_PRODUCTS . " p  where p.products_id = '" . $product->data['products_id'] . "' and p.manufacturers_id = m.manufacturers_id");
+$manufacturer = vam_db_fetch_array($manufacturer_query,true);
+
+$vamTemplate->assign('CATEGORY', $cat_data['categories_name']);
+$vamTemplate->assign('CATEGORY_ID', $current_category_id);
+$vamTemplate->assign('MANUFACTURER_ID',$manufacturer['manufacturers_id']);
+$vamTemplate->assign('MANUFACTURER_IMAGE',$manufacturer['manufacturers_image']);
+$vamTemplate->assign('MANUFACTURER_LINK',vam_href_link(FILENAME_DEFAULT, 'manufacturers_id='.$manufacturer['manufacturers_id']));
+$vamTemplate->assign('MANUFACTURER',$manufacturer['manufacturers_name']);
+
+if ($product->data['products_image'] != '')
+$image = DIR_WS_INFO_IMAGES.$product->data['products_image'];
+
+if (!file_exists($image)) $image = DIR_WS_INFO_IMAGES.'../noimage.gif';
+
+$vamTemplate->assign('PRODUCTS_IMAGE', $image);
+$vamTemplate->assign('PRODUCTS_IMAGE_DESCRIPTION', htmlentities(strip_tags($product->data['products_image_description'])));
+
 $vamTemplate->assign('language', $_SESSION['language']);
 
 $vamTemplate->assign('module_content', $module_content);
