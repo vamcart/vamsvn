6b01efa0131e869073b7516dfa861b1bd8b67bae
vam <vamshop@gmail.com>
2020-03-08 00:07:07 +0300

    Новый дизайн страницы с отзывами о магзаине

--- site_reviews_info.php
+++ site_reviews_info.php
@@ -49,11 +49,18 @@ require (DIR_WS_INCLUDES.'header.php');
 	   $avatar = DIR_WS_IMAGES.'avatars/'.$reviews['customers_avatar'];
 if (!is_file($avatar)) $avatar = false;
 
+
+$star_rating = '';
+for($i=0;$i<number_format($reviews['reviews_rating']);$i++)	{
+$star_rating .= '<span class="rating"><i class="fa fa-star"></i></span> ';
+}
+		
 $vamTemplate->assign('REVIEWS_ID', $reviews['reviews_id']);
 $vamTemplate->assign('REVIEWS_AVATAR', $avatar);
 $vamTemplate->assign('REVIEWS_AUTHOR', $reviews['customers_name']);
 $vamTemplate->assign('REVIEWS_DATE', vam_date_short($reviews['date_added']));
 $vamTemplate->assign('REVIEWS_TEXT', nl2br($reviews['reviews_text']));
+$vamTemplate->assign('REVIEWS_STAR_RATING', $star_rating);
 $vamTemplate->assign('REVIEWS_RATING', $reviews['reviews_rating']);
 $vamTemplate->assign('BUTTON_BACK', '<a class="button" href="'.vam_href_link(FILENAME_SITE_REVIEWS, $get_params).'">'.vam_image_button('back.png', IMAGE_BUTTON_BACK).'</a>');
 
