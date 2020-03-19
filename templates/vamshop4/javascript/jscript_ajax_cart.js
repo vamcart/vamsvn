c9e2d6f80b12438387823baea440f9000d4c77bd
vam <vamshop@gmail.com>
2020-03-19 20:04:06 +0300

    Корректировка popup окна корзины

--- templates/vamshop4/javascript/jscript_ajax_cart.js
+++ templates/vamshop4/javascript/jscript_ajax_cart.js
@@ -194,7 +194,7 @@ $(document).ready(function(){
 });
 
 function cartPopupOn(){ 
-$('body').append('<div id="load_status_bg"></div><div class="cart_popup"><div class="cart_popuptext">Товар добавлен в корзину!</div><div class="cart_popuplink"><a href="checkout.php" class="button"><span><img src="/images/icons/buttons/buy.png" alt="" title="" width="12" height="12" />&nbsp;Оформить заказ</span></a><br /><br /><a href="javascript:cartPopupOff()" class="button"><span><img src="/images/icons/buttons/back.png" alt="" title="" width="12" height="12" />&nbsp;Продолжить покупки</span></a></div></div>'); 
+$('body').append('<div id="load_status_bg"></div><div class="cart_popup"><div class="cart_popuptext">Товар добавлен в корзину!</div><div class="cart_popuplink"><a href="shopping_cart.php" class="button"><span><img src="/images/icons/buttons/buy.png" alt="" title="" width="12" height="12" />&nbsp;Перейти в корзину</span></a><br /><br /><a href="javascript:cartPopupOff()" class="button"><span><img src="/images/icons/buttons/back.png" alt="" title="" width="12" height="12" />&nbsp;Продолжить покупки</span></a></div></div>'); 
 $('#load_status_bg').show().css({'filter' : 'alpha(opacity=80)'}); 
 $('.cart_popup').show(); 
 $(document).click(function (){
