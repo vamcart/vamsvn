9e7eb14950da04b5e131603c1855939ecbd634ff
vam <vamshop@gmail.com>
2020-03-20 13:12:48 +0300

    Корректировка названия модуля оплаты cash

--- lang/russian/modules/payment/cash.php
+++ lang/russian/modules/payment/cash.php
@@ -17,8 +17,8 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-define('MODULE_PAYMENT_CASH_TEXT_DESCRIPTION', 'Оплата наличными (самовывоз)');
-define('MODULE_PAYMENT_CASH_TEXT_TITLE', 'Оплата наличными (самовывоз)');
+define('MODULE_PAYMENT_CASH_TEXT_DESCRIPTION', 'Оплата наличными при получении');
+define('MODULE_PAYMENT_CASH_TEXT_TITLE', 'Оплата наличными при получении');
 define('MODULE_PAYMENT_CASH_TEXT_INFO', '');
 define('MODULE_PAYMENT_CASH_STATUS_TITLE', 'Разрешить модуль Оплата наличными');
 define('MODULE_PAYMENT_CASH_STATUS_DESC', 'Вы хотите разрешить использование модуля при оформлении заказов?<br />Модуль будет доступен при оформлении заказа только если на странице выбора доставки был выбран модуль доставки самовывоз.');
