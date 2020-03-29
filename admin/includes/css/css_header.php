c379c34e48b23630befa3db2a07286b22f93d552
vam <vamshop@gmail.com>
2020-03-29 10:31:22 +0300

    Добавлены php файлы для подключения в админке js, css файлов в header и footer

---
+++ admin/includes/css/css_header.php
@@ -0,0 +1,7 @@
+<?php
+include(DIR_FS_ADMIN.DIR_WS_INCLUDES.'/css/css_header.php');
+include(DIR_FS_ADMIN.DIR_WS_INCLUDES.'/javascript/script_footer.php');
+?>
+<?php
+if (file_exists(dirname(__FILE__) . '/local.css.php')) include('local.css.php');
+?>
---
+++ admin/includes/javascript/script_footer_local.php
@@ -0,0 +1,6 @@
+<?php
+include(DIR_FS_ADMIN.DIR_WS_INCLUDES.'/javascript/script_header.php');
+?>
+<?php
+if (file_exists(dirname(__FILE__) . '/local.css.php')) include('local.css.php');
+?>
