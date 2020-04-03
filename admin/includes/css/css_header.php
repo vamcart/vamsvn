94f9e461b8905d7b3079934022a5d3a7478c0b59
vam <vamshop@gmail.com>
2020-04-03 17:09:08 +0300

    Корректировка подключения css, стили меню подключаются первыми

--- admin/includes/css/css_header.php
+++ admin/includes/css/css_header.php
@@ -1,3 +1,6 @@
+<link href="includes/javascript/vamshop-menu/css/vamshop-menu-vertical.min.css" rel="stylesheet">
+<link href="includes/javascript/vamshop-menu/css/jquery.mCustomScrollbar.css" rel="stylesheet">
+
 <link rel="stylesheet" type="text/css" href="includes/css/bootstrap4/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="includes/css/fontawesome/font-awesome.css">
 <link rel="stylesheet" type="text/css" href="includes/css/fontroboto/font-roboto.css">
@@ -7,6 +10,3 @@
 
 <link rel="stylesheet" type="text/css" href="/jscript/jquery/plugins/select2/select2.css">
 <link rel="stylesheet" type="text/css" href="/jscript/jquery/plugins/select2/select2-bootstrap.css">
-
-<link href="includes/javascript/vamshop-menu/css/vamshop-menu-vertical.min.css" rel="stylesheet">
-<link href="includes/javascript/vamshop-menu/css/jquery.mCustomScrollbar.css" rel="stylesheet">
