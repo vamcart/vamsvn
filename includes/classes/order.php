66c83d0fb8234c1a553e72e42e68313b2e0a262c
vam <vamshop@gmail.com>
2020-04-10 20:49:59 +0300

    Корректировка кода для интеграции с 1с

--- includes/classes/order.php
+++ includes/classes/order.php
@@ -163,19 +163,25 @@
       $orders_products_query = vam_db_query("SELECT * FROM " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . vam_db_input($order_id) . "'");
       while ($orders_products = vam_db_fetch_array($orders_products_query)) {
         $this->products[$index] = array('qty' => $orders_products['products_quantity'],
-	                                	'id' => $orders_products['products_id'],
                                         'name' => $orders_products['products_name'],
+                                        'id' => $orders_products['products_id'],
+                                        'guid' => $orders_products['guid'],
+                                        'opid' => $orders_products['orders_products_id'],                                        
                                         'model' => $orders_products['products_model'],
                                         'tax' => $orders_products['products_tax'],
-					                    'price'=>$orders_products['products_price'],
-					                    'shipping_time'=>$orders_products['products_shipping_time'],
-                                        'final_price' => $orders_products['final_price']);
+                                        'price' => $orders_products['products_price'],
+                                        'shipping_time'=>$orders_products['products_shipping_time'],
+                                        'discount' => $orders_products['products_discount_made'],
+                                        'final_price' => $orders_products['final_price'],
+                                        'allow_tax' => $orders_products['allow_tax']);
 
         $subindex = 0;
         $attributes_query = vam_db_query("SELECT * FROM " . TABLE_ORDERS_PRODUCTS_ATTRIBUTES . " where orders_id = '" . vam_db_input($order_id) . "' and orders_products_id = '" . (int)$orders_products['orders_products_id'] . "'");
         if (vam_db_num_rows($attributes_query)) {
           while ($attributes = vam_db_fetch_array($attributes_query)) {
             $this->products[$index]['attributes'][$subindex] = array('option' => $attributes['products_options'],
+                                                                     'orders_products_id' => $attributes['orders_products_id'],
+                                                                     'guid' => $attributes['guid'],
                                                                      'value' => $attributes['products_options_values'],
                                                                      'prefix' => $attributes['price_prefix'],
                                                                      'price' => $attributes['options_values_price']);
@@ -197,6 +203,7 @@
     	
     	$order_query = "SELECT
 	        				products_id,
+	        				guid,
 	        				orders_products_id,
 	        				products_model,
 	        				products_name,
@@ -210,6 +217,7 @@
 	while ($order_data_values = vam_db_fetch_array($order_query)) {
 		$attributes_query = "SELECT
 		        				products_options,
+		        				guid,
 		        				products_options_values,
 		        				price_prefix,
 		        				options_values_price
