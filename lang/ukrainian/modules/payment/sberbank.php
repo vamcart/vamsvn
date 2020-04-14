24aba7ce1266047011bd5d25ac0f23e431397b10
vam <vamshop@gmail.com>
2020-04-14 21:44:10 +0300

    Добавлен новый модуль оплаты - Сбербанк Онлайн

---
+++ lang/ukrainian/modules/payment/sberbank.php
@@ -0,0 +1,23 @@
+﻿<?php
+define('MODULE_PAYMENT_SBERBANK_TEXT_TITLE', 'Сбербанк (Мир, Visa, Mastercard)');
+define('MODULE_PAYMENT_SBERBANK_TEXT_PUBLIC_TITLE', 'Оплата банковской картой. Оплата происходит через ПАО СБЕРБАНК с использованием Банковских карт платежных систем: МИР, VISA International, Mastercard Worldwide');
+define('MODULE_PAYMENT_SBERBANK_TEXT_ADMIN_DESCRIPTION', '');
+define('MODULE_PAYMENT_SBERBANK_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');  
+define('MODULE_PAYMENT_SBERBANK_STATUS_TITLE' , 'Разрешить модуль Сбербанк');
+define('MODULE_PAYMENT_SBERBANK_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
+define('MODULE_PAYMENT_SBERBANK_ALLOWED_TITLE' , 'Разрешённые страны');
+define('MODULE_PAYMENT_SBERBANK_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+define('MODULE_PAYMENT_SBERBANK_UNAME_TITLE' , 'Логин:');
+define('MODULE_PAYMENT_SBERBANK_UNAME_DESC' , 'Укажите Ваш логин.');
+define('MODULE_PAYMENT_SBERBANK_PASS_TITLE' , 'Пароль');
+define('MODULE_PAYMENT_SBERBANK_PASS_DESC' , 'Укажите Ваш пароль');
+define('MODULE_PAYMENT_SBERBANK_SECRET_TITLE' , 'Секретный ключ');
+define('MODULE_PAYMENT_SBERBANK_SECRET_DESC' , 'Секретный ключ для проверки уведомлений о платеже (необязательно)');
+define('MODULE_PAYMENT_SBERBANK_SORT_ORDER_TITLE' , 'Порядок сортировки');
+define('MODULE_PAYMENT_SBERBANK_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
+define('MODULE_PAYMENT_SBERBANK_ZONE_TITLE' , 'Зона');
+define('MODULE_PAYMENT_SBERBANK_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
+define('MODULE_PAYMENT_SBERBANK_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
+define('MODULE_PAYMENT_SBERBANK_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
+  
+?>
---
+++ sberbank.php
@@ -0,0 +1,29 @@
+<?php
+/*------------------------------------------------------------------------------
+  $Id: sberbank.php 1310 2017-12-12 00:00:08 dependab1e $
+------------------------------------------------------------------------------*/
+if(1 != $_REQUEST['status'] || $_REQUEST['operation'] != 'deposited') die('');
+require('includes/application_top.php');
+require (DIR_WS_CLASSES.'order.php');
+$order = new order($_REQUEST['orderNumber']);
+$order_sum = $order->info['total'];
+if($_REQUEST['checksum'])
+{
+	$data = 'amount;'.$order_sum.';mdOrder;'.$_REQUEST['mOrder'].';operation;deposited;orderNumber;'.$_REQUEST['orderNumber'].';status;1;';
+	$hmac = hash_hmac ( 'sha256' , $data , MODULE_PAYMENT_SBERBANK_SECRET);
+	if($hmac != $_REQUEST['checksum']) die('');
+}
+echo '';
+
+$sql_data_array = array('orders_status' => MODULE_PAYMENT_SBERBANK_STATUS);
+vam_db_perform('orders', $sql_data_array, 'update', "orders_id='".$_REQUEST['orderNumber']."'");
+$sql_data_arrax = array(
+	'orders_id' => $_REQUEST['orderNumber'],
+	'orders_status_id' => MODULE_PAYMENT_PAYONLINESYSTEM_ORDER_STATUS_ID,
+	'date_added' => 'now()',
+	'customer_notified' => '0',
+	'comments' => 'Sberbank System accepted this order payment'
+);
+vam_db_perform('orders_status_history', $sql_data_arrax);
+
+?>
