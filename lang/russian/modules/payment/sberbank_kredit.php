16af80e282798f3f2a414e09e3748a61efa325f4
vam <vamshop@gmail.com>
2020-04-26 17:43:13 +0300

    Добавлен новый модуль оплаты - Сбербанк Кредит

---
+++ lang/russian/modules/payment/sberbank_kredit.php
@@ -0,0 +1,25 @@
+<?php
+define('MODULE_PAYMENT_SBERBANK_KREDIT_TEXT_TITLE', 'Сбербанк Кредит');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_TEXT_PUBLIC_TITLE', 'Сбербанк Кредит');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_TEXT_ADMIN_DESCRIPTION', 'Как правильно настроить модуль оплаты Сбербанк читайте в нашем <a href="http://blog.vamshop.ru/2020/04/16/%d0%bd%d0%be%d0%b2%d1%8b%d0%b9-%d0%bc%d0%be%d0%b4%d1%83%d0%bb%d1%8c-%d0%be%d0%bf%d0%bb%d0%b0%d1%82%d1%8b-%d1%81%d0%b1%d0%b5%d1%80%d0%b1%d0%b0%d0%bd%d0%ba/"><u>блоге</u></a>.');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');  
+define('MODULE_PAYMENT_SBERBANK_KREDIT_STATUS_TITLE' , 'Разрешить модуль Сбербанк');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_ALLOWED_TITLE' , 'Разрешённые страны');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_UNAME_TITLE' , 'Логин:');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_UNAME_DESC' , 'Укажите Ваш логин.');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_MODE_TITLE' , 'Режим работы:');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_MODE_DESC' , 'Выберите режим работы модуля. test - тестовый. production - боевой.');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_PASS_TITLE' , 'Пароль');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_PASS_DESC' , 'Укажите Ваш пароль');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_SECRET_TITLE' , 'Секретный ключ');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_SECRET_DESC' , 'Секретный ключ для проверки уведомлений о платеже (необязательно)');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_SORT_ORDER_TITLE' , 'Порядок сортировки');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_ZONE_TITLE' , 'Зона');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
+define('MODULE_PAYMENT_SBERBANK_KREDIT_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
+  
+?>
