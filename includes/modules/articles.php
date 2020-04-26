577bd8d8e16623b54e4e8822a6b8f79061d39f40
vam <vamshop@gmail.com>
2020-04-26 20:47:36 +0300

    Корректировка кода

--- includes/modules/articles.php
+++ includes/modules/articles.php
@@ -4,7 +4,7 @@ $module = new vamTemplate;
 
 $module->assign('tpl_path', 'templates/'.CURRENT_TEMPLATE.'/');
 
-$sql = "select a.articles_id, ad.articles_name, ad.articles_description from " . TABLE_ARTICLES . " a left join " . TABLE_ARTICLES_DESCRIPTION . " ad on ad.articles_id = a.articles_id where a.articles_status = '1' and ad.language_id = '" . (int)$_SESSION['languages_id'] . "' ORDER BY articles_date_added DESC LIMIT " . MAX_NEW_ARTICLES_PER_PAGE . "";
+$sql = "select a.articles_id, a.articles_image, a.articles_keywords, a.sort_order, a.articles_date_added, ad.articles_name, ad.articles_viewed, ad.articles_head_desc_tag, au.authors_id, au.authors_image, au.authors_name, td.topics_id, td.topics_name from " . TABLE_ARTICLES . " a left join " . TABLE_AUTHORS . " au on a.authors_id = au.authors_id, " . TABLE_ARTICLES_TO_TOPICS . " a2t left join " . TABLE_TOPICS_DESCRIPTION . " td on a2t.topics_id = td.topics_id, " . TABLE_ARTICLES_DESCRIPTION . " ad where (a.articles_date_available IS NULL or to_days(a.articles_date_available) <= to_days(now())) and a.articles_id = a2t.articles_id and a.articles_status = '1' and a.articles_id = ad.articles_id and ad.language_id = '" . (int) $_SESSION['languages_id'] . "' and td.language_id = '" . (int) $_SESSION['languages_id'] . "' ORDER BY articles_date_added DESC LIMIT " . MAX_NEW_ARTICLES_PER_PAGE . "";
 
 $row = 0;
 
@@ -12,29 +12,100 @@ $module_content = array ();
 
 $query = vamDBquery($sql,true);
 
-while ($one = vam_db_fetch_array($query,true)) {
+while ($articles_default = vam_db_fetch_array($query,true)) {
 
-$SEF_parameter = '';
+		$article_reviews_query = vamDBquery("select count(*) as total from ".TABLE_ARTICLE_REVIEWS." ar where ar.articles_id='".$articles_default['articles_id']."'");
+		$article_reviews = vam_db_fetch_array($article_reviews_query, true);
 
-if (SEARCH_ENGINE_FRIENDLY_URLS == 'true')
+		$author_reviews_query = vamDBquery("select count(*) as total from ".TABLE_AUTHOR_REVIEWS." au where au.authors_id='".$articles_default['authors_id']."'");
+		$author_reviews = vam_db_fetch_array($author_reviews_query, true);
 
-$SEF_parameter = '&headline='.vam_cleanName($one['articles_name']);
+		$SEF_parameter = '';
+		if (SEARCH_ENGINE_FRIENDLY_URLS == 'true')
+			$SEF_parameter = '&article='.vam_cleanName($articles_default['articles_name']);
 
-$module_content[]=array(
+		$SEF_parameter_author = '';
+		if (SEARCH_ENGINE_FRIENDLY_URLS == 'true')
+			$SEF_parameter_author = '&author='.vam_cleanName($articles_default['authors_name']);
 
-'ARTICLES_NAME' => $one['articles_name'],
+		$SEF_parameter_category = '';
+		if (SEARCH_ENGINE_FRIENDLY_URLS == 'true')
+			$SEF_parameter_category = '&category='.vam_cleanName($articles_default['topics_name']);
 
-'ARTICLES_DESCRIPTOIN' => $one['articles_description'],
+$products_tags = explode (",", $articles_default['articles_keywords']);
 
-'ARTICLES_URL' => vam_href_link(FILENAME_ARTICLE_INFO, 'articles_id=' . $one['articles_id'] . $SEF_parameter)
+    $blacklist = array();
+    $blacklist = explode(",",TAGS_BLACKLIST);
 
-);
+    foreach ($blacklist as $key => $value) {  
+$articles_default['articles_keywords'] = str_replace($value.",","",$articles_default['articles_keywords']);
+} 
+
+		$products_tags = explode (",", $articles_default['articles_keywords']);
+		$tags_data = array();
+		
+          	foreach ($products_tags as $tags) {
+                $tags_data[] = array(
+                'NAME' => trim($tags),
+                'LINK' => vam_href_link(FILENAME_ARTICLES, 'akeywords='.rawurlencode(trim($tags))));
+        //$info->assign('tags_data', $tags_data);
+            }
+
+//echo var_dump($tags_data);
+
+
+   $article_rating = null;
+	$reviews_rating_query = vam_db_query("select count(*) as total, TRUNCATE(SUM(reviews_rating),2) as rating from ".TABLE_ARTICLE_REVIEWS." r, ".TABLE_ARTICLE_REVIEWS_DESCRIPTION." rd where r.articles_id = '".(int)$articles_default['articles_id']."' and r.reviews_id = rd.reviews_id and rd.languages_id = '".$_SESSION['languages_id']."' and rd.reviews_text !=''");
+	$reviews_rating = vam_db_fetch_array($reviews_rating_query);
+	if ($reviews_rating['total'] > 0 && $reviews_rating['rating'] > 0) {
+	$article_rating = $reviews_rating['rating']/$reviews_rating['total'];
+	}	
+
+   $author_rating = null;
+	$author_rating_query = vam_db_query("select count(*) as total, TRUNCATE(SUM(reviews_rating),2) as rating from ".TABLE_AUTHOR_REVIEWS." r, ".TABLE_AUTHOR_REVIEWS_DESCRIPTION." rd where r.authors_id = '".(int)$articles_default['authors_id']."' and r.reviews_id = rd.reviews_id and rd.languages_id = '".$_SESSION['languages_id']."' and rd.reviews_text !=''");
+	$author_rating = vam_db_fetch_array($author_rating_query);
+	if ($author_rating['total'] > 0 && $author_rating['rating'] > 0) {
+	$author_rating = $author_rating['rating']/$author_rating['total'];
+	}	
+
+		$module_content[] = array (
+		
+		'ARTICLE_NAME' => $articles_default['articles_name'],
+		'ARTICLE_ID' => $articles_default['articles_id'],
+		'ARTICLE_REVIEWS' => $article_reviews['total'],
+		'ARTICLE_RATING' => $article_reviews['total'],
+		'ARTICLE_VIEWS' => $articles_default['articles_viewed'],
+		'ARTICLE_RATING' => intval($article_rating),
+		'ARTICLE_STAR_RATING' => vam_image('templates/'.CURRENT_TEMPLATE.'/img/stars_'.intval($article_rating).'.gif', sprintf(BOX_REVIEWS_TEXT_OF_5_STARS, intval($article_rating))),
+		'ARTICLE_NAME' => $articles_default['articles_name'],
+		'ARTICLE_NAME' => $articles_default['articles_name'],
+		'ARTICLE_IMAGE' => $articles_default['articles_image'],
+		'ARTICLE_KEYWORDS' => $articles_default['articles_keywords'],
+		'ARTICLE_KEYWORDS_ARRAY_TAGS' => $tags_data,
+		'ARTICLE_KEYWORDS_ARRAY' => array($articles_default['articles_keywords']),
+//		'ARTICLE_KEYWORDS_ARRAY' => explode(",", $articles_default['articles_keywords']),
+		'ARTICLE_SHORT_DESCRIPTION' => $articles_default['articles_head_desc_tag'], 
+		'ARTICLE_DATE' => vam_date_short($articles_default['articles_date_added']), 
+		'ARTICLE_LINK' => vam_href_link(FILENAME_ARTICLE_INFO, 'articles_id=' . $articles_default['articles_id'] . $SEF_parameter), 
+		'AUTHOR_NAME' => $articles_default['authors_name'], 
+		'AUTHOR_IMAGE' => $articles_default['authors_image'], 
+		'AUTHOR_ID' => $articles_default['authors_id'], 
+		'AUTHOR_REVIEWS' => $author_reviews['total'],
+		'AUTHOR_RATING' => intval($article_rating),
+		'AUTHOR_STAR_RATING' => vam_image('templates/'.CURRENT_TEMPLATE.'/img/stars_'.intval($article_rating).'.gif', sprintf(BOX_REVIEWS_TEXT_OF_5_STARS, intval($article_rating))),
+		'AUTHOR_LINK' =>  vam_href_link(FILENAME_ARTICLES, 'authors_id=' . $articles_default['authors_id'] . $SEF_parameter_author), 
+		'ARTICLE_CATEGORY_NAME' => $articles_default['topics_name'],
+		'ARTICLE_CATEGORY_LINK' => vam_href_link(FILENAME_ARTICLES, 'tPath=' . $articles_default['topics_id'] . $SEF_parameter_category)
+		
+		);
 
 }
 
 if (sizeof($module_content) > 0) {
 
 $module->assign('language', $_SESSION['language']);
+
+//echo var_dump($module_content);
 
 $module->assign('module_content',$module_content);
 
