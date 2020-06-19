7b945e0da6a8bafa1985db95f6f5f7041cf16c29
vam <vamshop@gmail.com>
2020-06-19 12:57:32 +0300

    Корректировка шаблона корзины, улучшен дизайн для смартфонов

--- templates/vamshop4/javascript/vamshop4.js
+++ templates/vamshop4/javascript/vamshop4.js
@@ -442,7 +442,7 @@ a.css(d.property,f-b+"px"))})});m.each(function(){var a=c(this);a.attr("style",a
 this.options)});b._afterUpdate&&b._afterUpdate(a,b._groups)};b._update=function(a,e){if(e&&"resize"===e.type){var d=c(window).width();if(d===n)return;n=d}a?-1===f&&(f=setTimeout(function(){q(e);f=-1},b._throttle)):q(e)};c(b._applyDataApi);c(window).bind("load",function(a){b._update(!1,a)});c(window).bind("resize orientationchange",function(a){b._update(!0,a)})})(jQuery);
 
 // Responsive equal height
-if ($(window).width() > 767) {
+if ($(window).width() > 360) {
 $(window).on('load', function () {
     $('.card-body .img-wrap').matchHeight();
     $('.card-body .attributes').matchHeight();
@@ -453,7 +453,7 @@ $(window).on('load', function () {
     $('.payment-method .card-title').matchHeight();
     $('.shipping-method .card-title').matchHeight();
 });
-$(document).ajaxComplete(function () {
+$(document).ajaxSuccess(function () {
     $('.card-body .img-wrap').matchHeight();
     $('.card-body .info-wrap .title').matchHeight();
     $('.card-body .attributes').matchHeight();
