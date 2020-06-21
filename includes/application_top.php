142465d1af2f9d71070d0f2616fbaa89db1d1fec
vam <vamshop@gmail.com>
2020-06-21 11:25:02 +0300

    Корректировка кода

--- includes/application_top.php
+++ includes/application_top.php
@@ -60,7 +60,7 @@ if (defined('DIR_WS_INCLUDES') === false) header('Location: install');
 $php4_3_10 = (0 == version_compare(phpversion(), "4.3.10"));
 define('PHP4_3_10', $php4_3_10);
 // define the project version
-define('PROJECT_VERSION', 'VamShop 1.99.20');
+define('PROJECT_VERSION', 'VamShop 1.99.21');
 
 // set the type of request (secure or not)
 $request_type = (getenv('HTTPS') == '1' || getenv('HTTPS') == 'on') ? 'SSL' : 'NONSSL';
