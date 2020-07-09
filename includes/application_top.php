1e8a7f08df6331299b52b1ff5fc6186f3386ff82
vam <vamshop@gmail.com>
2020-07-09 18:08:58 +0300

    Корректировка редиректа при разбивке на страницы фильтров

--- includes/application_top.php
+++ includes/application_top.php
@@ -794,7 +794,7 @@ define('TAX_DECIMAL_PLACES','2');
 
 $url = vam_href_link(basename($PHP_SELF), vam_get_all_get_params(array ('page', 'cPath', 'info', 'x', 'y')));
 
-if ($cID && PRODUCT_LIST_RECURSIVE == 'false') {
+if ($cID && PRODUCT_LIST_RECURSIVE == 'false' && !strstr($PHP_SELF, FILENAME_PRODUCTS_FILTERS)) {
 
 $listing_sql = "select p.products_fsk18, p.products_shippingtime, p.products_model, p.products_ean, pd.products_name, m.manufacturers_name, p.products_quantity, p.products_image, p.products_weight, pd.products_short_description, pd.products_description, p.products_id, p.manufacturers_id, p.products_price, p.products_vpe, p.products_vpe_status, p.products_vpe_value, p.products_discount_allowed, p.products_tax_class_id from products_description pd, products_to_categories p2c, products p left join manufacturers m on p.manufacturers_id = m.manufacturers_id left join specials s on p.products_id = s.products_id where p.products_status = '1' and p.products_id = p2c.products_id and pd.products_id = p2c.products_id and pd.language_id = '1' and p2c.categories_id = '".$cID."' ORDER BY p.products_price DESC";
 $max_result = MAX_DISPLAY_SEARCH_RESULTS;
