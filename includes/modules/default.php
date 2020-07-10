987f36214e9527e1a4d4570ba97c6c3ca3b05318
vam <vamshop@gmail.com>
2020-07-10 20:02:55 +0300

    Корректировка сортировки

--- includes/modules/default.php
+++ includes/modules/default.php
@@ -473,7 +473,7 @@ elseif ($category_depth == 'products' || $_GET['manufacturers_id']) {
     }
     }
     $manufacturer_sort .= '<a href="'.vam_href_link(FILENAME_DEFAULT, 'cat='.$current_category_id).'">' . TEXT_ALL_MANUFACTURERS . '</a> ';
-    $manufacturer_dropdown .= vam_draw_pull_down_menu('filter_id', $options, $_GET['filter_id'], 'class="form-control" onchange="this.form.submit()"');
+    $manufacturer_dropdown .= vam_draw_pull_down_menu('filter_id', $options, $_GET['filter_id'], 'class="form-control noselect2" onchange="this.form.submit()"');
     $manufacturer_dropdown .= '</form>'."\n"; 
   }
   }
