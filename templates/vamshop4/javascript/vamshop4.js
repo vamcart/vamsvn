dd1a333953f2e7f62e10a9717cc5dcfa5883da7c
vam <vamshop@gmail.com>
2020-08-24 00:10:25 +0300

    Код service worker'a вынесен в общим JS файл

--- templates/vamshop4/javascript/vamshop4.js
+++ templates/vamshop4/javascript/vamshop4.js
@@ -1,3 +1,48 @@
+// Register service worker to control making site work offline
+$(function(){
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/sw.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
+
+// Code to handle install prompt on desktop
+var deferredPrompt = null;
+var addBtn = document.querySelector('.a2hs-button');
+if (addBtn != null) {
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', function(e) {
+	
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = '';
+
+  addBtn.addEventListener('click', function(e) {
+
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(function(choiceResult) {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+}
+
+});
+
+
 $(document).ready(function(){
 
 // Lazy Load
