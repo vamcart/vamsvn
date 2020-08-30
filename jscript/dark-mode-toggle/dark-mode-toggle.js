de8c168502c934e60ea86f3962afea0516a1812f
vam <vamshop@gmail.com>
2020-08-30 10:28:40 +0300

    Корректировка стилей, добавлены css переменные

--- jscript/dark-mode-toggle/dark-mode-toggle.js
+++ jscript/dark-mode-toggle/dark-mode-toggle.js
@@ -36,7 +36,7 @@ const PERMANENT_COLOR_SCHEME = 'permanentcolorscheme';
 const ALL = 'all';
 const NOT_ALL = 'not all';
 const NAME = 'dark-mode-toggle';
-const DEFAULT_URL = 'https://googlechromelabs.github.io/dark-mode-toggle/demo/';
+const DEFAULT_URL = 'images/icons/dark-mode-toggle/';
 
 // See https://html.spec.whatwg.org/multipage/common-dom-interfaces.html ↵
 // #reflecting-content-attributes-in-idl-attributes.
