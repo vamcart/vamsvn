b8dc4b505716e8a897b87122f46487db9f6ecd56
vam <vamshop@gmail.com>
2020-09-26 22:38:03 +0300

    Код DaData подсказок вынесен в footer

--- templates/vamshop4/javascript/script_footer.php
+++ templates/vamshop4/javascript/script_footer.php
@@ -10,6 +10,7 @@ $bender->enqueue("jscript/jquery/plugins/cookie/jquery.cookie.js");
 $bender->enqueue("jscript/jquery/plugins/lazyload/lazyload.min.js");
 //$bender->enqueue("jscript/jquery/plugins/slick/slick.js");
 $bender->enqueue("jscript/jquery/plugins/owl/owl.carousel.min.js");
+//$bender->enqueue("jscript/jquery/plugins/tiny-slider/tiny-slider.js");
 //$bender->enqueue("jscript/jquery/plugins/select2/select2.js");
 //$bender->enqueue("jscript/jquery/plugins/select2/i18n/" . $_SESSION['language_code'] . ".js");
 $bender->enqueue("jscript/jquery/plugins/scrollup/jquery.scrollup.min.js");
@@ -24,6 +25,106 @@ if (AJAX_CART == 'true') $bender->enqueue("templates/".CURRENT_TEMPLATE."/javasc
 <?php
 echo $bender->output("templates/".CURRENT_TEMPLATE."/cache/".CURRENT_TEMPLATE."-packed.js");
 ?>
+<?php
+if ( strstr($PHP_SELF, FILENAME_ADDRESS_BOOK)
+	or strstr($PHP_SELF, FILENAME_ADDRESS_BOOK_PROCESS)
+	or strstr($PHP_SELF, FILENAME_CHECKOUT)
+	or strstr($PHP_SELF, FILENAME_ADDRESS_BOOK_PROCESS)
+	or strstr($PHP_SELF, FILENAME_CHECKOUT_PAYMENT_ADDRESS)
+	or strstr($PHP_SELF, FILENAME_CHECKOUT_SHIPPING_ADDRESS)
+	or strstr($PHP_SELF, FILENAME_CREATE_ACCOUNT) ) {
+?>
+<?php
+if (DADATA_API_KEY != "" and $_SESSION['language'] == "russian") {
+?>
+<!--[if lt IE 10]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.1/jquery.xdomainrequest.min.js"></script>
+<![endif]-->
+<script src="https://cdn.jsdelivr.net/jquery.suggestions/16.5.2/js/jquery.suggestions.min.js"></script>
+<script>
+$(function () {
+
+var token = "<?php echo DADATA_API_KEY; ?>";
+
+$("#firstname").suggestions({
+  serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+  partner: "VAMSHOP",
+  token: token,
+  scrollOnFocus: false,
+  type: "NAME",
+  params: {
+    parts: ["NAME"]
+  }
+});
+
+$("#lastname").suggestions({
+  serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+  partner: "VAMSHOP",
+  token: token,
+  scrollOnFocus: false,
+  type: "NAME",
+  params: {
+    parts: ["SURNAME"]
+  }
+});
+
+$("#email_address").suggestions({
+  serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+  partner: "VAMSHOP",
+  token: token,
+  scrollOnFocus: false,
+  type: "EMAIL",
+  params: {
+    parts: ["SURNAME"]
+  }
+});
+      
+$("#street_address").suggestions({
+  serviceUrl: "https://suggestions.dadata.ru/suggestions/api/4_1/rs",
+  partner: "VAMSHOP",
+  token: token,
+  scrollOnFocus: false,
+  type: "ADDRESS",
+  geoLocation: true,
+  onSelect: showSelected
+});
+
+function join(arr /*, separator */) {
+  var separator = arguments.length > 1 ? arguments[1] : " ";
+  return arr.filter(function(n){return n}).join(separator);
+}
+
+function showSelected(suggestion) {
+  var address = suggestion.data;
+  $("#postcode").val(address.postal_code);
+  if (address.region == "Москва" || address.region == "Санкт-Петербург" || address.region == "Севастополь") {
+  $("#state").val(address.region);
+  } else {
+  $("#state").val(join([
+    join([address.region, address.region_type_full], " ")
+    //join([address.region, address.region_type], " "),
+    //join([address.area_type, address.area], " ")
+  ]));
+  }
+  $("#city").val(join([
+    join([address.city_type, address.city], " "),
+    join([address.settlement_type, address.settlement], " ")
+  ]));
+  //$("#street_address").val(
+    //join([address.street_type, address.street], " "),
+    //join([address.house_type, address.house], " "),
+    //join([address.block_type, address.block], " "),
+    //join([address.flat_type, address.flat], " ")
+  //);
+}
+  
+});
+</script>
+<?php
+}
+}
+?>
+
 <?php
 if ( strstr($PHP_SELF, FILENAME_ADDRESS_BOOK)
 	or strstr($PHP_SELF, FILENAME_ADDRESS_BOOK_PROCESS)
