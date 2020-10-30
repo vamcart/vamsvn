3aba68c5c337ebbc51f0fa99b49093ddb800baa3
vam <vamshop@gmail.com>
2020-10-30 10:13:00 +0300

    Код яндекс метрики грузится с альтернативного CDN

--- checkout_success.php
+++ checkout_success.php
@@ -222,7 +222,7 @@ dataLayer.push({
 <script>
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
 
    ym('.YANDEX_METRIKA_ID.', "init", {
         id:'.YANDEX_METRIKA_ID.',
--- includes/google_conversiontracking.js.php
+++ includes/google_conversiontracking.js.php
@@ -40,7 +40,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <script>
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
 
    ym(<?php echo YANDEX_METRIKA_ID; ?>, "init", {
         id:<?php echo YANDEX_METRIKA_ID; ?>,
