b4d216188d03a9fb7d1d6e7f43c1cab984e77fc6
vam <vamshop@gmail.com>
2020-11-03 13:55:51 +0300

    Корректировка шаблонов плиток

--- includes/classes/product.php
+++ includes/classes/product.php
@@ -798,11 +798,11 @@ $orders_query = "select
 		global $PHP_SELF;
 		$vam_get_all_get_params_return = (basename($PHP_SELF) == 'product_info.php') ? preg_replace('/products_id=\d+&/', '', vam_get_all_get_params(array ('action'))) : vam_get_all_get_params(array ('action'));
 		if (AJAX_CART == 'true' && !vam_has_product_attributes($id)) {
-		$link = '<a class="btn btn-add-to-cart btn-block" href="'.vam_href_link(basename($PHP_SELF), 'action=buy_now&BUYproducts_id='.$id, 'NONSSL').'" onclick="doBuyNow(\''.$id.'\',\'1\'); return false;"><i class="fa fa-shopping-cart"></i> '.(($this->getProductCartStatus($id) == 1) ? IMAGE_BUTTON_IN_CART_IN : IMAGE_BUTTON_IN_CART).'</a>';
+		$link = '<a class="btn btn-add-to-cart btn-block'.(($this->getProductCartStatus($id) == 1) ? " active" : "").'" href="'.vam_href_link(basename($PHP_SELF), 'action=buy_now&BUYproducts_id='.$id, 'NONSSL').'" onclick="doBuyNow(\''.$id.'\',\'1\'); return false;"><i class="fa fa-shopping-cart"></i> '.(($this->getProductCartStatus($id) == 1) ? IMAGE_BUTTON_IN_CART_IN : IMAGE_BUTTON_IN_CART).'</a>';
 		} elseif (AJAX_CART == 'false' && vam_has_product_attributes($id)) {
-		$link = '<a class="btn btn-add-to-cart btn-block" href="'.vam_href_link(basename($PHP_SELF), 'action=buy_now&BUYproducts_id='.$id, 'NONSSL').'"><i class="fa fa-shopping-cart"></i> '.(($this->getProductCartStatus($id) == 1) ? IMAGE_BUTTON_IN_CART_IN : IMAGE_BUTTON_IN_CART).'</a>';
+		$link = '<a class="btn btn-add-to-cart btn-block'.(($this->getProductCartStatus($id) == 1) ? " active" : "").'" href="'.vam_href_link(basename($PHP_SELF), 'action=buy_now&BUYproducts_id='.$id, 'NONSSL').'"><i class="fa fa-shopping-cart"></i> '.(($this->getProductCartStatus($id) == 1) ? IMAGE_BUTTON_IN_CART_IN : IMAGE_BUTTON_IN_CART).'</a>';
 		} else {
-		$link = '<a class="btn btn-add-to-cart btn-block" href="'.vam_href_link(FILENAME_SHOPPING_CART, 'action=buy_now&BUYproducts_id='.$id, 'NONSSL').'"><i class="fa fa-shopping-cart"></i> '.(($this->getProductCartStatus($id) == 1) ? IMAGE_BUTTON_IN_CART_IN : IMAGE_BUTTON_IN_CART).'</a>';
+		$link = '<a class="btn btn-add-to-cart btn-block'.(($this->getProductCartStatus($id) == 1) ? " active" : "").'" href="'.vam_href_link(FILENAME_SHOPPING_CART, 'action=buy_now&BUYproducts_id='.$id, 'NONSSL').'"><i class="fa fa-shopping-cart"></i> '.(($this->getProductCartStatus($id) == 1) ? IMAGE_BUTTON_IN_CART_IN : IMAGE_BUTTON_IN_CART).'</a>';
 		}
 		
 		return $link;
