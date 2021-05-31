7abd3efd604bc69358c329529694aa8d4c9ac096
vam <vamshop@gmail.com>
2021-05-31 19:29:25 +0300

    Добавлена запись времени исполнения запроса в лог sql запросов

--- inc/vam_db_query.inc.php
+++ inc/vam_db_query.inc.php
@@ -27,57 +27,73 @@
     //$$link = mysqli_connect('localhost', 'root', '', 'vamshop');
     //echo $query.'<br>';
 
+//if ($_SERVER['REQUEST_URI'] == '/') {
+
     if (STORE_DB_TRANSACTIONS == 'true' && file_exists(STORE_PAGE_PARSE_TIME_LOG)) {
-      error_log('QUERY ' . $query . "\n", 3, STORE_PAGE_PARSE_TIME_LOG);
-    } 
+      error_log('QUERY ' . $query . ";"."\n"."\n", 3, STORE_PAGE_PARSE_TIME_LOG);
+    }
+
+//}
+    
     $queryStartTime = microtime(true);
     $result = mysqli_query($$link, $query) or vam_db_error($query, mysqli_errno($$link), mysqli_error($$link));
     $processTime = microtime(true) - $queryStartTime;
     $log_file = '';
     if (defined('DIR_FS_ADMIN')) {
       $log_file = 'db_query_slow-adm';
-      $max_time = 0.15;
+      $max_time = 1.0;
     } else {
       $log_file = 'db_query_slow';
-      $max_time = 0.15;
+      $max_time = 2.0;
     }
     if ($processTime > $max_time) {
       $log_file = DIR_FS_CATALOG . '.logs/' . $log_file . '-' . date('Y-m-d') . '.log';
       $processTime = number_format($processTime, 6);
-      $msg = $processTime . ' ' . date('Y-m-d H:i:s') . "\n" . $query . "\n";
+      $msg .= 'URI: ' . $_SERVER['REQUEST_URI'] . ";";
+      $msg .= $processTime . ' ' . date('Y-m-d H:i:s') . ";" . $query . ";";
       ob_start();
       debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
       $dbg = ob_get_contents();
       ob_end_clean();
       $dbg = str_replace(DIR_FS_CATALOG, '', $dbg);
-      $msg .= trim($dbg) . "\n";
-      $msg .= 'URI: ' . $_SERVER['REQUEST_URI'] . "\n";
-//      $msg .= 'IP: ' . (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'not set') . "\n";
-      error_log('##### ' . $msg . "\n", 3, $log_file);
+      $msg .= trim($dbg) . ";";
+      $msg .= 'URI: ' . $_SERVER['REQUEST_URI'] . ";";
+//      $msg .= 'IP: ' . (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'not set') . ";";
+      error_log('##### ' . $msg . ";", 3, $log_file);
     }
     if (substr(strtoupper($query), 0, 12) == 'ALTER TABLE ') {
       $log_file = 'db_query_alter.log';
       $log_file = DIR_FS_CATALOG . '.logs/' . $log_file;
       $processTime = number_format($processTime, 6);
-      $msg = $processTime . ' ' . date('Y-m-d H:i:s') . "\n" . $query . "\n";
+      $msg = $processTime . ' ' . date('Y-m-d H:i:s') . ";" . $query . ";";
       ob_start();
       debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
       $dbg = ob_get_contents();
       ob_end_clean();
       $dbg = str_replace(DIR_FS_CATALOG, '', $dbg);
-      $msg .= trim($dbg) . "\n";
-      $msg .= 'URI: ' . $_SERVER['REQUEST_URI'] . "\n";
-//      $msg .= 'IP: ' . (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'not set') . "\n";
-      error_log('##### ' . $msg . "\n", 3, $log_file);
+      $msg .= trim($dbg) . ";";
+      $msg .= 'URI: ' . $_SERVER['REQUEST_URI'] . ";";
+//      $msg .= 'IP: ' . (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'not set') . ";";
+      error_log('##### ' . $msg . ";", 3, $log_file);
     }
 
 
 //  echo 'time: '.$processTime.' Query: '.$query.'<br>';
 
+    $processTime = microtime(true) - $queryStartTime;
+    $processTime = number_format($processTime, 6);
+    $time .= 'URI: ' . $_SERVER['REQUEST_URI'] . ";";
+    $time .= $processTime . '; ' . date('Y-m-d H:i:s') . ";" . $query . ";";
+
+//if ($_SERVER['REQUEST_URI'] == '/') {
+
+
     if (STORE_DB_TRANSACTIONS == 'true' && file_exists(STORE_PAGE_PARSE_TIME_LOG)) {
        $result_error = mysqli_error($$link);
-       error_log('RESULT ' . $result->current_field . ' ' . $result_error . "\n", 3, STORE_PAGE_PARSE_TIME_LOG);
+       error_log('RESULT;' . $time . ';' . $result->current_field . ';' . $result_error . ";", 3, STORE_PAGE_PARSE_TIME_LOG);
     }
+    
+//}    
 
 //Start VaM db-error processing
     if (!$result) {
